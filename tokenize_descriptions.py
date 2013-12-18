e5416e91f17af99f33232c6edbee251e110f3c1b
J. Andrew Key <joeandrewkey@gmail.com>
2013-12-18 01:03:44 -0800

    Performed static content analysis with pylint, making the codebase more conformant.

--- tokenize_descriptions.py
+++ tokenize_descriptions.py
@@ -1,64 +1,62 @@
 #!/usr/bin/python
 
-import copy, json, os, sys, re, urllib, urllib2
-from subprocess import Popen, PIPE
-from query_templates import GENERIC_ELASTICSEARCH_QUERY
+import copy, json, sys, re, urllib2
+from query_templates import GENERIC_ELASTICSEARCH_QUERY, STOP_WORDS
 
 class InvalidArguments(Exception):
+	"""Thrown when invalid arguments are passed in via command line."""
 	pass
 
-STILL_BREAKABLE = 2
-UNIGRAM_SIZE = 1
 
-bad_characters = [ "\[", "\]", "'", "\{", "\}", '"', "/"]
-x = "|".join(bad_characters)
-cleanse_pattern = re.compile(x)
 
-unigram_tokens = []
-stop_words = ["CHECK", "CARD", "CHECKCARD", "PAYPOINT", "PURCHASE", "LLC" ]
-expected_fields = [ "BUSINESSSTANDARDNAME", "HOUSE", "STREET", "STRTYPE", \
-"CITYNAME", "STATE", "ZIP" ]
-complex_expected_fields = [ "lat", "lon"]
-
-#This function displays our output
 def display_results():
+	"""Displays our tokens, n-grams, and search results."""
 	numeric = re.compile("^[0-9/#]+$")
 
 	stop_tokens = []	
 	filtered_tokens = []
 	numeric_tokens = []
-	for token in tokens:
-		if token in stop_words:
+	for token in TOKENS:
+		if token in STOP_WORDS:
 			stop_tokens.append(token)
 		elif numeric.search(token): 
 			numeric_tokens.append(token)
 		else:
 			filtered_tokens.append(string_cleanse(token))
 
-	print "Unigrams are:\n\t" + str(tokens)
-	print "Unigrams matched to ElasticSearch:\n\t" + str(unigram_tokens)
+	print "Unigrams are:\n\t" + str(TOKENS)
+	print "Unigrams matched to ElasticSearch:\n\t" + str(UNIGRAM_TOKENS)
 	print "Of these:"
-	print "\t" + str(len(stop_tokens)) +    " stop words:      " + str(stop_tokens)
-	print "\t" + str(len(numeric_tokens)) + " numeric words:   " + str(numeric_tokens)
-	print "\t" + str(len(filtered_tokens)) + " worth searching: " + str(filtered_tokens)
+	print "\t" + str(len(stop_tokens)) + " stop words:      "\
+	+ str(stop_tokens)
+	print "\t" + str(len(numeric_tokens)) + " numeric words:   "\
+	+ str(numeric_tokens)
+	print "\t" + str(len(filtered_tokens)) + " worth searching: "\
+	+ str(filtered_tokens)
+
 	#show all search terms seperated by spaces		
 	query_string = " ".join(filtered_tokens)
-	n_gram_tokens = get_n_gram_tokens(filtered_tokens)
-	matched_n_gram_tokens = search_n_gram_tokens(n_gram_tokens)
-
 	
-	print "Search Attempt using n-grams = 1"
+	
+	print "Search Attempt using n-grams, n = 1"
 	print "You can also try Google:\n\t" + str(query_string)
 	get_query_string_search_results(query_string)
 
-	#FIXME these results actually suck, and aren't worth getting yet
-	#print "Search Attempt using n-grams > 1"
-	#print "These results currently are not especially good or relevant"
-	#get_composite_search_results(matched_n_gram_tokens,query_string)
+	#I think these results should be refined, as they sometimes over-score
+	#results that should be less relevant.
+	print "Search Attempt using n-grams, n >= 1"
+	print "WARNING: These results may over-score irrelevant n-grams."
+	print "\tIn the future, we can revise these results."
+	n_gram_tokens = get_n_gram_tokens(filtered_tokens)
+	matched_n_gram_tokens = search_n_gram_tokens(n_gram_tokens)
+	get_composite_search_results(matched_n_gram_tokens)
 
-def get_composite_search_results(matched_n_gram_tokens,query_string):
+def get_composite_search_results(matched_n_gram_tokens):
+	"""Obtains search results for a query composed of multiple
+	sub-queries.  At some point I may add 'query_string' as an
+	additional parameter."""
 	my_new_query = copy.deepcopy(GENERIC_ELASTICSEARCH_QUERY)
-	my_new_query["size"], my_new_query["from"] = 10,0
+	my_new_query["size"], my_new_query["from"] = 10, 0
 	
 	sub_query = {}
 	sub_query["match"] = {}
@@ -68,33 +66,37 @@ def get_composite_search_results(matched_n_gram_tokens,query_string):
 	sub_query["match"]["_all"]["boost"] = 1.2
 
 	for term in matched_n_gram_tokens:
-		s = copy.deepcopy(sub_query)
-		s["match"]["_all"]["query"] = term	
-		my_new_query["query"]["bool"]["should"].append(s)
+		my_sub_query = copy.deepcopy(sub_query)
+		my_sub_query["match"]["_all"]["query"] = term	
+		my_new_query["query"]["bool"]["should"].append(my_sub_query)
 
 	output = search_index(my_new_query)	
 	hits = output['hits']['hits']
 	display_hits(hits)
 
 def get_n_gram_tokens(list_of_tokens):
+	"""Generates a list of n-grams where n >= 2."""
 	n_gram_tokens = {}
-	x = len(list_of_tokens)
-	for j in range(x):
-		for i in range(x-j):
-			maybe = list_of_tokens[i:x-j]
-			y = len(maybe)
-			if y > UNIGRAM_SIZE:
-				if y not in n_gram_tokens:
-					n_gram_tokens[y] = []
-				n_gram_tokens[y].append(" ".join(maybe))
-
+	end_index = len(list_of_tokens)
+	for end_offset in range(end_index):
+		for start_index in range(end_index-end_offset):
+			n_gram_size = end_index - end_offset - start_index 
+			if n_gram_size > UNIGRAM_SIZE:
+				if n_gram_size not in n_gram_tokens:
+					n_gram_tokens[n_gram_size] = []
+				new_n_gram = \
+				list_of_tokens[start_index:end_index-end_offset]
+				n_gram_tokens[n_gram_size].append(" ".join(new_n_gram))
 	return n_gram_tokens
 
 def search_n_gram_tokens(n_gram_tokens):
+	"""Creates a boolean elasticsearch composed of multiple
+	sub-queries.  Each sub-query is itself a 'phrase' query
+	built of n-grams where n >= 2."""
 	matched_n_gram_tokens = []
 
 	my_new_query = copy.deepcopy(GENERIC_ELASTICSEARCH_QUERY)
-	my_new_query["size"], my_new_query["from"] = 0,0
+	my_new_query["size"], my_new_query["from"] = 0, 0
 
 	sub_query = {}
 	sub_query["match"] = {}
@@ -114,95 +116,104 @@ def search_n_gram_tokens(n_gram_tokens):
 				matched_n_gram_tokens.append(term)
 	return matched_n_gram_tokens
 	
-#Extracts the longest substring from our input, returning left over fragments
-def extract_longest_substring(longest_substring):
-	#We now use the longest substring, to find the following strings:
-	#1.  "original_term": the string that contained the longest substring
-	#2.  "pre": everything from the original term before the longest_substring
-	#3.  "post" : everything from the original term after the longest_substring
+def extract_longest_substring(long_substrings, longest_substring):
+	"""Extracts the longest substring from our input, returning left over fragments
+	We now use the longest_substring, to find the following strings:
+	1.  "original_term": the original string containing the longest_substring
+	2.  "pre": substring preceding the longest_substring
+	3.  "post" : substring following the longest_substring"""
 	
-	l = len(longest_substring)	
-	original_term = longest_substrings[l][longest_substring]
-	b = original_term.find(longest_substring)
-	c = b + len(longest_substring)
-	pre, post = original_term[:b], original_term[c:]
+	ls_len = len(longest_substring)	
+	original_term = long_substrings[ls_len][longest_substring]
+	start_index = original_term.find(longest_substring)
+	end_index = start_index + ls_len
+	pre, post = original_term[:start_index], original_term[end_index:]
 
 	#delete the current "longest substring" from our dictionary
-	del longest_substrings[l][longest_substring]
-	if len(longest_substrings[l]) == 0:
-		del longest_substrings[l]
+	del long_substrings[ls_len][longest_substring]
+	if len(long_substrings[ls_len]) == 0:
+		del long_substrings[ls_len]
 
 	return original_term, pre, post
 
-#Searches the merchants index and the merchant mapping
 def search_index(input_as_object):
-	input = json.dumps(input_as_object)
+	"""Searches the merchants index and the merchant mapping"""
+	input_string = json.dumps(input_as_object)
 	url = "http://brainstorm8:9200/merchants/merchant/_search"
-	request = urllib2.Request(url, input)
+	request = urllib2.Request(url, input_string)
 	response = urllib2.urlopen(request)
+	METRICS["query_count"] += 1
 	output = json.loads(response.read())
 	return output
 
-#Runs an ElasticSearch query to find the results
-def get_query_string_search_results(unigram_tokens):
+def get_query_string_search_results(my_query_string):
+	"""Runs an ElasticSearch query over all unigram tokens at once
+	to find a set of scored results."""
 	my_new_query = copy.deepcopy(GENERIC_ELASTICSEARCH_QUERY)
-	my_new_query["size"], my_new_query["from"] = 10,0
+	my_new_query["size"], my_new_query["from"] = 10, 0
 
 	sub_query = {}
 	sub_query["query_string"] = {}
 	sub_query["query_string"]["query"] = "__term"
 	my_new_query["query"]["bool"]["should"].append(sub_query)
-	sub_query["query_string"]["query"] = "".join(unigram_tokens)
+	sub_query["query_string"]["query"] = "".join(my_query_string)
 	
 	output = search_index(my_new_query)	
 	hits = output['hits']['hits']
 	display_hits(hits)
 
 def display_hits(hits):
+	"""Displays search results."""
+	expected_fields = [ "BUSINESSSTANDARDNAME", "HOUSE", "STREET"\
+	, "STRTYPE", "CITYNAME", "STATE", "ZIP" ]
+	complex_expected_fields = [ "lat", "lon"]
+
 	for item in hits: 
-		f = item['fields']
+		fields = item['fields']
 		#place "blanks" if expected field is null
-		for xf in expected_fields:
-			if xf not in f:
-				f[xf] = ""
-		for cf in complex_expected_fields:
-			if "pin.location" not in f:
-				f["pin.location"] = {}
-			if cf not in f["pin.location"]:
-				f["pin.location"][cf] = 0.0
+		for field in expected_fields:
+			if field not in fields:
+				fields[field] = ""
+		for field in complex_expected_fields:
+			if "pin.location" not in fields:
+				fields["pin.location"] = {}
+			if field not in fields["pin.location"]:
+				fields["pin.location"][field] = 0.0
 				
 		#display the search result
-		print "{0} {1} {2} {3} {4}, {5} {6} ({7}, {8})".format(f["BUSINESSSTANDARDNAME"]\
-		,f["HOUSE"], f["STREET"], f["STRTYPE"],f["CITYNAME"],f["STATE"],f["ZIP"]\
-		,f["pin.location"]["lat"], f["pin.location"]["lon"])
+		output_format = "{0} {1} {2} {3} {4}, {5} {6} ({7}, {8})"
+		print output_format.format(fields["BUSINESSSTANDARDNAME"]\
+		, fields["HOUSE"], fields["STREET"], fields["STRTYPE"]\
+		, fields["CITYNAME"], fields["STATE"], fields["ZIP"]\
+		, fields["pin.location"]["lat"], fields["pin.location"]["lon"])
 
-#Validates the command line arguments
 def initialize():
+	"""Validates the command line arguments."""
 	if len(sys.argv) != 2:
 		usage()
 		raise InvalidArguments("Incorrect number of arguments")
 	return sys.argv[1]
 
-#Recursively Finds all substrings for a term
-def powerset(term,substrings):
-	l = len(term)
-	if l not in substrings:
-		substrings[l] = {}
-	if term not in substrings[l]:
-		substrings[l][term] = ""
-	if l <= STILL_BREAKABLE:
+def powerset(term, substrings):
+	"""Recursively discover all substrings for a term."""
+	term_length = len(term)
+	if term_length not in substrings:
+		substrings[term_length] = {}
+	if term not in substrings[term_length]:
+		substrings[term_length][term] = ""
+	if term_length <= STILL_BREAKABLE:
 		return
-	if term in stop_words:
+	if term in STOP_WORDS:
 		return
 	else:
-		powerset(term[0:-1],substrings)
-		powerset(term[1:],substrings)
+		powerset(term[0:-1], substrings)
+		powerset(term[1:], substrings)
 
-#Looks for the longest substring in our merchants index that 
-#can actually be found.
 def search_substrings(substrings):
+	"""Looks for the longest substring in our merchants index that
+	#can actually be found."""
 	my_new_query = copy.deepcopy(GENERIC_ELASTICSEARCH_QUERY)
-	my_new_query["size"], my_new_query["from"] = 0,0
+	my_new_query["size"], my_new_query["from"] = 0, 0
 
 	sub_query = {}
 	sub_query["query_string"] = {}
@@ -218,49 +229,61 @@ def search_substrings(substrings):
 			if hit_count > 0:
 				return term
 
-#Recursively attempts to parse an unstructured transaction description into tokens
-def parse_description_into_search_tokens(longest_substrings, terms, input, recursive):
-	global tokens
-	
-	#This loop breaks up the input looking for new search terms
+def begin_parse(input_string):
+	"""Creates data structures used the first call into the 
+	parse_into_search_tokens function."""
+
+	print "Input: " + input_string
+	terms, long_substrings, recursive = [], {}, False	
+	parse_into_search_tokens(long_substrings, terms, input_string\
+	, recursive)
+
+def parse_into_search_tokens(long_substrings, terms, input_string, recursive):
+	"""Recursively attempts to parse an unstructured transaction
+	description into TOKENS."""
+	global TOKENS
+	#This loop breaks up the input string looking for new search terms
 	#that match portions of our ElasticSearch index
-	if len(input) >= STILL_BREAKABLE:
-		new_terms = input.split()
+	if len(input_string) >= STILL_BREAKABLE:
+		new_terms = input_string.split()
 		for term in new_terms:
 			term = string_cleanse(term)
 			if not recursive:
-				tokens.append(term)
+				TOKENS.append(term)
 			substrings = {}
-			powerset(term,substrings)
-			local_longest_substring = search_substrings(substrings)
-			if local_longest_substring is not None:
-				l = len(local_longest_substring)
-				if l not in longest_substrings:
-					longest_substrings[l] = {}
-				if local_longest_substring not in longest_substrings[l]:
-					longest_substrings[l][local_longest_substring] = term
+			powerset(term, substrings)
+			big_substring = search_substrings(substrings)
+			if big_substring is not None:
+				bs_length = len(big_substring)
+				if bs_length not in long_substrings:
+					long_substrings[bs_length] = {}
+				if big_substring not in long_substrings[bs_length]:
+					long_substrings[bs_length][big_substring] = term
 		terms.extend(new_terms)
 
 	#This check allows us to exit if no substrings are found
-	if len(longest_substrings) == 0:
+	if len(long_substrings) == 0:
 		return	
 
 	#This block finds the longest substring match in our index and adds it to our 
 	#dictionary of search terms.	
-	longest_substring = longest_substrings[sorted(longest_substrings.iterkeys())[-1]].keys()[0]
-	unigram_tokens.append(longest_substring)
+	longest_substring = long_substrings[sorted\
+	(long_substrings.iterkeys())[-1]].keys()[0]
+	UNIGRAM_TOKENS.append(longest_substring)
 
-	original_term, pre, post = extract_longest_substring(longest_substring)
-	tokens = rebuild_tokens(original_term,longest_substring,pre,post)
+	original_term, pre, post = extract_longest_substring(\
+	long_substrings, longest_substring)
+	TOKENS = rebuild_tokens(original_term, longest_substring, pre, post)
 
-	parse_description_into_search_tokens(longest_substrings, terms, pre + " " + post, True)
+	parse_into_search_tokens(long_substrings, terms, pre + " " + post, True)
 
-#Rebuild our complete list of tokens following a substring extraction
 def rebuild_tokens(original_term, longest_substring, pre, post):
+	"""Rebuilds our complete list of tokens following a substring
+	extraction."""
 	rebuilt_tokens = []
-	for i in range(len(tokens)):
-		if tokens[i] != original_term:	
-			rebuilt_tokens.append(tokens[i])
+	for i in range(len(TOKENS)):
+		if TOKENS[i] != original_term:	
+			rebuilt_tokens.append(TOKENS[i])
 		else:
 			if pre != "":
 				rebuilt_tokens.append(pre)
@@ -269,19 +292,25 @@ def rebuild_tokens(original_term, longest_substring, pre, post):
 				rebuilt_tokens.append(post)
 	return rebuilt_tokens
 
-def string_cleanse(s):
-	return re.sub(cleanse_pattern,"",s)	
+def string_cleanse(original_string):
+	"""Strips out characters that might confuse ElasticSearch."""
+	bad_characters = [ "\[", "\]", "'", "\{", "\}", '"', "/"]
+	bad_character_regex = "|".join(bad_characters)
+	cleanse_pattern = re.compile(bad_character_regex)
+	return re.sub(cleanse_pattern, "", original_string)	
 
 def usage():
+	"""Shows the user which parameters to send into the program."""
 	print "Usage:\n\t<quoted_transaction_description_string>"
 
-#input = "MEL'S DRIVE-IN #2 SAN FRANCISCOCA 24492153337286434101508"
+#INPUT_STRING = "MEL'S DRIVE-IN #2 SAN FRANCISCOCA 24492153337286434101508"
 
-input = initialize()
-
-longest_substrings, terms, tokens = {}, [], []
-print "Input: " + input
-recursive = False
-parse_description_into_search_tokens(longest_substrings, terms, input, recursive)
+UNIGRAM_SIZE, STILL_BREAKABLE = 1, 2
+UNIGRAM_TOKENS, TOKENS = [], []
+METRICS = { "query_count" : 0 }
+INPUT_STRING = initialize()
+begin_parse(INPUT_STRING)
 display_results()
+print "The total number of ElasticSearch queries issued: " + \
+str(METRICS["query_count"])
 
