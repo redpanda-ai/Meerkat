217c3469fdc03cd330cbfb76201521189ba71240
J. Andrew Key <joeandrewkey@gmail.com>
2013-12-22 03:32:45 -0800

    Pylint-ed query_templates.

--- query_templates.py
+++ query_templates.py
@@ -3,7 +3,7 @@
 """Words we wish to ignore while searching."""
 STOP_WORDS = ["CHECK", "CARD", "CHECKCARD", "PAYPOINT", "PURCHASE", "LLC" ]
 
-"""Template for building a search."""
+#Template for building a search.
 GENERIC_ELASTICSEARCH_QUERY = {}
 GENERIC_ELASTICSEARCH_QUERY["from"] = 0
 GENERIC_ELASTICSEARCH_QUERY["size"] = 10
@@ -15,29 +15,36 @@ GENERIC_ELASTICSEARCH_QUERY["query"]["bool"]["minimum_number_should_match"] = 1
 GENERIC_ELASTICSEARCH_QUERY["query"]["bool"]["boost"] = 1.0
 GENERIC_ELASTICSEARCH_QUERY["query"]["bool"]["should"] = []
 
-"""Aliases for magic numbers."""
-SENTINEL = 200.0
+def get_create_object(es_index, es_type, cell_id):
+	"""Builds object used for building a bulk create command."""
+	create_object = {}
+	create_object["create"] = {}
+	create_object["create"]["_index"] = es_index
+	create_object["create"]["_type"] = es_type
+	create_object["create"]["_id"] = cell_id 
+	return create_object
+
+#Aliases for magic numbers.
 NULL, FLOAT, DATE, INT, STRING = 0, 1, 2, 3, 4
 DATA_TYPE_NAME, PATTERN = 0, 1
 NAME, DATA_TYPE, INDEX = 0, 1, 2
 
-
 def get_mapping_template(es_type_name, shards, replicas, column_meta\
 , data_types, total_fields):
 	"Builds a mapping for an index type."""
-	object = {}
-	object["settings"] = {}
-	object["settings"]["number_of_shards"] = shards 
-	object["settings"]["number_of_replicas"] = replicas 
-	object["settings"]["mappings"] = {}
-	object["settings"]["mappings"][es_type_name] = {}
-	my_map = object["settings"]["mappings"][es_type_name] 
+	map_object = {}
+	map_object["settings"] = {}
+	map_object["settings"]["number_of_shards"] = shards 
+	map_object["settings"]["number_of_replicas"] = replicas 
+	map_object["settings"]["mappings"] = {}
+	map_object["settings"]["mappings"][es_type_name] = {}
+	my_map = map_object["settings"]["mappings"][es_type_name] 
 	my_map["_source"] = {}
 	my_map["_source"]["enabled"] = True	
 	my_map["_source"]["properties"] = {}
 	my_properties = my_map["_source"]["properties"] 
 
-	"""Adds a mapping for most non-null fields."""
+	#Adds a mapping for most non-null fields.
 	for column_number in range(total_fields):
 		data_type = column_meta[column_number][DATA_TYPE]
 		column_name = column_meta[column_number][NAME]
@@ -54,12 +61,12 @@ def get_mapping_template(es_type_name, shards, replicas, column_meta\
 			if column_type == "date":
 				my_properties[column_name]["format"] = \
 				"YYYYmmdd"
-	"""Adds a mapping for the geo-point."""
+	#Adds a mapping for the geo-point.
 	my_properties["pin"] = {}
 	my_properties["pin"]["properties"] = {}
 	my_properties["pin"]["properties"]["location"] = {}
 	my_properties["pin"]["properties"]["location"]["type"] = "geo_point"
 
-	"""Add composite fields here."""
+	#Add composite fields here.
 
-	return object		
+	return map_object		
