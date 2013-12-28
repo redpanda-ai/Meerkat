9d71c1e220efb3a5e27c41eb8aaa17de47804e92
J. A. Key <joeandrewkey@gmail.com>
2013-12-28 00:51:22 -0800

    Last bit of PyLint convention fixing.

--- src/various_tools.py
+++ src/various_tools.py
@@ -7,14 +7,14 @@ import re
 
 def string_cleanse(original_string):
 	"""Strips out characters that might confuse ElasticSearch."""
-	bad_characters = [ "\[", "\]", "'", "\{", "\}", '"', "/" ]
+	bad_characters = [ r"\[", r"\]", r"'", r"\{", r"\}", r'"', r"/" ]
 	bad_character_regex = "|".join(bad_characters)
 	cleanse_pattern = re.compile(bad_character_regex)
 	return re.sub(cleanse_pattern, "", original_string)
 
 def numeric_cleanse(original_string):
 	"""Strips out characters that might confuse ElasticSearch."""
-	bad_characters = [ "\[", "\]", "'", "\{", "\}", '"', "/", "-" ]
+	bad_characters = [ r"\[", r"\]", r"'", r"\{", r"\}", r'"', r"/", r"-" ]
 	bad_character_regex = "|".join(bad_characters)
 	cleanse_pattern = re.compile(bad_character_regex)
 	return re.sub(cleanse_pattern, "", original_string)
