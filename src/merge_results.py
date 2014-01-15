f9323feda6b77e1fa8ed902eeb94c9c249aa43e4
Matt Sevrens <msevrens@gmail.com>
2014-01-14 20:17:57 -0800

    fixing pylint errors

--- src/merge_results.py
+++ src/merge_results.py
@@ -9,25 +9,26 @@ FILE_A = open(sys.argv[1])
 FILE_B = open("../labeledTrans.csv")
 DICT_A = csv.DictReader(FILE_A)
 DICT_B = csv.DictReader(FILE_B)
-fileCopy = []
-fieldnames = DICT_B.fieldnames
+FILE_COPY = []
+FIELD_NAMES = DICT_B.fieldnames
 
 # Copy
-for lRow in DICT_B: 
-	fileCopy.append(lRow)
+for lRow in DICT_B:
+	FILE_COPY.append(lRow)
 
 # Merge
 for row in DICT_A:
-	for lRow in fileCopy:
-		if row['DESCRIPTION'] == lRow['DESCRIPTION'] and lRow['PERSISTENTRECORDID'] == "":
-			lRow['PERSISTENTRECORDID'] = row['PERSISTENTRECORDID']	
+	for lRow in FILE_COPY:
+		if (row['DESCRIPTION'] == lRow['DESCRIPTION']
+			and lRow['PERSISTENTRECORDID'] == ""):
+			lRow['PERSISTENTRECORDID'] = row['PERSISTENTRECORDID']
 			break
 
 # Write
-output = open('output.csv', 'w')
-csvwriter = csv.DictWriter(output, delimiter=',', fieldnames=fieldnames)
-csvwriter.writeheader()
-for row in fileCopy:
-	csvwriter.writerow(row)
-output.close()
+OUTPUT = open('output.csv', 'w')
+CSV_WRITER = csv.DictWriter(OUTPUT, delimiter=',', fieldnames=FIELD_NAMES)
+CSV_WRITER.writeheader()
+for row in FILE_COPY:
+	CSV_WRITER.writerow(row)
+OUTPUT.close()
 
