8e4db380286173c7df591821d783e8cbd39bb7dd
Matt Sevrens <msevrens@gmail.com>
2014-01-14 20:06:47 -0800

    changing module name

--- src/merge_results.py
+++ src/merge_results.py
@@ -1,23 +1,23 @@
 #!/usr/local/bin/python3
 
-"""This script merges a new set of labeled transactions into our 
-	existing set."""
+"""This script merges a new set of labeled
+ transactions into our existing set."""
 
 import csv, sys
 
-fileA = open(sys.argv[1])
-fileB = open("../labeledTrans.csv")
-dictA = csv.DictReader(fileA)
-dictB = csv.DictReader(fileB)
+FILE_A = open(sys.argv[1])
+FILE_B = open("../labeledTrans.csv")
+DICT_A = csv.DictReader(FILE_A)
+DICT_B = csv.DictReader(FILE_B)
 fileCopy = []
-fieldnames = dictB.fieldnames
+fieldnames = DICT_B.fieldnames
 
 # Copy
-for lRow in dictB: 
+for lRow in DICT_B: 
 	fileCopy.append(lRow)
 
 # Merge
-for row in dictA:
+for row in DICT_A:
 	for lRow in fileCopy:
 		if row['DESCRIPTION'] == lRow['DESCRIPTION'] and lRow['PERSISTENTRECORDID'] == "":
 			lRow['PERSISTENTRECORDID'] = row['PERSISTENTRECORDID']	
@@ -28,6 +28,6 @@ output = open('output.csv', 'w')
 csvwriter = csv.DictWriter(output, delimiter=',', fieldnames=fieldnames)
 csvwriter.writeheader()
 for row in fileCopy:
-    csvwriter.writerow(row)
+	csvwriter.writerow(row)
 output.close()
 
