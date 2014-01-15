67e548c7d82b3ab9e90b46f845921af79fe10348
Matt Sevrens <msevrens@gmail.com>
2014-01-14 20:34:30 -0800

    git thinks all module level variables are constants

--- src/merge_results.py
+++ src/merge_results.py
@@ -1,34 +1,35 @@
 #!/usr/local/bin/python3
+# pylint: disable=C0103
 
 """This script merges a new set of labeled
  transactions into our existing set."""
 
 import csv, sys
 
-FILE_A = open(sys.argv[1])
-FILE_B = open("../labeledTrans.csv")
-DICT_A = csv.DictReader(FILE_A)
-DICT_B = csv.DictReader(FILE_B)
-FILE_COPY = []
-FIELD_NAMES = DICT_B.fieldnames
+file_a = open(sys.argv[1])
+file_b = open("../labeledTrans.csv")
+dict_a = csv.DictReader(file_a)
+dict_b = csv.DictReader(file_b)
+file_copy = []
+field_names = dict_b.fieldnames
 
 # Copy
-for lRow in DICT_B:
-	FILE_COPY.append(lRow)
+for lRow in dict_b:
+	file_copy.append(lRow)
 
 # Merge
-for row in DICT_A:
-	for lRow in FILE_COPY:
+for row in dict_a:
+	for lRow in file_copy:
 		if (row['DESCRIPTION'] == lRow['DESCRIPTION']
 			and lRow['PERSISTENTRECORDID'] == ""):
 			lRow['PERSISTENTRECORDID'] = row['PERSISTENTRECORDID']
 			break
 
 # Write
-OUTPUT = open('output.csv', 'w')
-CSV_WRITER = csv.DictWriter(OUTPUT, delimiter=',', fieldnames=FIELD_NAMES)
-CSV_WRITER.writeheader()
-for row in FILE_COPY:
-	CSV_WRITER.writerow(row)
-OUTPUT.close()
+output = open('output.csv', 'w')
+csv_writer = csv.DictWriter(output, delimiter=',', fieldnames=field_names)
+csv_writer.writeheader()
+for row in file_copy:
+	csv_writer.writerow(row)
+output.close()
 
