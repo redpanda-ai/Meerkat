17d02e9891573806bfe4e8ee39b3db0a90c415ca
Matt Sevrens <msevrens@gmail.com>
2014-01-23 15:32:35 -0800

    making test_accuracy work as a function or a file

--- longtail/test_accuracy.py
+++ longtail/test_accuracy.py
@@ -6,51 +6,55 @@
 
 import csv, sys, math
 
-machine_labeled = open(sys.argv[1])
-human_labeled = open("../data/verifiedLabeledTrans.csv")
-dict_ML = csv.DictReader(machine_labeled)
-dict_HL = csv.DictReader(human_labeled)
+def test_accuracy(file_path):
 
-file_copy = []
-needs_hand_labeling = []
-non_physical = []
-mislabeled = []
+	human_labeled = open("../data/verifiedLabeledTrans.csv") if __name__ == "__main__" else file_path
+	machine_labeled = open(sys.argv[1])
+	dict_ML = csv.DictReader(machine_labeled)
+	dict_HL = csv.DictReader(human_labeled)
 
-total = 0
-correct = 0
+	file_copy = []
+	needs_hand_labeling = []
+	non_physical = []
+	mislabeled = []
 
-# Copy
-for hlRow in dict_HL:
-	file_copy.append(hlRow)
+	total = 0
+	correct = 0
 
-# Count
-num_labeled = len(file_copy)
-for mlRow in dict_ML:
-	total += 1
-	for index, hlRow in enumerate(file_copy):
-		if mlRow['DESCRIPTION'] == hlRow['DESCRIPTION']:
-			if mlRow['PERSISTENTRECORDID'] == hlRow['PERSISTENTRECORDID']:
-				# Transaction was correctly labeled
-				correct += 1
-				break
-			elif hlRow['IS_PHYSICAL_TRANSACTION'] == '0':
-				# Transaction is non physical
-				non_physical.append(mlRow['DESCRIPTION'])
-				break
-			elif hlRow['PERSISTENTRECORDID'] == "":
-				# Transaction is not yet labeled
+	# Copy
+	for hlRow in dict_HL:
+		file_copy.append(hlRow)
+
+	# Count
+	num_labeled = len(file_copy)
+	for mlRow in dict_ML:
+		total += 1
+		for index, hlRow in enumerate(file_copy):
+			if mlRow['DESCRIPTION'] == hlRow['DESCRIPTION']:
+				if mlRow['PERSISTENTRECORDID'] == hlRow['PERSISTENTRECORDID']:
+					# Transaction was correctly labeled
+					correct += 1
+					break
+				elif hlRow['IS_PHYSICAL_TRANSACTION'] == '0':
+					# Transaction is non physical
+					non_physical.append(mlRow['DESCRIPTION'])
+					break
+				elif hlRow['PERSISTENTRECORDID'] == "":
+					# Transaction is not yet labeled
+					needs_hand_labeling.append(mlRow['DESCRIPTION'])
+					break
+				else:
+					# Transaction is mislabeled
+					mislabeled.append(hlRow['DESCRIPTION'] + " - " + hlRow['PERSISTENTRECORDID'])
+					break
+			if index + 1 == num_labeled:
 				needs_hand_labeling.append(mlRow['DESCRIPTION'])
-				break
-			else:
-				# Transaction is mislabeled
-				mislabeled.append(hlRow['DESCRIPTION'] + " - " + hlRow['PERSISTENTRECORDID'])
-				break
-		if index + 1 == num_labeled:
-			needs_hand_labeling.append(mlRow['DESCRIPTION'])
 
-print("STATS:")
-print("Accuracy = " + str(math.ceil(correct/(total-len(needs_hand_labeling)) * 100)) + "%")
-print("Not Found = " + str(math.ceil(len(needs_hand_labeling)/total * 100)) + "%", '\n')
-print("NEEDS HAND LABELING:", '\n'.join(needs_hand_labeling), sep="\n")
-print("", "MISLABELED:", '\n'.join(mislabeled), sep="\n")
+	print("STATS:")
+	print("Accuracy = " + str(math.ceil(correct/(total-len(needs_hand_labeling)) * 100)) + "%")
+	print("Not Found = " + str(math.ceil(len(needs_hand_labeling)/total * 100)) + "%", '\n')
+	print("NEEDS HAND LABELING:", '\n'.join(needs_hand_labeling), sep="\n")
+	print("", "MISLABELED:", '\n'.join(mislabeled), sep="\n")
 
+if __name__ == "__main__":
+	test_accuracy("../data/verifiedLabeledTrans.csv")
