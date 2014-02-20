5eb1b279d963ea0e20b7d4ebf193a888a605d7ee
Matt Sevrens <msevrens@gmail.com>
2014-02-19 16:22:36 -0800

    improving accuracy output, first pass

--- longtail/accuracy.py
+++ longtail/accuracy.py
@@ -8,63 +8,84 @@ import csv, sys, math
 
 def test_accuracy(file_path):
 	"""Docstring to be determined."""
-	human_labeled = open("data/misc/verifiedLabeledTrans.csv") if __name__ == "__main__" else file_path
-	machine_labeled = open(sys.argv[1])
-	dict_ML = csv.DictReader(machine_labeled)
-	dict_HL = csv.DictReader(human_labeled)
 
-	file_copy = []
+	HL_file = open("data/misc/verifiedLabeledTrans.csv")
+	ML_file = open(file_path)
+	machine_labeled = list(csv.DictReader(ML_file))
+	human_labeled = list(csv.DictReader(HL_file))
+
 	needs_hand_labeling = []
 	non_physical = []
 	mislabeled = []
 	unlabeled = []
 
-	total = 0
-	correct = 0
-	found_in_verified = 0
+	total = len(machine_labeled)
+	correct = []
+	num_found = 0
 
-	# Copy
-	for hlRow in dict_HL:
-		file_copy.append(hlRow)
+	# Test Recall / Precision
+	for mlRow in machine_labeled:
 
-	# Count
-	num_labeled = len(file_copy)
-	for mlRow in dict_ML:
-		total += 1
-		for index, hlRow in enumerate(file_copy):
+		# Our confidence was not high enough to label
+		if mlRow['PERSISTENTRECORDID'] == "":
+			unlabeled.append(mlRow['DESCRIPTION'])
+			continue
+
+		# Verify against human labeled
+		for index, hlRow in enumerate(human_labeled):
 			if mlRow['DESCRIPTION'] == hlRow['DESCRIPTION']:
-				found_in_verified += 1
-				if mlRow['PERSISTENTRECORDID'] == hlRow['PERSISTENTRECORDID']:
+				if hlRow['PERSISTENTRECORDID'] == "":
+					# Transaction is not yet labeled
+					needs_hand_labeling.append(mlRow['DESCRIPTION'])
+					break
+				elif mlRow['PERSISTENTRECORDID'] == hlRow['PERSISTENTRECORDID']:
 					# Transaction was correctly labeled
-					correct += 1
+					correct.append(hlRow)
 					break
 				elif hlRow['IS_PHYSICAL_TRANSACTION'] == '0':
 					# Transaction is non physical
 					non_physical.append(mlRow['DESCRIPTION'])
 					break
-				elif hlRow['PERSISTENTRECORDID'] == "":
-					# Transaction is not yet labeled
-					needs_hand_labeling.append(mlRow['DESCRIPTION'])
-					break
-				elif mlRow['PERSISTENTRECORDID'] == "":
-					# Our confidence was not high enough to label
-					unlabeled.append(mlRow['DESCRIPTION'])
-					break
 				else:
 					# Transaction is mislabeled
 					mislabeled.append(hlRow['DESCRIPTION'] + " - " + hlRow['PERSISTENTRECORDID'])
 					break
-			if index + 1 == num_labeled:
+			elif index + 1 == len(human_labeled):
 				needs_hand_labeling.append(mlRow['DESCRIPTION'])
 
-	print("STATS:")
-	print("Accuracy = " + str(math.ceil(correct/(total-len(needs_hand_labeling)) * 100)) + "%")
-	print("Not Found in hand verified list = " + str(math.ceil(len(needs_hand_labeling)/total * 100)) + "%")
-	print("Incorrect binary classification = " + str(math.ceil(len(non_physical)/found_in_verified * 100)) + "%", '\n')
-	print("NEEDS HAND LABELING:", '\n'.join(needs_hand_labeling), sep="\n")
-	print("UNLABELED:", '\n'.join(unlabeled), sep="\n")
-	print("", "MISLABELED:", '\n'.join(mislabeled), sep="\n")
-	print("", "NON_PHYSICAL:", '\n'.join(non_physical), sep="\n")
+	# Test Binary
+	for i, item in enumerate(unlabeled):
+		for index, hlRow in enumerate(human_labeled):
+			if item == hlRow['DESCRIPTION']:
+				if hlRow['IS_PHYSICAL_TRANSACTION'] == '0':
+					# Transaction is non physical
+					non_physical.append(item)
+					break
+
+
+	num_labeled = total - len(unlabeled)
+	num_verified = num_labeled - len(needs_hand_labeling)
+
+	results = {}
+	results['total'] = total
+	results['correct'] = correct
+	results['needs_hand_labeling'] = needs_hand_labeling
+	results['non_physical'] = non_physical
+	results['unlabeled'] = unlabeled
+	results['mislabeled'] = mislabeled
+	results['recall'] = math.ceil((num_labeled / total) * 100)
+	results['precision'] = math.ceil((len(correct) / num_verified) * 100)
+	results['incorrect_binary'] = math.ceil((len(non_physical) / total) * 100)
+
+	return results		
 
 if __name__ == "__main__":
-	test_accuracy("data/misc/verifiedLabeledTrans.csv")
+
+	file_path = sys.argv[1] if len(sys.argv) > 1 else "data/output/longtailLabeled.csv"
+	results = test_accuracy(file_path)
+
+	print("STATS:")
+	print("Recall = " + str(results['recall']) + "%")
+	print("Precision = " + str(results['precision']) + "%")
+	print("Binary Classifier Accuracy = " + str(100 - results['incorrect_binary']) + "%")
+	print("", "MISLABELED:", '\n'.join(results['mislabeled']), sep="\n")
