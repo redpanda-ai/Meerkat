abea91423894381b6b24c81151a6b4ea3287776f
Matt Sevrens <msevrens@gmail.com>
2014-02-26 14:46:24 -0800

    adding doc strings

--- longtail/optimization.py
+++ longtail/optimization.py
@@ -4,7 +4,9 @@
 """This module performs hyperparameter optimization. This involves
 tuning the keys located under Longtail/config/keys. Those key value
 pairs map to hyperparameters used through out the Longtail Classifier.
-This module utilizes a common method known as grid search"""
+This module utilizes a common method known as grid search. In particular
+we are using randomized optimization as it works better where it
+is resource intensive to exaustively perform a standard grid_search"""
 
 from longtail.description_producer import initialize, get_desc_queue, tokenize, load_parameter_key
 
@@ -13,6 +15,11 @@ from scipy.stats import randint, uniform
 from sklearn.grid_search import RandomizedSearchCV
 
 def randomized_optimization():
+
+	"""Generates randomized parameter keys by
+	providing a range and distribution to sample from. 
+	Runs the classifier a fixed number of times and
+	provides the top score found"""
 
 	#Runs the entire program.
 	params = initialize()
