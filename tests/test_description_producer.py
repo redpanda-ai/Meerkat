9d65397667abf4e57933fd487d5cc935753a453b
Matt Sevrens <msevrens@gmail.com>
2014-06-12 13:02:52 -0700

    beginning to clean up unit tests

--- tests/test_description_producer.py
+++ tests/test_description_producer.py
@@ -1,4 +1,4 @@
-'''Unit tests for meerkat.description_producer'''
+"""Unit tests for meerkat.description_producer"""
 
 from meerkat import description_producer
 from meerkat.custom_exceptions import InvalidArguments, Misconfiguration
@@ -8,137 +8,136 @@ class TokenizeDescriptionTests(unittest.TestCase):
 
 	"""Our UnitTest class."""
 
-	config = """
-{
-	"concurrency" : 1,
-	"input" : {
-		"hyperparameters" : "config/hyperparameters/made_up_key_name.json",
-		"filename" : "data/input/100_bank_transaction_descriptions.csv",
-		"delimiter" : ",",
-		"encoding" : "utf-8"
-	},
-	"logging" : {
-		"level" : "critical",
-		"path" : "logs/foo.log",
-		"formatter" : "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-		"console" : true
-	},
-	"output" : {
-		"results" : {
-            "fields" : ["name", "chain_id", "category_labels", "address", "locality", "region", "postcode", "website", "tel", "chain_name", "latitude", "longitude", "neighborhood", "factual_id"],
-            "labels" : ["BUSINESS_NAME", "STORE_ID", "FACTUAL_CATEGORY", "STREET", "CITY", "STATE", "ZIP_CODE", "WEBSITE", "PHONE_NUMBER", "CHAIN_NAME", "LATITUDE", "LONGITUDE", "NEIGHBORHOOD", "FACTUAL_ID"]
-        },
-		"file" : {
-			"format" : "csv",
-			"path" : "data/input/unittestDeletable.csv"
-		}
-	},
-	"elasticsearch" : {
-		"cluster_nodes" : [
-		    "s01:9200",
-		    "s02:9200",
-		    "s03:9200",
-		    "s04:9200",
-		    "s05:9200",
-		    "s06:9200",
-		    "s07:9200",
-		    "s08:9200",
-		    "s09:9200",
-		    "s10:9200",
-		    "s11:9200",
-		    "s12:9200",
-		    "s13:9200",
-		    "s14:9200",
-		    "s15:9200",
-		    "s16:9200",
-		    "s17:9200",
-		    "s18:9200"
-    	],
-		"index" : "new_index",
-		"type" : "new_type",
-		"subqueries" : {
-			"largest_matching_string": {
-				"field_boosts" : "standard_fields",
-				"query_type" : "qs_query"
+	config = """{
+		"concurrency" : 1,
+		"input" : {
+			"hyperparameters" : "config/hyperparameters/made_up_key_name.json",
+			"filename" : "data/input/100_bank_transaction_descriptions.csv",
+			"delimiter" : ",",
+			"encoding" : "utf-8"
+		},
+		"logging" : {
+			"level" : "critical",
+			"path" : "logs/foo.log",
+			"formatter" : "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+			"console" : true
+		},
+		"output" : {
+			"results" : {
+	            "fields" : ["name", "chain_id", "category_labels", "address", "locality", "region", "postcode", "website", "tel", "chain_name", "latitude", "longitude", "neighborhood", "factual_id"],
+	            "labels" : ["BUSINESS_NAME", "STORE_ID", "FACTUAL_CATEGORY", "STREET", "CITY", "STATE", "ZIP_CODE", "WEBSITE", "PHONE_NUMBER", "CHAIN_NAME", "LATITUDE", "LONGITUDE", "NEIGHBORHOOD", "FACTUAL_ID"]
+	        },
+			"file" : {
+				"format" : "csv",
+				"path" : "data/input/unittestDeletable.csv"
 			}
 		},
-		"boost_labels" : [ "standard_fields", "composite.address" ],
-		"boost_vectors" : {
-			"factual_id" :        [ 0.0, 0.0 ],
-			"name" :              [ 1.0, 0.0 ],
-			"address" :           [ 1.0, 0.0 ],
-			"address_extended" :  [ 1.0, 0.0 ],
-			"po_box" :            [ 1.0, 0.0 ],
-			"locality" :          [ 1.0, 0.0 ],
-			"region" :            [ 1.0, 0.0 ],
-			"post_town" :         [ 1.0, 0.0 ],
-			"admin_region" :      [ 1.0, 0.0 ],
-			"postcode" :          [ 1.0, 0.0 ],
-			"country" :           [ 1.0, 0.0 ],
-			"tel" :               [ 1.0, 0.0 ],
-			"fax" :               [ 1.0, 0.0 ],
-			"neighborhood" :      [ 1.0, 0.0 ],
-			"email" :             [ 1.0, 0.0 ],
-			"category_ids" :      [ 1.0, 0.0 ],
-			"category_labels" :   [ 1.0, 0.0 ],
-			"status" :            [ 1.0, 0.0 ],
-			"chain_name" :        [ 1.0, 0.0 ],
-			"chain_id" :          [ 1.0, 0.0 ],
-			"pin.location" :      [ 0.0, 0.0 ],
-			"composite.address" : [ 0.0, 3.0 ]
-		},
-		"composite_fields" : [
-			{
-				"address" : {
-					"components" : [ "address", "address_extended", "locality", "region",
-						"postcode", "country" ],
-					"format" : "{0} {1} {2}, {3} {4}, {5}",
-					"index" : "analyzed",
-					"type" : "string"
+		"elasticsearch" : {
+			"cluster_nodes" : [
+			    "s01:9200",
+			    "s02:9200",
+			    "s03:9200",
+			    "s04:9200",
+			    "s05:9200",
+			    "s06:9200",
+			    "s07:9200",
+			    "s08:9200",
+			    "s09:9200",
+			    "s10:9200",
+			    "s11:9200",
+			    "s12:9200",
+			    "s13:9200",
+			    "s14:9200",
+			    "s15:9200",
+			    "s16:9200",
+			    "s17:9200",
+			    "s18:9200"
+	    	],
+			"index" : "new_index",
+			"type" : "new_type",
+			"subqueries" : {
+				"largest_matching_string": {
+					"field_boosts" : "standard_fields",
+					"query_type" : "qs_query"
 				}
-			}
-		],
-		"type_mapping" : {
-			"mappings" : {
-				"factual_type" : {
-					"_source" : {
-						"enabled" : true
-					},
-				"properties" : {
-					"factual_id" : { "index" : "analyzed", "type" : "string" },
-					"name" : { "index" : "analyzed", "type" : "string" },
-					"address" : { "index" : "analyzed", "type" : "string" },
-					"address_extended" : { "index" : "analyzed", "type" : "string" },
-					"po_box" : { "index" : "analyzed", "type" : "string" },
-					"locality" : { "index" : "analyzed", "type" : "string" },
-					"region" : { "index" : "analyzed", "type" : "string" },
-					"post_town" : { "index" : "analyzed", "type" : "string" },
-					"admin_region" : { "index" : "analyzed", "type" : "string" },
-					"postcode" : { "index" : "analyzed", "type" : "string" },
-					"country" : { "index" : "analyzed", "type" : "string" },
-					"tel" : { "index" : "analyzed", "type" : "string" },
-					"fax" : { "index" : "analyzed", "type" : "string" },
-					"neighborhood" : { "index" : "analyzed", "type" : "string" },
-					"website" : { "index" : "analyzed", "type" : "string" },
-					"email" : { "index" : "analyzed", "type" : "string" },
-					"category_ids" : { "index" : "analyzed", "type" : "string" },
-					"category_labels" : { "index" : "analyzed", "type" : "string" },
-					"status" : { "index" : "analyzed", "type" : "string" },
-					"chain_name" : { "index" : "analyzed", "type" : "string" },
-					"chain_id" : { "index" : "analyzed", "type" : "string" },
-					"pin" : { "properties" : { "location" : {
-						"type" : "geo_shape", "tree" : "quadtree", "precision" : "1m" } }
+			},
+			"boost_labels" : [ "standard_fields", "composite.address" ],
+			"boost_vectors" : {
+				"factual_id" :        [ 0.0, 0.0 ],
+				"name" :              [ 1.0, 0.0 ],
+				"address" :           [ 1.0, 0.0 ],
+				"address_extended" :  [ 1.0, 0.0 ],
+				"po_box" :            [ 1.0, 0.0 ],
+				"locality" :          [ 1.0, 0.0 ],
+				"region" :            [ 1.0, 0.0 ],
+				"post_town" :         [ 1.0, 0.0 ],
+				"admin_region" :      [ 1.0, 0.0 ],
+				"postcode" :          [ 1.0, 0.0 ],
+				"country" :           [ 1.0, 0.0 ],
+				"tel" :               [ 1.0, 0.0 ],
+				"fax" :               [ 1.0, 0.0 ],
+				"neighborhood" :      [ 1.0, 0.0 ],
+				"email" :             [ 1.0, 0.0 ],
+				"category_ids" :      [ 1.0, 0.0 ],
+				"category_labels" :   [ 1.0, 0.0 ],
+				"status" :            [ 1.0, 0.0 ],
+				"chain_name" :        [ 1.0, 0.0 ],
+				"chain_id" :          [ 1.0, 0.0 ],
+				"pin.location" :      [ 0.0, 0.0 ],
+				"composite.address" : [ 0.0, 3.0 ]
+			},
+			"composite_fields" : [
+				{
+					"address" : {
+						"components" : [ "address", "address_extended", "locality", "region",
+							"postcode", "country" ],
+						"format" : "{0} {1} {2}, {3} {4}, {5}",
+						"index" : "analyzed",
+						"type" : "string"
 					}
 				}
+			],
+			"type_mapping" : {
+				"mappings" : {
+					"factual_type" : {
+						"_source" : {
+							"enabled" : true
+						},
+						"properties" : {
+							"factual_id" : { "index" : "analyzed", "type" : "string" },
+							"name" : { "index" : "analyzed", "type" : "string" },
+							"address" : { "index" : "analyzed", "type" : "string" },
+							"address_extended" : { "index" : "analyzed", "type" : "string" },
+							"po_box" : { "index" : "analyzed", "type" : "string" },
+							"locality" : { "index" : "analyzed", "type" : "string" },
+							"region" : { "index" : "analyzed", "type" : "string" },
+							"post_town" : { "index" : "analyzed", "type" : "string" },
+							"admin_region" : { "index" : "analyzed", "type" : "string" },
+							"postcode" : { "index" : "analyzed", "type" : "string" },
+							"country" : { "index" : "analyzed", "type" : "string" },
+							"tel" : { "index" : "analyzed", "type" : "string" },
+							"fax" : { "index" : "analyzed", "type" : "string" },
+							"neighborhood" : { "index" : "analyzed", "type" : "string" },
+							"website" : { "index" : "analyzed", "type" : "string" },
+							"email" : { "index" : "analyzed", "type" : "string" },
+							"category_ids" : { "index" : "analyzed", "type" : "string" },
+							"category_labels" : { "index" : "analyzed", "type" : "string" },
+							"status" : { "index" : "analyzed", "type" : "string" },
+							"chain_name" : { "index" : "analyzed", "type" : "string" },
+							"chain_id" : { "index" : "analyzed", "type" : "string" },
+							"pin" : { "properties" : { "location" : {
+								"type" : "geo_shape", "tree" : "quadtree", "precision" : "1m" } }
+							}
+						}
+					}
+				},
+				"settings" : {
+					"number_of_replicas" : 1,
+					"number_of_shards" : 12
+				}
 			}
-		},
-		"settings" : {
-			"number_of_replicas" : 1,
-			"number_of_shards" : 12
 		}
-		}
-	}
-}"""
+	}"""
 
 
 	def setUp(self):
