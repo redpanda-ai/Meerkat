14bc7fab2af9200d754b5ce43c53f7ffe25fc429
J. Andrew Key <joeandrewkey@gmail.com>
2014-07-21 10:14:31 +0000

    Adding a special decorators module to help clean up our code.

---
+++ meerkat/decorators.py
@@ -0,0 +1,86 @@
+"""This module demonstrates decorators."""
+import time
+from functools import wraps
+
+def timethis(func):
+	"""Decorator that reports execution time."""
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		"""Wrapper for timethis"""
+		start = time.time()
+		result = func(*args, **kwargs)
+		end = time.time()
+		print(func.__name__, end-start)
+		return result
+	return wrapper
+
+def list_arguments(func):
+	"""This decorator lists args and kwargs."""
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		"""Another thing."""
+		print("Something for before.")
+		print("Args")
+		for arg in args:
+			print(arg)
+		print("Kwargs")
+		for key in kwargs:
+			print("%s, %s" % (key, kwargs[key]))
+		result = func(*args, **kwargs)
+		print("Something for after.")
+		return result
+	return wrapper
+
+def params(named=None, unnamed=None):
+	"""Decorator that takes arguments"""
+	if named is None:
+		named = []
+	if unnamed is None:
+		unnamed = []
+	def decorate(func):
+		"""Innder decorator that accepts a function."""
+		@wraps(func)
+		def wrapper(*args, **kwargs):
+			"""A wrapper."""
+			#Check named arguments
+			for key in named:
+				if key not in kwargs:
+					raise Exception("Missing %s" % (key))
+			#Check unnamed arguments
+			len_expected, len_actual = len(unnamed), len(args)
+			if len_actual != len_expected:
+				raise Exception("Expecting %d arguments but found %d arguments"\
+					% (len_expected, len_actual))
+			return func(*args, **kwargs)
+		return wrapper
+	return decorate
+
+#@list_arguments
+@timethis
+@list_arguments
+@params(named=["a", "b"], unnamed=["c"])
+def boo(*args, **kwargs):
+	"""This function uses the 'params' decorator"""
+	print("a is %s" % (kwargs["a"]))
+	print("b is %s" % (kwargs["b"]))
+	for arg in args:
+		print(arg)
+
+boo(1, a=1, b=2)
+
+@params(named=["a", "b"])
+def doo(*args, **kwargs):
+	"""This function also uses the 'params' decorator"""
+	print("a is %s" % (kwargs["a"]))
+	print("b is %s" % (kwargs["b"]))
+	for arg in args:
+		print(arg)
+
+doo(a=3, b=4)
+
+@params()
+def goo():
+	"""This function also uses the 'params' decorator"""
+	print("Foo doesn't do much.")
+
+goo()
