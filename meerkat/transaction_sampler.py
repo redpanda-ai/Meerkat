2fbc6e4c6f3036f2d3d1c2f4534074e88bbe267e
J. Andrew Key <joeandrewkey@gmail.com>
2014-07-22 07:16:08 +0000

    Cleaning pylint in transaction_sampler module.

--- meerkat/transaction_sampler.py
+++ meerkat/transaction_sampler.py
@@ -1,3 +1,4 @@
+"""This module creates a random sample from a large group of gzipped files."""
 #!/bin/python3.3
 
 import glob
@@ -8,12 +9,13 @@ import sys
 from random import shuffle
 
 def clean_line(line):
+	"""Trims binary junk from a gzipped line"""
 	return str(line)[2:-3]
 
-def bucket_me(input_file, d):
+def bucket_me(input_file, shuffle_dict):
+	"""Buckets an input file, placing hte result in a dictionary"""
 	#print("Processing {0}".format(input_file))
 	logging.critical("Processing %s", input_file)
-	unzipped_input = None
 	count = 0
 	with gzip.open(input_file, "rb") as gzipped_input:
 		is_first_line = True
@@ -23,35 +25,34 @@ def bucket_me(input_file, d):
 				sys.stdout.write(".")
 				sys.stdout.flush()
 			line = clean_line(line)
-			#print(line)
 			if is_first_line:
 				header = "SHUFFLE_ID|" + line
 				is_first_line = False
 				continue
-			x = line.split("|")
-			if x[0] not in d:
-				d[x[0]] = 1
-			else:
-				d[x[0]] += 1
-	make_histogram(d)
+			shuffle_id = line.split("|")[0]
+			if shuffle_id not in shuffle_dict:
+				shuffle_dict[shuffle_id] = 0
+			shuffle_dict[shuffle_id] += 1
+	make_histogram(shuffle_dict)
 	return header
 
 def get_header(input_file):
+	"""Builds a header from the first line in a file."""
 	logging.critical("Getting header from %s", input_file)
 	with gzip.open(input_file, "rb") as gzipped_input:
-		is_first_line = True
 		for line in gzipped_input:
 			line = clean_line(line)
 			header = "SHUFFLE_ID|" + line
 			return header
 
-def make_histogram(d):
+def make_histogram(shuffle_dict):
+	"""Creates a histogram of transaction buckets from the dictionary"""
 	buckets = {}
-	for key in d:
-		if d[key] not in buckets:
-			buckets[d[key]] = [ key ]
+	for key in shuffle_dict:
+		if shuffle_dict[key] not in buckets:
+			buckets[shuffle_dict[key]] = [key]
 		else:
-			buckets[d[key]].append(key)
+			buckets[shuffle_dict[key]].append(key)
 
 	total = 0
 	for key in buckets:
@@ -59,9 +60,11 @@ def make_histogram(d):
 
 	logging.critical("\nBucket # - Members in Bucket [percent of total]")
 	for sorted_key in sorted(buckets.keys()):
-		logging.critical("%d - %d [%.2f]" % (sorted_key, len(buckets[sorted_key]), len(buckets[sorted_key]) * 100 / total ))
+		logging.critical("%d - %d [%.2f]" % (sorted_key,\
+			len(buckets[sorted_key]), len(buckets[sorted_key]) * 100 / total ))
 
 def filter_me(input_file, y):
+	"""Filters the a gzipped input file."""
 	logging.critical("Processing %s", input_file)
 	with gzip.open(input_file, "rb") as gzipped_input:
 		count = 0
@@ -71,49 +74,42 @@ def filter_me(input_file, y):
 				sys.stdout.write(".")
 				sys.stdout.flush()
 			line = clean_line(line)
-			x = line.split("|")
-			key = x[0]
+			line_tokens = line.split("|")
+			key = line_tokens[0]
 			if key in y:
 				y[key].append(line)
 
-input_path = sys.argv[1]
-os.chdir(input_path)
-input_files = sorted(glob.glob('*.gz'))
+def start(input_path):
+	os.chdir(input_path)
+	input_files = sorted(glob.glob('*.gz'))
 
-d, y = {}, {}
-header = get_header(input_files[0])
-#print(header)
-#print(input_files)
-[bucket_me(x, d) for x in input_files]
+	shuffle_dict, y = {}, {}
+	header = get_header(input_files[0])
 
-#Could add a filter here to remove where d.keys where number of records < 25
-#z = [ a for a in d.keys() if d[a] >= 25 ]
-#all_members = z
+	[bucket_me(input_file, shuffle_dict) for input_file in input_files]
 
-all_members = list(d.keys())
-len_all_members = len(all_members)
+	all_members = list(shuffle_dict.keys())
 
-x = [all_members[i] for i in range(len_all_members)]
-logging.critical("Shuffling")
-shuffle(x)
+	shuffle_list = [all_members[i] for i in range(len(all_members))]
+	logging.critical("Shuffling")
+	shuffle(shuffle_list)
 
-sample_size_in_members = 20000
-x = x[:sample_size_in_members]
+	sample_size_in_members = 20000
+	shuffle_list = shuffle_list[:sample_size_in_members]
+	for item in shuffle_list:
+		y[item] = []
+	[filter_me(z, y) for z in input_files]
+	with open("outfile", "w") as outfile:
+		outfile.write(header + "\n")
+		for c in range(len(shuffle_list)):
+			count = 0
+			for item in y[shuffle_list[c]]:
+				count += 1
+				if count % 20000 == 0:
+					sys.stdout.write(".")
+					sys.stdout.flush()
+				line = str(c) + "|" + item + "\n"
+				outfile.write(line)
 
-for item in x:
-	y[item] = []
-
-[filter_me(z, y) for z in input_files]
-
-with open("outfile", "w") as outfile:
-	outfile.write(header + "\n")
-	for c in range(len(x)):
-		count = 0
-		for item in y[x[c]]:
-			count += 1
-			if count % 20000 == 0:
-				sys.stdout.write(".")
-				sys.stdout.flush()
-			line = str(c) + "|" + item + "\n"
-			outfile.write(line)
-			#print("%d|%s" % (c,item))
+#Main program
+start(sys.argv[1])
