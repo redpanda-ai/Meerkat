223a155c294b070b8d1b04d6ac6c73e287067991
J. Andrew Key <joeandrewkey@gmail.com>
2014-09-10 05:40:31 +0000

    Updating build_topology script to use parameters.

--- scripts/build_topology.sh
+++ scripts/build_topology.sh
@@ -1,48 +1,65 @@
+prefix="n"
+node_start=21
+node_end=100
+slave_start=21
+slave_end=100
+master_start=1
+master_end=0
+echo -e "Nodes are:"
+nodes=()
+for ((a=${node_start}; a <= ${node_end} ; a++))
+do
+	nodes+=("${prefix}${a}")
+	echo -e "${prefix}${a}"
+done
+
+echo -e "Slaves are:"
+slaves=()
+for ((b=${slave_start}; b <= ${slave_end} ; b++))
+do
+	slaves+=("${prefix}${b}")
+	echo -e "${prefix}${b}"
+done
+
+echo -e "Masters are:"
+masters=()
+for ((c=${master_start}; c <= ${master_end} ; c++))
+do
+	masters+=("${prefix}${c}")
+	echo -e "${prefix}${c}"
+done
+
+KEY="/root/.ssh/meerkat.pem"
+
 echo -e "Blanking cluster"
-./scp_all.sh /etc/elasticsearch/elasticsearch.yml 
+for n in "${nodes[@]}"
+do
+	scp -i ${KEY} /etc/elasticsearch/elasticsearch.yml ${n}:/etc/elasticsearch/elasticsearch.yml
+done
 echo -e "Naming Nodes"
-./rename_all.sh
+for m in "${nodes[@]}"
+do
+	./rename.sh ${m}
+done
 echo -e "Setting Masters"
-./x_master.sh n01
-./x_master.sh n02
+for i in "${masters[@]}"
+do
+	./y_slave.sh ${i}
+done
 echo -e "Setting Slaves"
-./y_slave.sh n03
-./y_slave.sh n04
-./y_slave.sh n05
-./y_slave.sh n06
-./y_slave.sh n07
-./y_slave.sh n08
-./y_slave.sh n09
-./y_slave.sh n10
-./y_slave.sh n11
-./y_slave.sh n12
-./y_slave.sh n13
-./y_slave.sh n14
-./y_slave.sh n15
-./y_slave.sh n16
-./y_slave.sh n17
-./y_slave.sh n18
-./y_slave.sh n19
-./y_slave.sh n20
+for j in "${slaves[@]}"
+do
+	./y_slave.sh ${j}
+done
 echo -e "Mounting EBS to /data"
-./prep_data.sh n01
-./prep_data.sh n02
-./prep_data.sh n03
-./prep_data.sh n04
-./prep_data.sh n05
-./prep_data.sh n06
-./prep_data.sh n07
-./prep_data.sh n08
-./prep_data.sh n09
-./prep_data.sh n10
-./prep_data.sh n11
-./prep_data.sh n12
-./prep_data.sh n13
-./prep_data.sh n14
-./prep_data.sh n15
-./prep_data.sh n16
-./prep_data.sh n17
-./prep_data.sh n18
-./prep_data.sh n19
-./prep_data.sh n20
+for k in "${nodes[@]}"
+do
+	./prep_data.sh ${k}
+done
+echo -e "Mounting EBS to /data"
+for p in "${nodes[@]}"
+do
+	ssh -i ${KEY} ${p} "service elasticsearch start"
+done
+
 echo -e "Complete."
