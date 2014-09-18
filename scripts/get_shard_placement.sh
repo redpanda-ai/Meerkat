ee245b36373aafcc48b53332e0fda2a2b782c35a
J. Andrew Key <joeandrewkey@gmail.com>
2014-09-18 17:19:22 +0000

    Updating production infrastructure scripts to be easier.

--- scripts/get_shard_placement.sh
+++ scripts/get_shard_placement.sh
@@ -1,6 +1,8 @@
-prefix="n"
+prefix="x"
 node_start=10001
-node_end=10039
+node_end=10526
+
+
 
 #echo -e "Nodes are:"
 nodes=()
@@ -10,52 +12,11 @@ do
 	#echo -e "${prefix}${a}"
 done
 
-KEY="/root/.ssh/meerkat.pem"
+SSH_OPTIONS="-i /root/.ssh/meerkat.pem -o StrictHostKeyChecking=no"
 echo -e "Inspecting"
 for n in "${nodes[@]}"
 do
 	echo -e "${n}"
-	ssh -i ${KEY} ${n} "ls -FalR /data" | grep "factual_index\/[0-9]*\:" | sed -e 's/.*\/\(.*\):/\1/' | sort -n | uniq
+	ssh ${SSH_OPTIONS} ${n} "ls -FalR /data" | grep "factual_index\/[0-9]*\:" | sed -e 's/.*\/\(.*\):/\1/' | sort -n | uniq
 done
-
-prefix="b"
-node_start=10001
-node_end=10068
-
-#echo -e "Nodes are:"
-nodes=()
-for ((a=${node_start}; a <= ${node_end} ; a++))
-do
-	nodes+=("${prefix}${a}")
-	#echo -e "${prefix}${a}"
-done
-
-KEY="/root/.ssh/meerkat.pem"
-#echo -e "Inspecting"
-for n in "${nodes[@]}"
-do
-	echo -e "${n}"
-	ssh -i ${KEY} ${n} "ls -FalR /data" | grep "factual_index\/[0-9]*\:" | sed -e 's/.*\/\(.*\):/\1/' | sort -n | uniq
-done
-
-prefix="spot"
-node_start=10001
-node_end=10699
-
-#echo -e "Nodes are:"
-nodes=()
-for ((a=${node_start}; a <= ${node_end} ; a++))
-do
-	nodes+=("${prefix}${a}")
-	#echo -e "${prefix}${a}"
-done
-
-KEY="/root/.ssh/meerkat.pem"
-#echo -e "Inspecting"
-for n in "${nodes[@]}"
-do
-	echo -e "${n}"
-	ssh -i ${KEY} ${n} "ls -FalR /data" | grep "factual_index\/[0-9]*\:" | sed -e 's/.*\/\(.*\):/\1/' | sort -n | uniq
-done
-
 
--- scripts/search_mode.sh
+++ scripts/search_mode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -XPUT ${1}:9200/_cluster/settings -d '{ "transient" : { "threadpool.search.size" : 200, "threadpool.search.queue_size": 500 } }'
+curl -XPUT ${1}:9200/_cluster/settings -d '{ "transient" : { "threadpool.search.size" : 400, "threadpool.search.queue_size": 1000 } }'
 curl -XPUT ${1}:9200/_cluster/settings -d '{ "transient" : { "threadpool.bulk.size" : 10, "threadpool.bulk.queue_size": 200 } }'
 curl -XPUT ${1}:9200/_cluster/settings -d '{ "transient" : { "threadpool.index.size" : 50, "threadpool.index.queue_size": 500 } }'
 
