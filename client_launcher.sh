2b57e3758afcee2bf6251260faa14669b985a4bd
J. Andrew Key <joeandrewkey@gmail.com>
2014-09-28 14:31:37 +0000

    Updating settings, yet again, for production.

--- client_launcher.sh
+++ client_launcher.sh
@@ -7,6 +7,6 @@ fi
 
 cat config/template.json | sed -e "s/__CONTAINER/${1}/g" | sed -e "s/__FILTER/${2}/g" > config/${3}.json
 
-time python3.3 -u -m meerkat config/${3}.json > ${3}.out 2>&1 &
+time python3.3 -u -m meerkat config/${3}.json > /data/1/input/${3}.out 2>&1 &
 #time nohup python3.3 -u -m meerkat config/${3}.json > ${3}.out 2>&1&
 
