7d00fac9a9fd4cdf7f966ec78600944a9cdab276
J. Andrew Key <joeandrewkey@gmail.com>
2014-09-29 17:05:39 +0000

    Adding script to split panel file into gzipped parts.

---
+++ scripts/splitter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tail -n +2 ${1} | split -a 3 -d -l 1000000 - ${1}.
+denom=` ls -Fal ${1}.* | awk ' { print $9 } ' | cut -d '.' -f 3 | tail -n 1 `
+for file in ${1}.*
+do
+	head -n 1 ${1} > tmp_file
+	cat $file >> tmp_file
+	mv -f tmp_file $file
+	mv -f $file $file.$denom
+	gzip $file.$denom
+done
