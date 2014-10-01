93ee175ddb73bca67dfb59d1bcdb13f09960ec46
J. Andrew Key <joeandrewkey@gmail.com>
2014-10-01 19:25:06 +0000

    Fixing error that prevented launching ES clusters of size > 1.

--- meerkat/build_stack.py
+++ meerkat/build_stack.py
@@ -12,9 +12,6 @@ from boto.ec2.connection import EC2Connection
 from boto.regioninfo import RegionInfo
 from boto.ec2.blockdevicemapping import BlockDeviceType, BlockDeviceMapping
 
-#AWS_SECRET_ACCESS_KEY="0432+/xrV6cpnc2N7F5VpJbhKhXBuxIJpYwM3bQl"
-#AWS_ACCESS_KEY_ID="AKIAIHJQDVN46QEUHU6A"
-
 def connect_to_ec2(region):
 	"""Returns a connection to EC2"""
 	try:
@@ -26,6 +23,8 @@ def connect_to_ec2(region):
 	return conn
 
 def confirm_security_groups(conn, params):
+	"""Confirms that the security groups we need for accessng our stack
+	are correctly in place"""
 	existing_groups = params["security_groups"]
 	existing_group_count = len(existing_groups)
 	security_groups = conn.get_all_security_groups()
@@ -50,6 +49,7 @@ def confirm_security_groups(conn, params):
 	params["all_security_groups"] = all_groups
 
 def create_security_group(conn, name):
+	"""Creates a new security group for our stack."""
 	try:
 		my_security_group = conn.create_security_group(name, name)
 		print("Added {0}".format(my_security_group))
@@ -61,19 +61,22 @@ def create_security_group(conn, name):
 def initialize():
 	"""Validates the command line arguments."""
 	input_file, params = None, None
-	if len(sys.argv) != 2:
-		usage()
+	if len(sys.argv) != 3:
+		print("Supply the following arguments: config_file, stack-name")
 		raise InvalidArguments(msg="Incorrect number of arguments", expr=None)
 	try:
 		input_file = open(sys.argv[1], encoding='utf-8')
 		params = json.loads(input_file.read())
 		input_file.close()
+		params["name"] = sys.argv[2]
 	except IOError:
 		logging.error("%s not found, aborting.", sys.argv[1])
 		sys.exit()
 	return params
 
 def acquire_instances(ec2_conn, params):
+	"""Requests EC2 instances from Amazon and monitors the request until
+	it is fulfilled.."""
 	print("Acquiring instances")
 	#set up block device mapping
 	bdm = BlockDeviceMapping()
@@ -114,10 +117,12 @@ def acquire_instances(ec2_conn, params):
 					j += 1
 					print("Attempt #{0} in 3 seconds.".format(j))
 					time.sleep(3)
+			#Display state for this instances
+			print("IP: {0}, ID: {1}, State: {2}".format(private_ip_address, id, state))
+			#Increment count if running
+			if state == "running":
+				count_runners += 1
 
-		print("IP: {0}, ID: {1}, State: {2}".format(private_ip_address, id, state))
-		if state == "running":
-			count_runners += 1
 		if count_runners < params["instance_count"]:
 			print("Still waiting on {0} instances...".format(params["instance_count"] - count_runners))
 			time.sleep(10)
@@ -125,6 +130,7 @@ def acquire_instances(ec2_conn, params):
 	params["instances"] = instances
 
 def map_block_devices(ec2_conn, params):
+	"""Maps block devices, such as ephemeral storage to devices on the instance."""
 	bdm = BlockDeviceMapping()
 	eph0 = BlockDeviceType(ephemeral_name = 'ephemeral0')
 	eph1 = BlockDeviceType(ephemeral_name = 'ephemeral1')
@@ -133,7 +139,8 @@ def map_block_devices(ec2_conn, params):
 	print("Map is {0}".format(bdm))
 	params["bdm"] = bdm
 
-def prep_servers_2(params):
+def send_shell_commands(params, command_set):
+	"""Sends a list of shell commands, to each instance, displaying the result."""
 	#Try multiple times to get instance update if necessary
 	max_attempts = 6
 	for j in range(0, max_attempts):
@@ -141,7 +148,7 @@ def prep_servers_2(params):
 			if j >= 0:
 				print("Making attempt {0} of {1} for ssh access.".format(j, max_attempts))
 			for instance in params["instances"]:
-				run_ssh_commands(instance.private_ip_address, params)
+				run_ssh_commands(instance.private_ip_address, params, command_set)
 			break
 			if j >= max_attempts:
 				print("Error using ssh to access instance, aborting abnormally.")
@@ -151,14 +158,8 @@ def prep_servers_2(params):
 			print("Attempt #{0} in 10 seconds.".format(j))
 			time.sleep(10)
 
-def prep_servers(params):
-	print("Waiting 60 seconds for ssh daemon.")
-	time.sleep(60)
-	params["instances"]
-	for instance in params["instances"]:
-		run_ssh_commands(instance.private_ip_address, params)
-
 def customize_config_file(params, src_file):
+	"""Duplicates a configuration file template and then customizes the duplicate."""
 	templates = params["template_files"]
 	src_file = templates["path"] + templates[src_file]
 	dst_file = src_file + "." + params["name"]
@@ -178,6 +179,8 @@ def customize_config_file(params, src_file):
 	return dst_file
 
 def configure_servers(params):
+	"""Creates all configuration files and uploads the configuration needed on each
+	instance."""
 	# __UNICAST HOSTS -> "172.31.1.191", "172.31.1.192"
 	# __MINIMUM_MASTER_NODES -> params["minimum_master_nodes"]
 	# __NODE_NAME -> params["name"] + a number
@@ -188,10 +191,11 @@ def configure_servers(params):
 	load_file = customize_config_file(params, "load")
 	merge_file = customize_config_file(params, "merge")
 
-	print("Copying configuration to hosts.")
+	print("Copying configuration template to hosts.")
 	copy_configuration_to_hosts(params, yml_file)
 
 def get_unicast_hosts(params):
+	"""Creates a list of unicast hosts, which will be 'master' nodes"""
 	result = ''
 	hosts = params["instances"][0:params["minimum_master_nodes"]]
 	for host in hosts:
@@ -211,9 +215,9 @@ def copy_configuration_to_hosts(params, dst_file):
 		sftp.put(dst_file, "/etc/elasticsearch/elasticsearch.yml")
 		ssh.close()
 
-def run_ssh_commands(instance_ip_address, params):
+def run_ssh_commands(instance_ip_address, params, command_list):
 	rsa_private_key_file = params["key_file"]
-	shell_commands = params["shell_commands"]
+	shell_commands = params[command_list]
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh.connect(instance_ip_address, username="root", key_filename=rsa_private_key_file)
@@ -222,9 +226,9 @@ def run_ssh_commands(instance_ip_address, params):
 		stdin, stdout, stderr = ssh.exec_command(item)
 		x = stdout.readlines()
 		y = stderr.readlines()
-		if x != "":
+		if str(x) != "[]":
 			print(x)
-		if y != "":
+		if str(y) != "[]":
 			print(y)
 	ssh.close()
 
@@ -235,7 +239,10 @@ def start():
 	confirm_security_groups(ec2_conn, params)
 	map_block_devices(ec2_conn, params)
 	acquire_instances(ec2_conn, params)
-	prep_servers_2(params)
+	send_shell_commands(params, "mount_data_commands")
 	configure_servers(params)
+	send_shell_commands(params, "configure_hybrid_commands")
+	#set_masters(params)
+	#set_slaves(params)
 
 start()
