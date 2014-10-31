e2534d239b6a26120a7966032957ed8da25b9151
Matt Sevrens <msevrens@gmail.com>
2014-10-30 18:02:42 -0700

    up and running

--- meerkat/web_service/__main__.py
+++ meerkat/web_service/__main__.py
@@ -6,18 +6,28 @@ import tornado.ioloop
 from tornado_json.routes import get_routes
 from tornado_json.application import Application
 
-import api
+import meerkat.web_service as api
+
+#################### USAGE ######################
+
+# python3.3 -m meerkat.web_service
+
+#################################################
 
 def main():
 
 	routes = get_routes(api)
 
-	# Create the application
-    application = Application(routes=routes, settings={})
+	print(
+		json.dumps([(url, repr(route)) for url, route in routes], indent=2)
+	)
 
-    # Start the application on port 8888
-    application.listen(8888)
-    tornado.ioloop.IOLoop.instance().start()
+	# Create the application
+	application = Application(routes=routes, settings={})
+
+	# Start the application on port 8888
+	application.listen(8888)
+	tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
     main()
--- meerkat/web_service/api.py
+++ meerkat/web_service/api.py
@@ -5,13 +5,28 @@ from tornado_json import schema
 
 class Meerkat(APIHandler):
 
+	with open("meerkat/web_service/schema_input.json") as data_file:    
+		schema_input = json.load(data_file)
+
+	with open("meerkat/web_service/example_input.json") as data_file:    
+		example_input = json.load(data_file)
+
+	with open("meerkat/web_service/schema_output.json") as data_file:    
+		schema_output = json.load(data_file)
+
+	with open("meerkat/web_service/example_output.json") as data_file:    
+		example_output = json.load(data_file)
+
 	@schema.validate(
-		input_schema = json.loads("schema_input.json"),
-		input_example = json.loads("example_input.json"),
-		output_schema = json.loads("schema_output.json"),
-		output_example = json.loads("example_output.json")
+		input_schema = schema_input,
+		input_example = example_input,
+		output_schema = schema_output,
+		output_example = example_output
 	)
 
 	def post(self):
-	"""Handle post requests"""
+		"""Handle post requests"""
 
+		return {
+
+		}
