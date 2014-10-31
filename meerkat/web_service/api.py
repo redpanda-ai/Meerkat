74a8f1bada582aae8daaf11a012aefb78e975a3f
Matt Sevrens <msevrens@gmail.com>
2014-10-30 17:15:03 -0700

    schema validation

--- meerkat/web_service/api.py
+++ meerkat/web_service/api.py
@@ -1,12 +1,17 @@
-from tornado import gen
+import json
+
 from tornado_json.requesthandlers import APIHandler
 from tornado_json import schema
 
 class Meerkat(APIHandler):
 
 	@schema.validate(
-
+		input_schema = json.loads("schema_input.json"),
+		input_example = json.loads("example_input.json"),
+		output_schema = json.loads("schema_output.json"),
+		output_example = json.loads("example_output.json")
 	)
 
 	def post(self):
 	"""Handle post requests"""
+
