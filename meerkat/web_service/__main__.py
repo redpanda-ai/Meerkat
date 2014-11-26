b83bf7b9af33e8f50c2e8eacc8d21d2edc0ea4b7
J. Andrew Key <joeandrewkey@gmail.com>
2014-11-26 04:23:47 +0000

    Addressing issue #111, adding https support

--- meerkat/web_service/__main__.py
+++ meerkat/web_service/__main__.py
@@ -1,31 +1,44 @@
 #!/usr/local/bin/python3.3
 
 import json
+import os
 
 from pprint import pprint
+
+import tornado.httpserver
 import tornado.ioloop
+
 from tornado_json.routes import get_routes
 from tornado_json.application import Application
+from tornado.options import define, options
 
 from meerkat.web_service.api import Meerkat_API
+#Define some defaults
+define("port", default=443, help="run on the given port", type=int)
 
 #################### USAGE ######################
 
 # python3.3 -m meerkat.web_service
 
-# curl -X POST -d @data/input/input.json http://localhost:8888/meerkat/ --header "Content-Type:application/json" | python3.3 -m json.tool
-
+# curl --insecure -s -X POST -d @big.json https://localhost:443/meerkat/ --header "Content-Type:application/json" | python3.3 -m json.tool
 #################################################
 
 def main():
-
+	#Log some useful stuff
+	tornado.options.parse_command_line()
 	routes = [("/meerkat/?", Meerkat_API)]
 
-	# Create the application
+	data_dir = "/root/git/Meerkat/"
+	ssl_options = {
+		"certfile" : os.path.join(data_dir, "server.crt"),
+		"keyfile" : os.path.join(data_dir, "server.key"),
+	}
+	# Create the tornado_json.application
 	application = Application(routes=routes, settings={})
-
-	# Start the application on port 8888
-	application.listen(8888)
+	# Create the http server
+	http_server = tornado.httpserver.HTTPServer(application, ssl_options=ssl_options)
+	# Start the http_server listening on port 443
+	http_server.listen(options.port)
 	tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
