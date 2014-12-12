88e42d2193357c9b8d17761d58ca9951679693c4
J. Andrew Key <joeandrewkey@gmail.com>
2014-12-12 22:07:01 +0000

    Pylint corrections for meerkat.__main__

--- meerkat/__main__.py
+++ meerkat/__main__.py
@@ -1,9 +1,7 @@
 #!/usr/local/bin/python3.3
 
-"""This file is the file first run when 
+"""This file is the file first run when
 Meerkat is executed as a package"""
-
-import os, re
 
 from meerkat.producer import mode_switch, initialize, validate_params
 from meerkat.various_tools import purge
@@ -11,15 +9,13 @@ from meerkat.various_tools import purge
 # Run the Meerkat Classifier
 
 try:
+	PARAMS = initialize()
+	validate_params(PARAMS)
+	mode_switch(PARAMS)
 
-	params = initialize()
-	validate_params(params)
-	mode_switch(params)
-
-except (KeyboardInterrupt, SystemExit) as e:
-
-	if params["input"].get("S3", "") != "":
-		input_dir = params["input"]["S3"]["src_local_path"]
-		output_dir = params["input"]["S3"]["dst_local_path"]
-		purge(input_dir, "output*")
-		purge(output_dir, "output*")
+except (KeyboardInterrupt, SystemExit) as error:
+	if PARAMS["input"].get("S3", "") != "":
+		INPUT_DIR = PARAMS["input"]["S3"]["src_local_path"]
+		OUTPUT_DIR = PARAMS["input"]["S3"]["dst_local_path"]
+		purge(INPUT_DIR, "output*")
+		purge(OUTPUT_DIR, "output*")
