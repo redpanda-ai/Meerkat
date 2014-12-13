a4a573bee06d9b97962dc81741fbf4ee09fe8ba2
J. Andrew Key <joeandrewkey@gmail.com>
2014-12-13 16:24:39 +0000

    Pylint corrections for  meerkat.tools.transaction_sampler module.

--- meerkat/tools/transaction_sampler.py
+++ meerkat/tools/transaction_sampler.py
@@ -1,5 +1,5 @@
 #!/bin/python3.3
-
+"""This module is used to create random samples from panel files."""
 import glob
 import gzip
 import logging
@@ -8,12 +8,14 @@ import sys
 from random import shuffle
 
 def clean_line(line):
+	"""Strips garbarge from both ends of a line, rendering it clean."""
 	return str(line)[2:-3]
 
 def bucket_me(input_file, d):
+	"""Creates a buckets for the file.  This will be used for a histogram"""
 	#print("Processing {0}".format(input_file))
 	logging.critical("Processing %s", input_file)
-	unzipped_input = None
+	#unzipped_input = None
 	count = 0
 	with gzip.open(input_file, "rb") as gzipped_input:
 		is_first_line = True
@@ -37,19 +39,21 @@ def bucket_me(input_file, d):
 	return header
 
 def get_header(input_file):
+	"""Gets the header from an input file."""
 	logging.critical("Getting header from %s", input_file)
 	with gzip.open(input_file, "rb") as gzipped_input:
-		is_first_line = True
+		#is_first_line = True
 		for line in gzipped_input:
 			line = clean_line(line)
 			header = "SHUFFLE_ID|" + line
 			return header
 
 def make_histogram(d):
+	"""Creates a histogram from the buckets."""
 	buckets = {}
 	for key in d:
 		if d[key] not in buckets:
-			buckets[d[key]] = [ key ]
+			buckets[d[key]] = [key]
 		else:
 			buckets[d[key]].append(key)
 
@@ -59,9 +63,11 @@ def make_histogram(d):
 
 	logging.critical("\nBucket # - Members in Bucket [percent of total]")
 	for sorted_key in sorted(buckets.keys()):
-		logging.critical("%d - %d [%.2f]" % (sorted_key, len(buckets[sorted_key]), len(buckets[sorted_key]) * 100 / total ))
+		logging.critical("%d - %d [%.2f]" % (sorted_key,\
+			len(buckets[sorted_key]), len(buckets[sorted_key]) * 100 / total ))
 
 def filter_me(input_file, y):
+	"""Filters out transactions belonging to the random sample of users."""
 	logging.critical("Processing %s", input_file)
 	with gzip.open(input_file, "rb") as gzipped_input:
 		count = 0
@@ -76,44 +82,49 @@ def filter_me(input_file, y):
 			if key in y:
 				y[key].append(line)
 
-input_path = sys.argv[1]
-os.chdir(input_path)
-input_files = sorted(glob.glob('*.gz'))
+def start(input_path):
+	"""Runs the main program."""
+	os.chdir(input_path)
+	input_files = sorted(glob.glob('*.gz'))
 
-d, y = {}, {}
-header = get_header(input_files[0])
-#print(header)
-#print(input_files)
-[bucket_me(x, d) for x in input_files]
+	d, y = {}, {}
+	header = get_header(input_files[0])
+	#print(header)
+	#print(input_files)
+	_ = [bucket_me(x, d) for x in input_files]
 
-#Could add a filter here to remove where d.keys where number of records < 25
-#z = [ a for a in d.keys() if d[a] >= 25 ]
-#all_members = z
+	#Could add a filter here to remove where d.keys where number of records < 25
+	#z = [ a for a in d.keys() if d[a] >= 25 ]
+	#all_members = z
 
-all_members = list(d.keys())
-len_all_members = len(all_members)
+	all_members = list(d.keys())
+	len_all_members = len(all_members)
 
-x = [all_members[i] for i in range(len_all_members)]
-logging.critical("Shuffling")
-shuffle(x)
+	x = [all_members[i] for i in range(len_all_members)]
+	logging.critical("Shuffling")
+	shuffle(x)
 
-sample_size_in_members = 20000
-x = x[:sample_size_in_members]
+	sample_size_in_members = 20000
+	x = x[:sample_size_in_members]
 
-for item in x:
-	y[item] = []
+	for item in x:
+		y[item] = []
 
-[filter_me(z, y) for z in input_files]
+	_ = [filter_me(z, y) for z in input_files]
 
-with open("outfile", "w") as outfile:
-	outfile.write(header + "\n")
-	for c in range(len(x)):
-		count = 0
-		for item in y[x[c]]:
-			count += 1
-			if count % 20000 == 0:
-				sys.stdout.write(".")
-				sys.stdout.flush()
-			line = str(c) + "|" + item + "\n"
-			outfile.write(line)
-			#print("%d|%s" % (c,item))
+	with open("outfile", "w") as outfile:
+		outfile.write(header + "\n")
+		for c in range(len(x)):
+			count = 0
+			for item in y[x[c]]:
+				count += 1
+				if count % 20000 == 0:
+					sys.stdout.write(".")
+					sys.stdout.flush()
+				line = str(c) + "|" + item + "\n"
+				outfile.write(line)
+				#print("%d|%s" % (c,item))
+
+#MAIN PROGRAM
+INPUT_PATH = sys.argv[1]
+start(INPUT_PATH)
