b1eafec9975b9461bff487bb3c18e9e8e364ca0c
J. Andrew Key <joeandrewkey@gmail.com>
2014-12-13 16:08:29 +0000

    Pylint corrections for meerkat.binary_classifier.__main__ module.

--- meerkat/binary_classifier/__main__.py
+++ meerkat/binary_classifier/__main__.py
@@ -1,22 +1,27 @@
 #!/usr/local/bin/python3.3
+"""This file is run first when executing binary_classifier as a subpackage"""
 
-"""This file is run first when executing 
-binary_classifier as a subpackage"""
-
-from meerkat.binary_classifier.bay import process_list
+#from meerkat.binary_classifier.bay import process_list
 from meerkat.custom_exceptions import InvalidArguments
 
 import sys, logging
 
+
+def start(arguments):
+	"""This function does everything."""
+	try:
+		input_file = open(arguments[1], encoding='utf-8')
+		#trans_list = input_file.read().splitlines()
+		#physical = process_list(trans_list)
+		#print(physical)
+		input_file.close()
+	except FileNotFoundError:
+		print(arguments[1], " not found, aborting.")
+		logging.error(arguments[1] + " not found, aborting.")
+		sys.exit()
+
 if len(sys.argv) != 2:
 	raise InvalidArguments(msg="Incorrect number of arguments", expr=None)
-try:
-	input_file = open(sys.argv[1], encoding='utf-8')
-	trans_list = input_file.read().splitlines()
-	physical = process_list(trans_list)
-	print(physical)		
-	input_file.close()
-except FileNotFoundError:
-	print(sys.argv[1], " not found, aborting.")
-	logging.error(sys.argv[1] + " not found, aborting.")
-	sys.exit()
+
+#MAIN PROGRAM
+start(sys.argv)
