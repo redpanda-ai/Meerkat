955f952d53eed63f2a0f651d1001fabe5a7e6549
J. Andrew Key <joeandrewkey@gmail.com>
2014-12-13 16:01:28 +0000

    Pylint corrections for meerkat.binary_classifier module.

--- meerkat/binary_classifier/verify.py
+++ meerkat/binary_classifier/verify.py
@@ -11,12 +11,14 @@ Created on Mar 11, 2014
 
 # Note: Transactions file must be pipe delimited.
 
-# python3.3 -m meerkat.binary_classifier.verify [pickled_classifier] [labeled_transactions_file]
-# python3.3 -m meerkat.binary_classifier.verify meerkat/binary_classifier/models/final_card.pkl data/input/users.txt
+# python3.3 -m meerkat.binary_classifier.verify \
+# [pickled_classifier] [labeled_transactions_file]
+# python3.3 -m meerkat.binary_classifier.verify \
+# meerkat/binary_classifier/models/final_card.pkl data/input/users.txt
 
 #####################################################
 
-import os
+#import os
 import sys
 
 from sklearn.externals import joblib
@@ -30,17 +32,16 @@ def test_model(file_to_test):
 	score = grid_search.score(transactions, labels)
 	print("Score: ", score)
 
-def verify_arguments():
+def verify_arguments(arguments):
 	""" Verifies proper usage """
-
-	sufficient_arguments = (len(sys.argv) == 3)
+	sufficient_arguments = (len(arguments) == 3)
 
 	if not sufficient_arguments:
 		print("Insufficient arguments. Please see usage")
 		sys.exit()
 
-	classifier = sys.argv[1]
-	transactions_file = sys.argv[2]
+	classifier = arguments[1]
+	transactions_file = arguments[2]
 
 	classifier_included = classifier.endswith('.pkl')
 	transactions_included = transactions_file.endswith(".txt")
@@ -49,11 +50,10 @@ def verify_arguments():
 		print("Erroneous arguments. Please see usage")
 		sys.exit()
 
-def run_from_command_line(command_line_arguments):
+def run_from_command_line(arguments):
 	"""Runs these commands if the module is invoked from the command line"""
-
-	verify_arguments()
-	test_model(sys.argv[2])
+	verify_arguments(arguments)
+	test_model(arguments[2])
 
 if __name__ == "__main__":
 	run_from_command_line(sys.argv)
