0f18af983040e9ae1e7e21289c4e2651f45f9685
Matt Sevrens <msevrens@gmail.com>
2015-01-21 13:45:11 -0800

    adding a common bigram generator

---
+++ meerkat/binary_classifier/generate_common_bigrams.py
@@ -0,0 +1,90 @@
+#!/usr/local/bin/python3.3
+
+"""This module takes processes a sample of transactions and outputs common
+bigrams
+
+Created on Jan 21, 2015
+@author: Matthew Sevrens
+"""
+
+#################### USAGE ##########################
+
+# python3.3 generate_common_bigrams.py [transaction_sample] 
+# python3.3 generate_common_bigrams.py data/card_sample.txt
+
+# Required Columns: 
+# DESCRIPTION_UNMASKED
+
+#####################################################
+
+import contextlib
+import csv
+import pandas as pd
+import sys
+
+from gensim.models import Phrases
+
+from various_tools import safe_print, safe_input
+
+class DummyFile(object):
+    def write(self, x): pass
+
+@contextlib.contextmanager
+def nostderr():
+    save_stderr = sys.stderr
+    sys.stderr = DummyFile()
+    yield
+    sys.stderr = save_stderr
+
+def verify_arguments():
+	"""Verify Usage"""
+
+	sufficient_arguments = (len(sys.argv) == 2)
+
+	if not sufficient_arguments:
+		safe_print("Insufficient arguments. Please see usage")
+		sys.exit()
+
+	sample = sys.argv[1]
+
+	sample_included = sample.endswith('.txt')
+
+	if not sample_included:
+		safe_print("Erroneous arguments. Please see usage")
+		sys.exit()
+
+def add_local_params(params):
+	"""Adds additional local params"""
+
+	params["generate_common_bigrams"] = {
+	}
+
+	return params
+
+def run_from_command_line(cla):
+	"""Runs these commands if the module is invoked from the command line"""
+
+	verify_arguments()
+	params = {}
+	params = add_local_params(params)
+	first_chunk = True
+
+	reader = pd.read_csv(cla[1], chunksize=5000, na_filter=False, quoting=csv.QUOTE_NONE, encoding="utf-8", sep='|', error_bad_lines=False)
+
+	# Process Transactions
+	for chunk in reader:
+
+		transactions = [row["DESCRIPTION_UNMASKED"].split(" ") for i, row in chunk.iterrows()]
+
+		if first_chunk:
+			bigrams = Phrases(transactions, max_vocab_size=5000)
+		else:
+			bigrams.add_vocab(transactions)
+
+	bigrams.save("models/bigram_model")
+
+	#for index, value in df['DESCRIPTION_UNMASKED'].iteritems():
+	#	safe_print(value)
+
+if __name__ == "__main__":
+	run_from_command_line(sys.argv)
