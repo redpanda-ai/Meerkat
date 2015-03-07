7fe7be06d7241328ae619dbd6681d2f44fcae621
J. Andrew Key <joeandrewkey@gmail.com>
2015-03-07 04:04:32 +0000

    Progress made, just saving to remote repo to protect changes.

--- meerkat/file_daemon.py
+++ meerkat/file_daemon.py
@@ -1,8 +1,10 @@
 import boto
 import json
 import logging
+import paramiko
 import re
 import sys
+import time
 
 from boto.s3.connection import Location
 from .custom_exceptions import FileProblem, InvalidArguments
@@ -23,26 +25,76 @@ def get_parameters():
 	return params
 
 def start():
-	"""This function starts the new_daemon."""
+	count, max_count = 0, 1
+	sleep_time_sec = 5
 	params = get_parameters()
+	ssh = paramiko.SSHClient()
+	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	params["launchpad"]["ssh"] = ssh
+	distribute_clients(params)
+	while count < max_count:
+		params["pending_files"] = []
+		logging.info("Scan #{0} of #{1}".format(count, max_count))
+		scan_locations(params)
+		count += 1
+		count_running_clients(params)
+		logging.info("Resting for {0} seconds.".format(sleep_time_sec))
+		time.sleep(sleep_time_sec)
+
+def distribute_clients(params):
+	lp = params["launchpad"]
+	ssh, instance_ips, username = lp["ssh"], lp["instance_ips"], lp["username"]
+	key_filename, producer = lp["key_filename"], lp["producer"]
+	producer_hash = lp["producer_hash"]
+	command = "sha1sum " + producer
+	hash = None
+	for instance_ip in instance_ips:
+		logging.info("Checking producer client on {0}".format(instance_ip))
+		ssh.connect(instance_ip, username=username, key_filename=key_filename)
+		_, stdout, _ = ssh.exec_command(command)
+		for line in stdout.readlines():
+			hash = line[:40]
+		logging.info("SHA1 is {0}".format(hash))
+		if hash != producer_hash:
+			logging.critical("Sorry, producer is the wrong version.")
+			sys.exit()
+
+def count_running_clients(params):
+	polling_command = "ps -ef|grep python3.3|grep -v grep|awk ' { print $12 }'"
+	lp = params["launchpad"]
+	ssh, instance_ips, username = lp["ssh"], lp["instance_ips"], lp["username"]
+	key_filename = lp["key_filename"]
+	for instance_ip in instance_ips:
+		logging.info("Counting running clients on {0}".format(instance_ip))
+		ssh.connect(instance_ip, username=username, key_filename=key_filename)
+		_, stdout, _ = ssh.exec_command(polling_command)
+		process_count = 0
+		for line in stdout.readlines():
+			process_count += 1
+			logging.info(line.strip())
+		logging.info("{0} processes found.".format(process_count))
+
+
+def scan_locations(params):
+	"""This function starts the new_daemon."""
 	location_pairs = params["location_pairs"]
 	params["s3_conn"] = boto.connect_s3()
+	#Distribute the meerkat.producer if necessary
+	#This producer needs updating to deal with oddities:
+		#Strangely ordered columns
+		#Strangely named columns
 
 	for pair in location_pairs:
 		logging.info("Compariing {0}".format(pair["name"]))
-		logging.debug("Scanning\n\t{0}\n\t{1}".format(pair["src_location"], pair["dst_location"]))
+		logging.info("Scanning\n\t{0}\n\t{1}".format(pair["src_location"], pair["dst_location"]))
 		src_dict = scan_s3_location(params, pair["src_location"])
-		#logging.info(src_dict)
 		dst_dict = scan_s3_location(params, pair["dst_location"])
 		update_pending_files(params, src_dict, dst_dict)
 
 	logging.info("There are {0} pending files".format(len(params["pending_files"])))
 
-
 def scan_s3_location(params, location):
 	"""Scan a single s3 location, and build a dict of files and timestamps"""
-	#TODO: Implement
-	#logging.info("Location {0}".format(location))
 	location_pattern = re.compile("^([^/]+)/(.*)$")
 	matches = location_pattern.search(location)
 	bucket_name = matches.group(1)
@@ -67,10 +119,16 @@ def update_pending_files(params, src_dict, dst_dict):
 	params["pending_files"].extend(newer_src)
 	logging.info("Src count {0}, dst count {1}".format(len(src_dict), len(dst_dict)))
 	logging.info("Not in dst {0}, Newer src {1}".format(len(not_in_dst), len(newer_src)))
+	logging.debug("List of unprocessed files:")
+	for item in not_in_dst:
+		logging.debug(item)
+	logging.debug("List of files that are newer at the source:")
+	for item in newer_src:
+		logging.debug(item)
 
 if __name__ == "__main__":
 	#MAIN PROGRAM
 	logging.basicConfig(format='%(asctime)s %(message)s', filename='logs/new_daemon.log', \
 		level=logging.INFO)
-	logging.info("Toy module activated.")
+	logging.info("Scanning module activated.")
 	start()
