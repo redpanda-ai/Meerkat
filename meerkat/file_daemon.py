74b0a5f0c9c8dbf8f199493e8db18f9ca16bfdc7
J. Andrew Key <joeandrewkey@gmail.com>
2015-03-10 22:11:07 +0000

    Fixing bug that allowed an empty stack of 'pending_files' to break the file_daemon.'

--- meerkat/file_daemon.py
+++ meerkat/file_daemon.py
@@ -36,11 +36,12 @@ def start():
 	params["launchpad"]["ssh"] = ssh
 	distribute_clients(params)
 	while count < max_count:
-		params["pending_files"] = []
+		params["not_finished"] = []
 		logging.info("Scan #{0} of #{1}".format(count, max_count))
 		scan_locations(params)
 		count += 1
 		count_running_clients(params)
+		dispatch_clients(params)
 		logging.info("Resting for {0} seconds.".format(sleep_time_sec))
 		time.sleep(sleep_time_sec)
 	logging.info("Done.")
@@ -63,21 +64,70 @@ def distribute_clients(params):
 			logging.critical("Sorry, producer is the wrong version.")
 			sys.exit()
 
-def count_running_clients(params):
-	polling_command = "ps -ef|grep python3.3|grep -v grep|awk ' { print $11,$12 }'"
+def dispatch_clients(params):
 	lp = params["launchpad"]
 	ssh, instance_ips, username = lp["ssh"], lp["instance_ips"], lp["username"]
 	key_filename = lp["key_filename"]
+	running_pattern = re.compile("^(.*) (.*)$")
+
+	polling_command = "ps -ef|grep python3.3|grep -v grep|awk ' { print $11,$12 }'"
+	#command_base = "cd /root/git/Meerkat/ && nohup python3.3 -m meerkat.file_producer "
+	command_base = "carriage "
+	total_slots = params["launchpad"]["per_instance_clients"]
 	for instance_ip in instance_ips:
 		logging.info("Counting running clients on {0}".format(instance_ip))
 		ssh.connect(instance_ip, username=username, key_filename=key_filename)
 		_, stdout, _ = ssh.exec_command(polling_command)
 		process_count = 0
+		in_progress = []
 		for line in stdout.readlines():
-			process_count += 1
-			logging.info(line)
-		logging.info("{0} processes found.".format(process_count))
+			if running_pattern.search(line):
+				process_count += 1
+		remaining_slots = total_slots - process_count
+		logging.info("{0} has {1} remaining slots".format(instance_ip, remaining_slots))
+		for i in range(remaining_slots):
+			if params["not_started"]:
+				item = params["not_started"].pop()
+				#logging.info("Launching {0}, which is {1}".format(i, item))
+				panel_name, panel_file = item[0:2]
+				command = command_base + item[0] + " " + item[1] + " " + item[0] + "." + item[1] + ".log"
+				logging.info("Command: {0}".format(command))
+				#print(command)
+				#_, stdout, _ = ssh.exec_command(command)
+				#logging.info(stdout)
+				#logging.info("Command: {0}".format(command))
 
+def count_running_clients(params):
+	polling_command = "ps -ef|grep python3.3|grep -v grep|awk ' { print $12,$13 }'"
+	lp = params["launchpad"]
+	ssh, instance_ips, username = lp["ssh"], lp["instance_ips"], lp["username"]
+	key_filename = lp["key_filename"]
+	running_pattern = re.compile("^(.*) (.*)$")
+	params["in_progress"] = []
+	for instance_ip in instance_ips:
+		logging.info("Counting running clients on {0}".format(instance_ip))
+		ssh.connect(instance_ip, username=username, key_filename=key_filename)
+		_, stdout, _ = ssh.exec_command(polling_command)
+		process_count = 0
+		in_progress = []
+		for line in stdout.readlines():
+			if running_pattern.search(line):
+				matches = running_pattern.search(line)
+				process_count += 1
+				panel_name = matches.group(1)
+				panel_file = matches.group(2)
+				in_progress.append((panel_name, panel_file))
+				logging.info("Panel name: {0}, Panel file: {1}".format(panel_name, panel_file))
+		logging.info("{0} processes found.".format(process_count))
+		params["in_progress"].extend(in_progress)
+	#Valuable metrics
+	not_finished = params["not_finished"]
+	in_progress = params["in_progress"]
+	not_started = list(set(not_finished) - set(in_progress))
+	params["not_started"] = not_started
+	logging.info("Number of files not yet finished: {0}".format(len(not_finished)))
+	logging.info("Number of files in progress: {0}".format(len(in_progress)))
+	logging.info("Number of files not yet started: {0}".format(len(not_started)))
 
 def scan_locations(params):
 	"""This function starts the new_daemon."""
@@ -91,7 +141,7 @@ def scan_locations(params):
 		dst_dict = scan_s3_location(params, pair["dst_location"])
 		update_pending_files(params, name, src_dict, dst_dict)
 
-	logging.info("There are {0} pending files".format(len(params["pending_files"])))
+	logging.info("There are {0} pending files".format(len(params["not_finished"])))
 
 def scan_s3_location(params, location):
 	"""Scan a single s3 location, and build a dict of files and timestamps"""
@@ -113,10 +163,10 @@ def update_pending_files(params, name, src_dict, dst_dict):
 	dst_keys = dst_dict.keys()
 	not_in_dst = [ (name, k) for k in src_dict.keys() if k not in dst_keys ]
 	newer_src = [ (name, k) for k in src_dict.keys() if k in dst_keys and src_dict[k][3] > dst_dict[k][3] ]
-	if "pending_files" not in params:
-		params["pending_files"] = []
-	params["pending_files"].extend(not_in_dst)
-	params["pending_files"].extend(newer_src)
+	if "not_finished" not in params:
+		params["not_finished"] = []
+	params["not_finished"].extend(not_in_dst)
+	params["not_finished"].extend(newer_src)
 	logging.info("Src count {0}, dst count {1}".format(len(src_dict), len(dst_dict)))
 	logging.info("Not in dst {0}, Newer src {1}".format(len(not_in_dst), len(newer_src)))
 	logging.debug("List of unprocessed files:")
