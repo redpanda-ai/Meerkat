9e641e2261039b2b7a05cc1c2c0b9008ed214b59
Matt Sevrens <msevrens@gmail.com>
2015-04-03 13:11:23 -0700

    added code to load and encode some transactions

--- meerkat/classification/deep_classification.py
+++ meerkat/classification/deep_classification.py
@@ -16,9 +16,10 @@ Created on Mar 31, 2015
 #####################################################
 
 import os
+import sys
 
+import pandas as pd
 import numpy as np
-from pandas.io.parsers import read_csv
 from sklearn.utils import shuffle
 
 from lasagne import layers
@@ -34,7 +35,7 @@ ALPHA_DICT = {}
 for i, a in enumerate(ALPHABET):
     ALPHA_DICT[a] = i
 
-deep = NeuralNet(
+deep_conv = NeuralNet(
     layers=[
         ('input', layers.InputLayer),
         ('conv1', layers.Conv1DLayer),
@@ -66,71 +67,29 @@ deep = NeuralNet(
 def character_encode(str, l):
     """Transform a transaction to a properly encoded representation"""
     s = str.lower()[0:l]
-    t = np.zeros((len(ALPHABET), l), dtype=np.int)
+    t = np.zeros((len(ALPHABET), l), dtype=np.float)
     for i, c in reversed(list(enumerate(s))):
         if c in ALPHABET:
-            t[ALPHA_DICT[c]][len(s) - i + 1] = 1
+            t[ALPHA_DICT[c]][len(s) - i - 1] = 1
     return t
 
-# TEMPORARY EXAMPLE CODE
-def load(test=False, cols=None):
-    """Loads data from FTEST if *test* is True, otherwise from FTRAIN.
-    Pass a list of *cols* if you're only interested in a subset of the
-    target columns.
-    """
-    fname = FTEST if test else FTRAIN
-    df = read_csv(os.path.expanduser(fname))  # load pandas dataframe
+def load_data(filename):
+    """Loads transactions and encodes them into a proper format""" 
+    
+    l = 128
+    df = pd.read_csv(filename, na_filter=False, encoding="utf-8", sep='|', error_bad_lines=False)
+    X = np.empty((len(df.index), len(ALPHABET), l))
+    y = np.zeros((len(df.index)), dtype=np.int)
 
-    # The Image column has pixel values separated by space; convert
-    # the values to numpy arrays:
-    df['Image'] = df['Image'].apply(lambda im: np.fromstring(im, sep=' '))
+    for index, row in df.iterrows():
+        X[index] = character_encode(row["DESCRIPTION_UNMASKED"], l)
 
-    if cols:  # get a subset of columns
-        df = df[list(cols) + ['Image']]
-
-    print(df.count())  # prints the number of values for each column
-    df = df.dropna()  # drop all rows that have missing values in them
-
-    X = np.vstack(df['Image'].values) / 255.  # scale pixel values to [0, 1]
-    X = X.astype(np.float32)
-
-    if not test:  # only FTRAIN has any target columns
-        y = df[df.columns[:-1]].values
-        y = (y - 48) / 48  # scale target coordinates to [-1, 1]
-        X, y = shuffle(X, y, random_state=42)  # shuffle train data
-        y = y.astype(np.float32)
-    else:
-        y = None
+    print(X.shape)
 
     return X, y
 
-net1 = NeuralNet(
-    layers=[  # three layers: one hidden layer
-        ('input', layers.InputLayer),
-        ('hidden', layers.DenseLayer),
-        ('output', layers.DenseLayer),
-        ],
-    # layer parameters:
-    input_shape=(None, 9216),  # 96x96 input pixels per batch
-    hidden_num_units=100,  # number of units in hidden layer
-    output_nonlinearity=None,  # output layer uses identity function
-    output_num_units=30,  # 30 target values
-
-    # optimization method:
-    update=nesterov_momentum,
-    update_learning_rate=0.01,
-    update_momentum=0.9,
-
-    regression=True,  # flag to indicate we're dealing with regression problem
-    max_epochs=400,  # we want to train this many epochs
-    verbose=1,
-)
-
-X, y = load()
-
-print(X.reshape(-1, 1, 96, 96).shape)
-
-#net1.fit(X, y)
+X, y = load_data("data/misc/transaction_type_GT_Bank.txt")
+#deep_conv.fit(X, y)
 
 #TODO Character Quantization
 #TODO Data Loading
