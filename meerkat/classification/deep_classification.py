f559d7c18d559c8932fed7af286b61f5ba20c58d
Matt Sevrens <msevrens@gmail.com>
2015-04-06 13:48:53 -0700

    a few hyperparameter changes to match the original research more

--- meerkat/classification/deep_classification.py
+++ meerkat/classification/deep_classification.py
@@ -34,42 +34,6 @@ ALPHA_DICT = {}
 for i, a in enumerate(ALPHABET):
     ALPHA_DICT[a] = i
 
-deep_conv = NeuralNet(
-    layers=[
-        ('input', layers.InputLayer),
-        ('conv1', layers.Conv1DLayer),
-        ('pool1', layers.MaxPool1DLayer),
-        ('conv2', layers.Conv1DLayer),
-        ('pool2', layers.MaxPool1DLayer),
-        ('conv3', layers.Conv1DLayer),
-        ('conv4', layers.Conv1DLayer),
-        ('conv5', layers.Conv1DLayer),
-        ('conv6', layers.Conv1DLayer),
-        ('pool3', layers.MaxPool1DLayer),
-        ('hidden1', layers.DenseLayer),
-        ('dropout1', layers.DropoutLayer),
-        ('hidden2', layers.DenseLayer),
-        ('output', layers.DenseLayer),
-    ],
-    input_shape=(None, len(ALPHABET), 128),
-    conv1_num_filters=7, conv1_filter_length=7, pool1_ds=3,
-    conv2_num_filters=7, conv2_filter_length=7, pool2_ds=3,
-    conv3_num_filters=3, conv3_filter_length=2, pool3_ds=3,
-    conv4_num_filters=3, conv4_filter_length=2,
-    conv5_num_filters=3, conv5_filter_length=2, 
-    conv6_num_filters=3, conv6_filter_length=2,
-    hidden1_num_units=512,
-    hidden2_num_units=512,
-    output_num_units=11,
-    output_nonlinearity=lasagne.nonlinearities.softmax,
-    update_learning_rate=0.01,
-    dropout1_p=0.5,
-    update=nesterov_momentum,
-    update_momentum=0.9,
-    max_epochs=100,
-    verbose=1
-)
-
 def character_encode(str, l):
     """Transform a transaction to a properly encoded representation"""
     s = str.lower()[0:l]
@@ -93,8 +57,47 @@ def load_data(filename):
         X[index] = character_encode(row["DESCRIPTION_UNMASKED"], l)
         y[index] = label_map[row["TRANSACTION_ORIGIN"]]
 
-    return X, y
+    return X, y, len(labels)
 
-X, y = load_data("data/misc/transaction_type_GT_Bank.txt")
+X, y, output_units = load_data("data/input/bank_sample.txt")
+
+deep_conv = NeuralNet(
+    layers=[
+        ('input', layers.InputLayer),
+        ('conv1', layers.Conv1DLayer),
+        ('pool1', layers.MaxPool1DLayer),
+        ('conv2', layers.Conv1DLayer),
+        ('pool2', layers.MaxPool1DLayer),
+        ('conv3', layers.Conv1DLayer),
+        ('conv4', layers.Conv1DLayer),
+        ('conv5', layers.Conv1DLayer),
+        ('conv6', layers.Conv1DLayer),
+        ('pool3', layers.MaxPool1DLayer),
+        ('hidden1', layers.DenseLayer),
+        ('dropout1', layers.DropoutLayer),
+        ('hidden2', layers.DenseLayer),
+        ('dropout2', layers.DropoutLayer),
+        ('output', layers.DenseLayer),
+    ],
+    input_shape=(None, len(ALPHABET), 128),
+    conv1_num_filters=1, conv1_filter_length=7, pool1_ds=3,
+    conv2_num_filters=1, conv2_filter_length=7, pool2_ds=3,
+    conv3_num_filters=1, conv3_filter_length=3, 
+    conv4_num_filters=1, conv4_filter_length=3,
+    conv5_num_filters=1, conv5_filter_length=3, 
+    conv6_num_filters=1, conv6_filter_length=3, pool3_ds=3,
+    hidden1_num_units=512,
+    hidden2_num_units=512,
+    output_num_units=output_units,
+    output_nonlinearity=lasagne.nonlinearities.softmax,
+    update_learning_rate=0.01,
+    dropout1_p=0.5,
+    dropout2_p=0.5,
+    update=nesterov_momentum,
+    update_momentum=0.9,
+    max_epochs=100,
+    verbose=1
+)
+
 deep_conv.fit(X, y)
 print(deep_conv.predict(X))
