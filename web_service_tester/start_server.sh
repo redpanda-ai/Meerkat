599b2f7040672f33167b4866f3a242d2e783bad4
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-06-12 20:53:04 +0000

    now writing log to the correct location

--- web_service_tester/start_server.sh
+++ web_service_tester/start_server.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 cd ..
-sudo nohup python3 -m meerkat.web_service > async.log 2>&1 &
-cd web_service_test
+sudo nohup python3 -m meerkat.web_service > web_service_tester/async.log 2>&1 &
+cd web_service_tester
