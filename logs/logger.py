20d2400bd8712ad04504e988c736c2f10e548b20
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-06-12 21:22:33 +0000

    made demonstrative file more clearly working when run off of command line

--- logs/logger.py
+++ logs/logger.py
@@ -18,18 +18,19 @@ def create_timed_rotating_log(path):
 	logger = logging.getLogger("Rotating Log")
 	logger.setLevel(logging.INFO)
 
-	# create a handler to rotate 7 logs every minute
+	# create a handler to rotate 7 logs, with each one generated every
+	# 10 seconds
 	handler = TimedRotatingFileHandler(path,
-									   when = "d",
-									   interval = 1,
+									   when = "s",
+									   interval = 10,
 									   backupCount = 7)
 	logger.addHandler(handler)
 	
 	# add dummy info to the logs, just for demonstration purposes
-	# while True:
-	# 	print(os.listdir("."), i)
-	# 	logger.info(time.time())
-	# 	time.sleep(3)
+	while True:
+	 	print(os.listdir("."))
+	 	logger.info(time.time())
+	 	time.sleep(3)
  
 #----------------------------------------------------------------------
 if __name__ == "__main__":
