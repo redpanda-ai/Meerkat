b99f5719f22e74935878d4ea912ef5f3c17f271d
J. Andrew Key <joeandrewkey@gmail.com>
2015-07-01 18:22:16 +0000

    Fixing issue with string cleanse and < symbol and single quote.

--- meerkat/various_tools.py
+++ meerkat/various_tools.py
@@ -157,6 +157,7 @@ def get_merchant_by_id(params, factual_id, es_connection, index=""):
 
 def numeric_cleanse(original_string):
 	"""Strips out characters that might confuse ElasticSearch."""
+	#TODO: destroy this function
 	bad_characters = [r"\[", r"\]", r"'", r"\{", r"\}", r'"', r"/", r"-"]
 	bad_character_regex = "|".join(bad_characters)
 	cleanse_pattern = re.compile(bad_character_regex)
@@ -180,7 +181,7 @@ def purge(my_dir, pattern):
 def string_cleanse(original_string):
 	"""Strips out characters that might confuse ElasticSearch."""
 	bad_characters = [r"\[", r"\]", r"\{", r"\}", r'"', r"/", r"\\", r"\:",\
-		r"\(", r"\)", r"-", r"\+", r">", r"!", r"\*", r"\|\|", r"&&", r"~"]
+		r"\(", r"\)", r"-", r"\+", r"<", r">", r"'", r"!", r"\*", r"\|\|", r"&&", r"~"]
 	bad_character_regex = "|".join(bad_characters)
 	cleanse_pattern = re.compile(bad_character_regex)
 	with_spaces = re.sub(cleanse_pattern, " ", original_string)
