8e3c82139b8032b1399c3a8cb7d55ff72e6c28ac
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-07-01 20:32:56 +0000

    accomodated mercchant subsets

--- meerkat/classification/find_merchants.py
+++ meerkat/classification/find_merchants.py
@@ -14,22 +14,33 @@ def find_merchant(transaction):
 	"""
 		Looks through a transaction for a merchant that matches a trie
 	"""
+	longest = None
 	transaction = standardize(transaction)
 	for start in range(len(transaction) - 1):
 		if MERCHANT_TRIE.has_keys_with_prefix(transaction[start]):
 			for end in range(start, len(transaction) + 1):
-				if transaction[start:end] in MERCHANT_TRIE and len(transaction[start:end]) > 3:
-					return transaction[start:end]
-	return None
+				if transaction[start:end] in MERCHANT_TRIE and \
+				   len(transaction[start:end]) > 3 and \
+				   (longest == None or len(longest) < len(transaction[start:end])):
+					longest = transaction[start:end]
+
+	return longest
+def column_print(merchant, expected, transaction):
+	out = ""
+	out += merchant[:12] + (" " * (12-len(merchant))) + " | "
+	out += expected[:12] + (" " * (12-len(expected))) + " | "
+	return out + transaction
 
 def main():
 	"""runs the file"""
 	# This file is located in S3 under s3yodlee/development/bank/3_year_card_sample.txt
-	transactions = open("data/input/3_year_card_sample.log")
-	reader = csv.reader(transactions, delimiter='|')
+	# transactions = open("data/input/3_year_card_sample.log")
+	transactions = open("data/input/merchant_labels.log",encoding = "ISO-8859-1")
+	reader = csv.reader(transactions, delimiter=',')
 	correct = 0
 	count = 0
 	guesses = 0
+	print(column_print("Guessed", "Actual", "Transaction"))
 	for line in reader:
 		merchant = find_merchant(line[0])
 		if merchant != None:
@@ -37,7 +48,7 @@ def main():
 		if standardize(merchant) == standardize(line[1]):
 			correct += 1
 		elif merchant != None:
-			print(merchant, " | ", line[1], " | ", line[0])
+			print(column_print(merchant, standardize(line[1]), line[0]))
 		count += 1
 		if count > 1000:
 			break
