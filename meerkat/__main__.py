3f3dba09ad5633f4f7b40ee8789b5552c550448e
RayaanKhatau <rk2748@columbia.edu>
2015-07-23 22:48:08 +0000

    Added pass statements to legally maintain indentation

--- meerkat/__main__.py
+++ meerkat/__main__.py
@@ -3,8 +3,9 @@
 """This file is the file first run when
 Meerkat is executed as a package"""
 
+
 #from meerkat.producer import mode_switch, initialize, validate_params
-from meerkat.various_tools import purge
+#from meerkat.various_tools import purge
 
 # Run the Meerkat Classifier
 
@@ -12,10 +13,17 @@ try:
 	#PARAMS = initialize()
 	#validate_params(PARAMS)
 	#mode_switch(PARAMS)
+	
+	#Next line is used to prevent expected an indentented block (syntax-error)
+	pass
 
 except (KeyboardInterrupt, SystemExit) as error:
-	if #PARAMS["input"].get("S3", "") != "":
+	
+	#if PARAMS["input"].get("S3", "") != "":
 		#INPUT_DIR = PARAMS["input"]["S3"]["src_local_path"]
 		#OUTPUT_DIR = PARAMS["input"]["S3"]["dst_local_path"]
 		#purge(INPUT_DIR, "output*")
 		#purge(OUTPUT_DIR, "output*")
+		
+	#Next line is used to prevent expected an indentented block (syntax-error)
+	pass
