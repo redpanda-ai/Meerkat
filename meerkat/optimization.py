e1a91773897b183fc9569757ca3ee29d72504b55
RayaanKhatau <rk2748@columbia.edu>
2015-08-12 22:21:38 +0000

    Debugging

--- meerkat/optimization.py
+++ meerkat/optimization.py
@@ -189,6 +189,7 @@ def run_meerkat(params, dataset):
 
 
 	# Test Accuracy
+	print("RESULTS -___")
 	pprint(result_list)
 	accuracy_results = vest_accuracy(params, result_list=result_list)
 	print_results(accuracy_results)
