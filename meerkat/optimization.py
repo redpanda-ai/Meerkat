95a98c7a24070217812d5eee9910624866ea7a61
Matt Sevrens <msevrens@gmail.com>
2015-08-14 10:17:03 -0700

    removing unnecessary functions in optimization.py

--- meerkat/optimization.py
+++ meerkat/optimization.py
@@ -47,124 +47,8 @@ from meerkat.accuracy import print_results, vest_accuracy
 from meerkat.various_tools import load_dict_list, queue_to_list, safe_print, get_us_cities
 from meerkat.various_tools import load_params, load_hyperparameters, progress
 from meerkat.web_service.web_consumer import Web_Consumer
-#CONSTANTS
-USED_IN_HEADER, ORIGIN, NAME_IN_MEERKAT, NAME_IN_ORIGIN = 0, 1, 2, 3
+
 consumer = Web_Consumer()
-
-def get_field_mappings(params):
-	"""Returns a list of field_mappings."""
-	return [[x[NAME_IN_ORIGIN], x[NAME_IN_MEERKAT]]
-		for x in get_unified_header(params)
-		if (x[ORIGIN] == "search") and (x[NAME_IN_MEERKAT] != x[NAME_IN_ORIGIN])]
-
-def get_meerkat_fields(params):
-	"""Return a list of meerkat fields to add to the panel output."""
-	return [x[NAME_IN_MEERKAT]
-		for x in get_unified_header(params)
-		if (x[USED_IN_HEADER] == True) and (x[ORIGIN] == "search")]
-
-def get_column_map(params):
-	"""Fix old or erroneous column names"""
-	container = params["container"].upper()
-	column_mapping_list = [
-		(x[NAME_IN_ORIGIN], x[NAME_IN_MEERKAT].replace("__BLANK", container))
-		for x in get_unified_header(params)
-		if (x[ORIGIN] == "input") and (x[NAME_IN_MEERKAT] != x[NAME_IN_ORIGIN])]
-	column_map = {}
-	for name_in_origin, name_in_meerkat in column_mapping_list:
-		column_map[name_in_origin] = name_in_meerkat
-	return column_map
-
-def get_panel_header(params):
-	"""Return an ordered consistent header for panels"""
-	return [
-		x[NAME_IN_MEERKAT].replace("__BLANK", params["container"].upper())
-		for x in get_unified_header(params)]
-
-def get_unified_header(params):
-	"""Return the unified_header object, minus the first row."""
-	return params["unified_header"][1:]
-
-class DummyFile(object):
-    def write(self, x): pass
-
-@contextlib.contextmanager
-def nostdout():
-    save_stdout = sys.stdout
-    save_stderr = sys.stderr
-    sys.stdout = DummyFile()
-    sys.stderr = DummyFile()
-    yield
-    sys.stderr = save_stderr
-    sys.stdout = save_stdout
-
-def test_train_split(dataset):
-	"""Load the verification source, and split the
-	data into a test and training set"""
-
-	# Shuffle/Split Data
-	test, train = [], []
-	shuffle(dataset)
-	split_arr = array_split(array(dataset), 2)
-	test = list(split_arr[0])
-	train = list(split_arr[1])
-
-	return test, train
-
-def two_fold(hyperparameters, known, params):
-	"""Run two-fold cross validation. Combine Scores"""
-
-	# Run Randomized Optimization and Cross Validate
-	d0_top_score = randomized_optimization(hyperparameters, known, params, d0)
-	d1_top_score = randomized_optimization(hyperparameters, known, params, d1)
-
-	# Cross Validate
-	d1_results = cross_validate(d0_top_score, d1)
-	d0_results = cross_validate(d1_top_score, d0)
-
-	# Show Scores
-	print("FINAL SCORES:")
-	pprint(d0_results)
-	pprint(d1_results)
-
-	# Save Scores
-	save_cross_fold_results(d0_top_score, d0_results, d1_top_score, d1_results)
-
-def cross_validate(top_score, dataset):
-	"""Validate model built with training set against
-	test set"""
-
-	# Split boosts from other hyperparameters and format accordingly
-	hyperparameters = top_score["hyperparameters"]
-	boost_vectors, boost_labels, hyperparameters = split_hyperparameters(hyperparameters)
-
-	# Override Params
-	params["elasticsearch"]["boost_labels"] = boost_labels
-	params["elasticsearch"]["boost_vectors"] = boost_vectors
-
-	# Run Classifier
-	print("\n", "CROSS VALIDATE", "\n")
-	#accuracy = run_meerkat(params, desc_queue, hyperparameters)
-	accuracy = run_meerkat(params, dataset)
-	return accuracy
-
-def save_cross_fold_results(d0_top_score, d0_results, d1_top_score, d1_results):
-
-	# Save Scores
-	file_name = os.path.splitext(os.path.basename(sys.argv[1]))[0] + "_" + "cross_validation_results.json" 
-	record = open(file_name, 'w')
-
-	pprint("d0 as Training Data - Top Score found through Optimization:", record)
-	pprint(d0_top_score, record)
-
-	pprint("d0 as Training Data - Score of d1 on this set of hyperparameters:", record)
-	pprint(d1_results, record)
-
-	pprint("d1 as Training Data - Top Score found through Optimization:", record)
-	pprint(d1_top_score, record)
-
-	pprint("d1 as Training Data - Score of d0 on this set of hyperparameters:", record)
-	pprint(d0_results, record)
 
 def run_meerkat(params, dataset):
 	"""Run meerkat on a set of transactions"""
@@ -418,24 +302,6 @@ def split_hyperparameters(hyperparameters):
 		
 	return boost_vectors, boost_labels, other
 
-def get_desc_queue(dataset):
-	"""Alt version of get_desc_queue"""
-
-	transactions = [deepcopy(X) for X in dataset]
-	desc_queue = queue.Queue()
-	users = collections.defaultdict(list)
-
-	# Split into user buckets
-	for row in transactions:
-		user = row['UNIQUE_MEM_ID']
-		users[user].append(row)
-
-	# Add Users to Queue
-	for key, value in users.items():
-		desc_queue.put(users[key])
-
-	return desc_queue
-
 def run_classifier(hyperparameters, params, dataset):
 	""" Runs the classifier with a given set of hyperparameters"""
 
@@ -447,8 +313,8 @@ def run_classifier(hyperparameters, params, dataset):
 	hyperparameters["boost_vectors"] = boost_vectors
 
 	consumer.update_hyperparams(hyperparameters)
-	#accuracy = run_meerkat(params, desc_queue, hyperparameters)
-	accuracy = run_meerkat(params,dataset)
+	accuracy = run_meerkat(params, dataset)
+
 	return accuracy
 
 def add_local_params(params):
@@ -459,7 +325,6 @@ def add_local_params(params):
 	params["optimization"]["scores"] = []
 
 	params["optimization"]["settings"] = {
-		"folds": 1,
 		"initial_search_space": 2,
 		"initial_learning_rate": 0.25,
 		"iteration_search_space": 2,
@@ -545,15 +410,9 @@ def run_from_command_line(command_line_arguments):
 		"po_box" : "1.159",
 	}
 
+	# Use all data and optimize
 	dataset = load_dataset(params)
-	#dataset = format_web_consumer(dataset)
-	
-	# Use all data or Cross Validate
-	if params["optimization"]["settings"]["folds"] == 1:
-		randomized_optimization(hyperparameters, known, params, dataset)
-	else:
-		d0, d1 = test_train_split(dataset)
-		two_fold(hyperparameters, known, params)
+	randomized_optimization(hyperparameters, known, params, dataset)
 
 	# Run Speed Tests
 	time_delta = datetime.datetime.now() - start_time
