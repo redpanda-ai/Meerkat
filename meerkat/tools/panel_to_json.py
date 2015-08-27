4b445702fb616d3667e96c6c96bc7a0c3ae23f4a
J. Andrew Key <joeandrewkey@gmail.com>
2015-08-27 20:41:34 +0000

    Correcting pylint errors.

--- meerkat/tools/panel_to_json.py
+++ meerkat/tools/panel_to_json.py
@@ -1,3 +1,5 @@
+"""This module samples panel files to produce JSON."""
+
 #################### USAGE ##########################
 
 # python3.3 panel_to_json.py [panel_file] [number_of_transactions]
@@ -13,8 +15,8 @@ import json
 
 def dict_2_json(obj, filename):
 	"""Saves a dict as a json file"""
-	with open(filename, 'w') as fp:
-		json.dump(obj, fp, indent=4)
+	with open(filename, 'w') as output_file:
+		json.dump(obj, output_file, indent=4)
 
 def string_cleanse(original_string):
 	"""Strips out characters that might confuse ElasticSearch."""
@@ -24,27 +26,33 @@ def string_cleanse(original_string):
 	with_spaces = re.sub(cleanse_pattern, " ", original_string)
 	return ' '.join(with_spaces.split())
 
-query = {
-	"cobrand_id" : 1234,
-	"user_id" : 2345,
-	"container" : "card",
-	"transaction_list" : []
-}
-
-df = pd.read_csv(sys.argv[1], na_filter=False, encoding="utf-8", sep='|', error_bad_lines=False)
-
-rows = np.random.choice(df.index.values, int(sys.argv[2]), replace=False)
-sampled_df = df.ix[rows]
-
-for index, row in sampled_df.iterrows():
-
-	trans = {
-		"amount" : 100.00,
-		"date" : row["TRANSACTION_DATE"],
-		"ledger_entry" : "debit",
-		"transaction_id" : int(index),
-		"description" : row["DESCRIPTION_UNMASKED"]
+def well_named_function():
+	"""This function brings it all together."""
+	query = {
+		"cobrand_id" : 1234,
+		"user_id" : 2345,
+		"container" : "card",
+		"transaction_list" : []
 	}
-	query["transaction_list"].append(trans)
 
-dict_2_json(query, "web_service_input.json")
+	data_frame = pd.read_csv(sys.argv[1], na_filter=False, encoding="utf-8",
+		 sep='|', error_bad_lines=False)
+
+	rows = np.random.choice(data_frame.index.values, int(sys.argv[2]),
+		replace=False)
+	sampled_df = data_frame.ix[rows]
+
+	for index, row in sampled_df.iterrows():
+		trans = {
+			"amount" : 100.00,
+			"date" : row["TRANSACTION_DATE"],
+			"ledger_entry" : "debit",
+			"transaction_id" : int(index),
+			"description" : row["DESCRIPTION_UNMASKED"]
+		}
+		query["transaction_list"].append(trans)
+
+	dict_2_json(query, "web_service_input.json")
+
+if __name__ == "main":
+	well_named_function()
