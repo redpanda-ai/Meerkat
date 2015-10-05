0b9b1a87ac34327f9dafc40c6515265d1c28b50c
Joseph Altmaier <jaltmaier@yodlee.com>
2015-10-05 23:06:21 +0000

    Add doc strigs to new unit tests

--- tests/test_web_consumer.py
+++ tests/test_web_consumer.py
@@ -15,18 +15,21 @@ class WebConsumerTest(unittest.TestCase):
         return
 
     def test_static_bank_category_map(self):
+        """Assert that the correct static category label has been applied from the bank map"""
         transactions = web_consumer_fixture.get_transaction_bank_fallback_classifiable()
         self.consumer._Web_Consumer__apply_missing_categories(transactions)
         self.assertEqual(len(transactions[0]["category_labels"]), 1)
         self.assertEqual(transactions[0]["category_labels"][0], "Utilities")
 
     def test_static_card_category_map(self):
+        """Assert that the correct static category label has been applied from the card map"""
         transactions = web_consumer_fixture.get_transaction_card_fallback_classifiable()
         self.consumer._Web_Consumer__apply_missing_categories(transactions)
         self.assertEqual(len(transactions[0]["category_labels"]), 1)
         self.assertEqual(transactions[0]["category_labels"][0], "Restaurants/Dining")
 
     def test_static_subtype_category_map(self):
+        """Assert that the transaction subtype is applied when the static map indicates it should be"""
         transactions = web_consumer_fixture.get_transaction_subtype_fallback()
         self.consumer._Web_Consumer__apply_missing_categories(transactions)
         self.assertEqual(len(transactions[0]["category_labels"]), 1)
