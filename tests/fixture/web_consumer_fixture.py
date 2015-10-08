2080ca07cb20cfdd472a11b360d3936581818039
Joseph Altmaier <jaltmaier@yodlee.com>
2015-10-08 18:04:18 +0000

    Add a test for __sws handling

--- tests/fixture/web_consumer_fixture.py
+++ tests/fixture/web_consumer_fixture.py
@@ -1,4 +1,5 @@
 def get_transaction():
+    """Create and return an array containing a single transaction"""
     return [{
         "date": "2014-08-10T00:00:00",
         "description": "taco bell scarsdale, ny",
@@ -9,6 +10,7 @@ def get_transaction():
 
 
 def get_transaction_with_subtype():
+    """Create and return an array containing a single transaction with a subtype appended"""
     return [{
         "date": "2014-08-10T00:00:00",
         "description": "taco bell scarsdale, ny",
@@ -20,18 +22,21 @@ def get_transaction_with_subtype():
 
 
 def get_transaction_bank_fallback_classifiable():
+    """Return an array containing a single transaction for a merchant whose name only appears in the bank fallback map"""
     return [{
         "CNN": "Con Edison"
     }]
 
 
 def get_transaction_card_fallback_classifiable():
+    """Return an array containing a single transaction for a merchant whose name only appears in the card fallback map"""
     return [{
         "CNN": "Legal Sea Foods"
     }]
 
 
 def get_transaction_subtype_fallback():
+    """Return an array containing a single transaction for a merchant whose category must be looked up using the transaction subtype"""
     return [{
         "CNN": "Capital One",
         "txn_sub_type": "SSA"
@@ -39,6 +44,7 @@ def get_transaction_subtype_fallback():
 
 
 def get_transaction_subtype_no_fallback():
+    """Return an array containing a single transaction for a merchant whose category must be looked up using the transaction subtype and whose transaction subtype does not appearn in the fallback map"""
     return [{
         "CNN": "Capital One",
         "txn_sub_type": "Joseph Rules"
@@ -46,6 +52,7 @@ def get_transaction_subtype_no_fallback():
 
 
 def get_test_request_bank():
+    """return an API request with the "bank" container"""
     return {
         "container": "bank",
         "transaction_list": get_test_transaction_list()
@@ -53,6 +60,7 @@ def get_test_request_bank():
 
 
 def get_test_request_card():
+    """return an API request with the "card" container"""
     return {
         "container": "card",
         "transaction_list": get_test_transaction_list()
@@ -60,17 +68,34 @@ def get_test_request_card():
 
 
 def get_test_transaction_list():
+    """Return a list of transactions which will test different paths in the web_consumer"""
     return [
         {
-            "ledger_entry": "credit"
+            "ledger_entry": "credit",
+            "description": "some physical location"
         },
         {
-            "ledger_entry": "debit"
+            "ledger_entry": "debit",
+            "description": "some non-physical location"
+        },
+        {
+            "ledger_entry": "credit",
+            "description": "some physical location"
+        },
+        {
+            "ledger_entry": "debit",
+            "description": "some non-physical location"
         }
     ]
 
 
 def get_mock_cnn(transactions, label_key="CNN"):
+    """Return a mock CNN which appends a parsable merchant name"""
     for trans in transactions:
         trans[label_key] = "joseph - rules"
     return transactions
+
+
+def get_mock_sws(description):
+    """Return a mock SWS classifier which returns true or false depending on the test data"""
+    return description == "some physical location"
