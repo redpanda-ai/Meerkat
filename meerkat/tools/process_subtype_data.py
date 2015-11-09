38367e5959010e1cf2c53f74133417c40d887eb1
Matt Sevrens <msevrens@gmail.com>
2015-11-09 15:11:38 -0800

    adding utility file for processing subtype data

---
+++ meerkat/tools/process_subtype_data.py
@@ -0,0 +1,71 @@
+#!/usr/local/bin/python3.3
+
+"""This module processes the subtype training data into a format
+acceptable for usage with Crepe
+
+Created on Nov 9, 2015
+@author: @author: Matthew Sevrens
+"""
+
+import pandas as pd
+
+import json
+import csv
+
+#################### USAGE ##########################
+
+# python3 process_subtype_data.py [container]
+# python3 process_subtype_data.py card
+
+#####################################################
+
+def load_label_map(filename):
+	"""Load a permanent label map"""
+	input_file = open(filename, encoding='utf-8')
+	label_map = json.loads(input_file.read())
+	input_file.close()
+	return label_map
+
+def process_data(groups, ledger_entry):
+	"""Process and save data"""
+
+	label_map = load_label_map("meerkat/classification/label_maps/" + sys.argv[1] + "_" ledger_entry + "_subtype_label_map.json")
+	label_map = dict(zip(label_map.values(), label_map.keys()))
+	df = groups[ledger_entry]
+	processed = []
+
+	# Process Rows
+	for index, row in df.iterrows():
+		masked_row = {'CLASS_NUM' : label_map[row["PROPOSED_SUBTYPE"]], 
+			  	      'BLANK': "",
+			          'DESCRIPTION': ' '.join(row["DESCRIPTION"].split())
+		}
+		proposed.append(masked_row)
+
+		if row["DESCRIPTION_UNMASKED"] != "":
+			unmasked_row = {'CLASS_NUM' : label_map[row["PROPOSED_SUBTYPE"]], 
+			  	            'BLANK': "",
+			                'DESCRIPTION': ' '.join(row["DESCRIPTION_UNMASKED"].split())
+			}
+			processed.append(unmasked_row)
+
+	# Convert to Dataframe and Shuffle
+	training_data_df = pd.DataFrame(proposed)
+	training_data_df = training_data_df.reindex(np.random.permutation(training_data_df.index))
+
+	# Make Test and Train
+	msk = np.random.rand(len(training_data_df)) < 0.90
+	train = training_data_df[msk]
+	test = training_data_df[~msk]
+
+	# Save
+	file_prefix = sys.argv[1] + "_" + ledger_entry
+	train.to_csv(file_prefix + "_train_subtype.csv", cols=["CLASS_NUM", "BLANK", "DESCRIPTION"], header=False, index=False, index_label=False)
+	test.to_csv(file_prefix + "_test_subtype.csv", cols=["CLASS_NUM", "BLANK", "DESCRIPTION"], header=False, index=False, index_label=False)
+
+df = pd.read_csv("data/input/aggregated_" + sys.argv[1] + "_subtype_training_data.csv", na_filter=False, quoting=csv.QUOTE_NONE, encoding="utf-8", sep='|', error_bad_lines=False)
+grouped = df.groupby('LEDGER_ENTRY', as_index=False)
+groups = dict(list(grouped))
+
+process_data(groups, "credit")
+process_data(groups, "debit")
