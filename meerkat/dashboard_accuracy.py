ac988e7c68854ff861e5bfa37f77ecd6c99c3b93
Joseph Altmaier <joseph.altmaier@gmail.com>
2015-11-10 19:26:05 +0000

    See if Jenkins can run my script at all

--- meerkat/dashboard_accuracy.py
+++ meerkat/dashboard_accuracy.py
@@ -16,44 +16,45 @@ BANK_CREDIT_SUBTYPE_CNN = get_cnn("bank_credit_subtype")
 FOLDER = "development/dashboard/"
 
 def run_from_command_line():
-    keys = __get_data_from_s3()
-    bank_merchant = "1k_labeled_bank_merchant_samples"
-    bank_label_map = "meerkat/classification/label_maps/permanent_bank_label_map.json"
-    bank_cnn_map = "meerkat/classification/label_maps/reverse_bank_label_map.json"
-    bank_merchant_results = CNN_accuracy(bank_merchant, BANK_MERCHANT_CNN, bank_cnn_map, bank_label_map)
-    print_results(bank_merchant_results)
+    print("Joseph is the best")
+    # keys = __get_data_from_s3()
+    # bank_merchant = "1k_labeled_bank_merchant_samples"
+    # bank_label_map = "meerkat/classification/label_maps/permanent_bank_label_map.json"
+    # bank_cnn_map = "meerkat/classification/label_maps/reverse_bank_label_map.json"
+    # bank_merchant_results = CNN_accuracy(bank_merchant, BANK_MERCHANT_CNN, bank_cnn_map, bank_label_map)
+    # print_results(bank_merchant_results)
 
-    card_merchant = "1k_labeled_card_merchant_samples"
-    card_label_map = "meerkat/classification/label_maps/permanent_card_label_map.json"
-    card_cnn_map = "meerkat/classification/label_maps/reverse_card_label_map.json"
-    card_merchant_results = CNN_accuracy(card_merchant, CARD_MERCHANT_CNN, card_cnn_map, card_label_map)
-    print_results(card_merchant_results)
+    # card_merchant = "1k_labeled_card_merchant_samples"
+    # card_label_map = "meerkat/classification/label_maps/permanent_card_label_map.json"
+    # card_cnn_map = "meerkat/classification/label_maps/reverse_card_label_map.json"
+    # card_merchant_results = CNN_accuracy(card_merchant, CARD_MERCHANT_CNN, card_cnn_map, card_label_map)
+    # print_results(card_merchant_results)
 
-    bank_debit_subtype = "1k_labeled_bank_debit_samples"
-    bank_debit_map = load_params("meerkat/classification/label_maps/bank_debit_subtype_label_map.json")
-    bank_debit_reverse_map = __invert_subtype_map(bank_debit_map)
-    bank_debit_results = CNN_accuracy(bank_debit_subtype, BANK_DEBIT_SUBTYPE_CNN, bank_debit_map, bank_debit_reverse_map, label_key="Proposed Subtype")
-    print_results(bank_debit_results)
+    # bank_debit_subtype = "1k_labeled_bank_debit_samples"
+    # bank_debit_map = load_params("meerkat/classification/label_maps/bank_debit_subtype_label_map.json")
+    # bank_debit_reverse_map = __invert_subtype_map(bank_debit_map)
+    # bank_debit_results = CNN_accuracy(bank_debit_subtype, BANK_DEBIT_SUBTYPE_CNN, bank_debit_map, bank_debit_reverse_map, label_key="Proposed Subtype")
+    # print_results(bank_debit_results)
 
-    bank_credit_subtype = "1k_labeled_bank_credit_samples"
-    bank_credit_map = load_params("meerkat/classification/label_maps/bank_credit_subtype_label_map.json")
-    bank_credit_reverse_map = __invert_subtype_map(bank_credit_map)
-    bank_credit_results = CNN_accuracy(bank_credit_subtype, BANK_CREDIT_SUBTYPE_CNN, bank_credit_map, bank_credit_reverse_map, label_key="Proposed Subtype")
-    print_results(bank_credit_results)
+    # bank_credit_subtype = "1k_labeled_bank_credit_samples"
+    # bank_credit_map = load_params("meerkat/classification/label_maps/bank_credit_subtype_label_map.json")
+    # bank_credit_reverse_map = __invert_subtype_map(bank_credit_map)
+    # bank_credit_results = CNN_accuracy(bank_credit_subtype, BANK_CREDIT_SUBTYPE_CNN, bank_credit_map, bank_credit_reverse_map, label_key="Proposed Subtype")
+    # print_results(bank_credit_results)
 
-    card_debit_subtype = "1k_labeled_card_debit_samples"
-    card_debit_map = load_params("meerkat/classification/label_maps/card_debit_subtype_label_map.json")
-    card_debit_reverse_map = __invert_subtype_map(card_debit_map)
-    card_debit_results = CNN_accuracy(card_debit_subtype, CARD_DEBIT_SUBTYPE_CNN, card_debit_map, card_debit_reverse_map, label_key="Proposed Subtype")
-    print_results(card_debit_results)
+    # card_debit_subtype = "1k_labeled_card_debit_samples"
+    # card_debit_map = load_params("meerkat/classification/label_maps/card_debit_subtype_label_map.json")
+    # card_debit_reverse_map = __invert_subtype_map(card_debit_map)
+    # card_debit_results = CNN_accuracy(card_debit_subtype, CARD_DEBIT_SUBTYPE_CNN, card_debit_map, card_debit_reverse_map, label_key="Proposed Subtype")
+    # print_results(card_debit_results)
 
-    card_credit_subtype = "1k_labeled_card_credit_samples"
-    card_credit_map = load_params("meerkat/classification/label_maps/card_credit_subtype_label_map.json")
-    card_credit_reverse_map = __invert_subtype_map(card_credit_map)
-    card_credit_results = CNN_accuracy(card_credit_subtype, CARD_CREDIT_SUBTYPE_CNN, card_credit_map, card_credit_reverse_map, label_key="Proposed Subtype")
-    print_results(card_credit_results)
+    # card_credit_subtype = "1k_labeled_card_credit_samples"
+    # card_credit_map = load_params("meerkat/classification/label_maps/card_credit_subtype_label_map.json")
+    # card_credit_reverse_map = __invert_subtype_map(card_credit_map)
+    # card_credit_results = CNN_accuracy(card_credit_subtype, CARD_CREDIT_SUBTYPE_CNN, card_credit_map, card_credit_reverse_map, label_key="Proposed Subtype")
+    # print_results(card_credit_results)
 
-    __remove_local_data(keys)
+    # __remove_local_data(keys)
 
 def __invert_subtype_map(subtype_map):
     reverse_map = {}
