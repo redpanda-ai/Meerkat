d334ef6a6969c4038b88d1b125ae45637a87c2d5
Tina Wu <diwubupt@gmail.com>
2015-12-15 18:08:30 +0000

    add unit tests for get_unicast_hosts()

--- tests/test_build_backend_layer.py
+++ tests/test_build_backend_layer.py
@@ -1,13 +1,9 @@
-"""Unit test for meerkat.various_tools"""
+"""Unit test for meerkat.build_backend_layer"""
 
-import meerkat.various_tools
-import numpy as np
 import unittest
 import sys
-import json
 import meerkat.build_backend_layer as builder
 
-from jsonschema import validate
 from meerkat.custom_exceptions import InvalidArguments
 from nose_parameterized import parameterized
 
@@ -19,13 +15,16 @@ class BuildBackendLayerTests(unittest.TestCase):
 	"""Our UnitTest class."""
 
 	def test_run_ssh_commands__missing_pem_file(self):
+		""" run_ssh_commands test that pem file is missing """
 		fix = {
 			"key_file" : "tests/missing.pem",
 			"command_list" : ["uname"]
 		}
-		self.assertRaises(Exception, builder.run_ssh_commands, "localhost", fix, "command_list", login="ubuntu")
+		self.assertRaises(Exception, builder.run_ssh_commands, 
+			"localhost", fix, "command_list", login="ubuntu")
 
-	def test_run_ssh_commands__three_commands(self):
+	def test_run_ssh_commands__two_commands(self):
+		""" run_ssh_commands test that two commands are run """
 		fix = {
 			"key_file" : "tests/test.pem",
 			"command_list" : ["uname", "whoami"]
@@ -35,19 +34,22 @@ class BuildBackendLayerTests(unittest.TestCase):
 		self.assertEqual(expected, result)
 
 	def test_run_ssh_commands__normal_use(self):
+		""" run_ssh_commands test that a command is run """
 		fix = {
 			"key_file" : "tests/test.pem",
 			"command_list" : ["uname"]
 		}
-		expected = [ "['Linux\\n']" ]
+		expected = ["['Linux\\n']"]
 		result = builder.run_ssh_commands("localhost", fix, "command_list", login="ubuntu")
 		self.assertEqual(expected, result)
 
 	def test_run_ssh_commands__no_commands(self):
+		""" run_ssh_commands test that command list is empty """
 		fix = {
 			"key_file" : "tests/test.pem",
 		}
-		self.assertRaises(KeyError, builder.run_ssh_commands, "localhost", fix, "command_list", login="ubuntu")
+		self.assertRaises(KeyError, builder.run_ssh_commands, 
+			"localhost", fix, "command_list", login="ubuntu")
 
 	def test_initialize__normal_case(self):
 		""" Assert that params is initialized """
@@ -56,24 +58,25 @@ class BuildBackendLayerTests(unittest.TestCase):
 		self.assertNotEqual(result, None)
 		
 	def test_initialize__invalid_arguments_number(self):
-		""" Assert that when the input argument number is incorrect, InvalidArguments Exception is thrown """
+		""" Assert that when arguments number is invalid, InvalidArguments Exception is thrown """
 		sys.argv = ["meerkat.build_backend_layer", "cluster_name"]
 		self.assertRaises(InvalidArguments, builder.initialize)
 
 	@parameterized.expand([
-		(1, 0, 2, ["instance0", "instance1", "instance2"], (["instance0"],[],["instance1", "instance2"])),
-		(0, 1, 0, ["instance0"], ([],["instance0"],[]))
+		(1, 0, 2, ["instance0", "instance1", "instance2"], 
+			(["instance0"], [], ["instance1", "instance2"])),
+		(0, 1, 0, ["instance0"], ([], ["instance0"], []))
 	])
 	def test_get_instance_lists_parameterized(self, masters, hybrids, slaves, instance_list, expected):
 		"""Assert that instance lists are built correctly."""
 		fix = {
 			"instance_layout" : {
-                "masters" : masters,
-                "hybrids" : hybrids,
-                "slaves" : slaves
-            },
-            "instances" : instance_list
-        }
+				"masters" : masters,
+				"hybrids" : hybrids,
+				"slaves" : slaves
+			},
+			"instances" : instance_list
+		}
 		builder.get_instance_lists(fix)
 		self.assertEqual(fix["masters"], expected[0])
 		self.assertEqual(fix["hybrids"], expected[1])
@@ -96,7 +99,7 @@ class BuildBackendLayerTests(unittest.TestCase):
 		self.assertEqual(fix["slaves"], [])
 
 	def test_get_instance_lists__invalid_count_numbers(self):
-		""" Assert that when sum of count numbers isn't equal to number of instances, ValueError is thrown """
+		""" Assert that when layout sum isn't equal to instances number, ValueError is thrown """
 		fix = {
 			"instance_layout" : {
 				"masters" : 1,
@@ -126,6 +129,20 @@ class BuildBackendLayerTests(unittest.TestCase):
 		expected = ''
 		builder.get_master_ip_list(fix)
 		self.assertEqual(expected, fix["master_ip_list"])
+
+	@parameterized.expand([
+		([Master("1.2.3.4"), Master("5.6.7.8")], 1, '"1.2.3.4"'),
+		([], 1, ''),
+		([Master("1.2.3.4"), Master("5.6.7.8"), Master("localhost")], 2, '"1.2.3.4", "5.6.7.8"')
+	])
+	def test_get_unicast_hosts__parameterized(self, instances, min_nodes, expected):
+		""" Assert that a list of unicast hosts are created """
+		fix = {
+			"instances" : instances,
+			"minimum_master_nodes" : min_nodes
+		}
+		builder.get_unicast_hosts(fix)
+		self.assertEqual(fix["unicast_hosts"], expected)
 
 if __name__ == '__main__':
 	unittest.main()
