41a716d34a48784cdd90757f5bb2e390beb92843
opan <oscarpan@bu.edu>
2015-12-15 01:35:10 +0000

    create tests for acquire_instances

--- tests/test_bulid_backend_layer.py
+++ tests/test_bulid_backend_layer.py
@@ -14,35 +14,70 @@ def delete_security_group(ec2_conn, sg_id, vpc_id):
 	if len(security_group) != 0:
 		security_group[0].delete()
 
+def delete_instance(ec2_conn, instance_id):
+	'delelet A SINGLE instance that serves for unit tests'
+	ec2_conn.terminate_instances(instance_ids=[instance_id])
 
 class BuildBackendLayerTests(unittest.TestCase):
 	"""Our UnitTest class."""
 
 	@classmethod
 	def setUpClass(cls):
-		'create necessary objects that might be used in multiple test cases'
+		"""create necessary objects that might be used in multiple test cases"""
 		cls.default_region, cls.default_vpc_id = 'us-west-2', 'vpc-a930dacc'
 		cls.my_region = boto.ec2.get_region(cls.default_region)
 		cls.ec2_conn = EC2Connection(region=cls.my_region)
+		cls.hvm_other_linux_ami = 'ami-f5e4a3c5'
+		cls.default_subnet_id = 'subnet-e58d7580'
+		cls.default_availability_zone = 'us-west-2b'
+		cls.default_sg = cls.ec2_conn.get_all_security_groups(
+			filters={'group-name':'default', 'vpc-id': cls.default_vpc_id})
+
+	"""
+	def test_acquire_instances__normal_use(self):
+		
+		This test creates a new instance and security group every time 
+		we run it. It finishes when the new instance is running. This test
+		cost about $0.01 - $0.26, depending on the instance type
+		
+		params = {'ami-id': self.hvm_other_linux_ami, 'key_name': 'jkey',
+			'instance_type': 't2.micro', 'placement': self.default_availability_zone,
+			'subnet-id': self.default_subnet_id, 'instance_layout': {'hybrids': 1,
+			'masters': 0, 'slaves': 0}, 'all_security_groups': self.default_sg}
+		expected_owner_id = '003144629351'
+		reservation = builder.acquire_instances(self.ec2_conn, params)
+		self.assertEqual(reservation.owner_id, expected_owner_id)
+		delete_instance(self.ec2_conn, reservations.instances[0].id)
+	"""
+	def test_acquire_instances__wrong_virtualization(self):
+		"""using a non-hvm ami to create a t2.micro instance"""
+		params = {'ami-id': 'ami-cf707eff', 'key_name': 'jkey',
+			'instance_type': 't2.micro', 'placement': self.default_availability_zone,
+			'subnet-id': self.default_subnet_id, 'instance_layout': {'hybrids': 1,
+			'masters': 0, 'slaves': 0}, 'all_security_groups': self.default_sg}
+		self.assertRaises(SystemExit, builder.acquire_instances, self.ec2_conn, params)
+
 
 	def test_create_security_group__normal_use(self):
 		test_sg_id = 'dummy_sg_' + str(uuid.uuid4())
 		expected = test_sg_id
-		result = builder.create_security_group(self.ec2_conn, test_sg_id, self.default_vpc_id)
+		result = builder.create_security_group(self.ec2_conn, test_sg_id,
+			 self.default_vpc_id)
 		self.assertEqual(expected, result.name)
 		delete_security_group(self.ec2_conn, test_sg_id, self.default_vpc_id)
 
 	def test_create_security_group__using_existing_sg_name(self):
 		test_sg_id = 'dummy_sg_' + str(uuid.uuid4())
-		_ = builder.create_security_group(self.ec2_conn, test_sg_id, self.default_vpc_id)
+		_ = builder.create_security_group(self.ec2_conn, test_sg_id,
+			 self.default_vpc_id)
 		self.assertRaises(SystemExit, builder.create_security_group,
-						  self.ec2_conn, test_sg_id, self.default_vpc_id)
+			self.ec2_conn, test_sg_id, self.default_vpc_id)
 		delete_security_group(self.ec2_conn, test_sg_id, self.default_vpc_id)
 
 	def test_create_security_group__invalid_vpc_id(self):
 		test_sg_id = 'dummy_sg_' + str(uuid.uuid4())
 		self.assertRaises(SystemExit, builder.create_security_group, self.ec2_conn,
-						  test_sg_id, 'invalid_vpc_id')
+			test_sg_id, 'invalid_vpc_id')
 
 	def test_run_ssh_commands__missing_pem_file(self):
 		fix = {
