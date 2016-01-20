efca940c925e2acb60cb237dc71be3012ee5af42
Oscar Pan <oscarpan@bu.edu>
2016-01-20 04:49:50 +0000

    apply_CNN now also produces CNN performance stats. such as precision, recall, true +/- and cm.

--- meerkat/tools/apply_CNN.py
+++ meerkat/tools/apply_CNN.py
@@ -33,6 +33,7 @@ import sys
 import csv
 import json
 import os
+import numpy as np
 
 from meerkat.classification.lua_bridge import get_cnn_by_path
 
@@ -43,7 +44,7 @@ default_human_label_key = 'PROPOSED_SUBTYPE'
 
 def compare_label(*args, **kwargs):
 	"""similar to generic_test in accuracy.py, with unnecessary items dropped"""
-	machine, cnn_column, human_column = args[:]
+	machine, cnn_column, human_column, cm = args[:]
 	doc_key = kwargs.get("doc_key", default_doc_key)
 
 	# Create Quicker Lookup
@@ -79,6 +80,11 @@ def compare_label(*args, **kwargs):
 		mislabeled.append([human_row[doc_key], human_row[human_column],
 			machine_row[cnn_column]])
 		"""
+		# Update cm
+		row = machine_row['LABEL'] - 1
+		column = machine_row['PREDICTED_LABEL'] - 1
+		cm[row][column] += 1
+
 		if machine_row[cnn_column] == "":
 			unpredicted.append([machine_row[doc_key], machine_row[human_column]])
 			continue
@@ -90,7 +96,7 @@ def compare_label(*args, **kwargs):
 			continue
 		mislabeled.append([machine_row[doc_key], machine_row[human_column],
 			machine_row[cnn_column]])
-	return mislabeled, correct, unpredicted, needs_hand_labeling
+	return mislabeled, correct, unpredicted, needs_hand_labeling, cm
 
 def make_unique_ID(row):
 	"""assign each transaction a unique ID"""
@@ -137,9 +143,9 @@ for chunk in reader:
 			continue
 		item['PREDICTED_LABEL'] = reversed_label_map[item[default_machine_label_key]]
 
-	mislabeled, correct, unpredicted, needs_hand_labeling =\
+	mislabeled, correct, unpredicted, needs_hand_labeling, confusion_matrix =\
 		compare_label(machine_labeled, default_machine_label_key,
-		default_human_label_key, doc_key=default_doc_key)
+		default_human_label_key, confusion_matrix, doc_key=default_doc_key)
 
 	# Save
 	# Check if data/CNN_stats/ esxists, if not creste one
@@ -165,4 +171,30 @@ for chunk in reader:
 		df = pd.DataFrame(needs_hand_labeling)
 		df.to_csv('data/CNN_stats/need_labeling.csv', mode='a', index=False,
 			header=['TRANSACTION_DESCRIPTION'])
+
+# calculate recall, precision, false +/-, true +/- from confusion maxtrix
+true_positive = pd.DataFrame([confusion_matrix[i][i] for i in range(num_labels)])
+cm = pd.DataFrame(confusion_matrix)
+actual = pd.DataFrame(cm.sum(axis=1))
+recall = true_positive / actual
+#if we use pandas 0.17 we can do the rounding neater
+recall = np.round(recall, decimals=4)
+column_sum = pd.DataFrame(cm.sum())
+false_positive = column_sum - true_positive
+precision = true_positive / column_sum
+precision = np.round(precision, decimals=4)
+misclassification = actual - true_positive
+label = pd.DataFrame(pd.read_json(sys.argv[2], typ='series')).sort_index()
+label.index = range(num_labels)
+
+stat = pd.concat([label, actual, true_positive, false_positive, recall, precision,
+	misclassification], axis=1)
+stat.columns = ['Class', 'Actual', 'True_Positive', 'False_Positive', 'Recall',
+	'Precision', 'False_Negative']
+
+cm = pd.concat([label, cm], axis=1)
+cm.columns = ['Class'] + [str(x) for x in range(num_labels)]
+
+stat.to_csv('data/CNN_stats/CNN_stat.csv', index=False)
+cm.to_csv('data/CNN_stats/Con_Matrix.csv')
 
