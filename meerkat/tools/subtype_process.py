a269f1b7734aaf9a847c48ff66c45d57aa36775c
Oscar Pan <oscarpan@bu.edu>
2016-01-21 03:13:15 +0000

    return subtypes in the formats of the existing ones.

--- meerkat/tools/subtype_process.py
+++ meerkat/tools/subtype_process.py
@@ -19,6 +19,7 @@ import os
 import sys
 import json
 import csv
+import string
 
 import numpy as np
 import pandas as pd
@@ -36,6 +37,14 @@ def load_label_map(filename):
 	input_file.close()
 	return label_map
 
+def cap_first_letter(label):
+	"""Make sure the first letter of each word is capitalized"""
+	temp = label.split()
+	for i in range(len(temp)):
+		if temp not in ['by', 'with']:
+			temp[i] = temp[i][0].upper() + temp[i][1:]
+	return ' '.join(word for word in temp)
+
 training_data = []
 
 # Load Data
@@ -46,7 +55,8 @@ df['LEDGER_ENTRY'] = df['LEDGER_ENTRY'].str.lower()
 grouped = df.groupby('LEDGER_ENTRY', as_index=False)
 groups = dict(list(grouped))
 df = groups[sys.argv[2]]
-df["PROPOSED_SUBTYPE"] = df["PROPOSED_SUBTYPE"].str.strip().str.lower()
+df["PROPOSED_SUBTYPE"] = df["PROPOSED_SUBTYPE"].str.strip()
+df['PROPOSED_SUBTYPE'] = df['PROPOSED_SUBTYPE'].apply(cap_first_letter)
 class_names = df["PROPOSED_SUBTYPE"].value_counts().index.tolist()
 
 # Create a label map
@@ -58,12 +68,12 @@ a = lambda x: label_map[x["PROPOSED_SUBTYPE"]]
 df['LABEL'] = df.apply(a, axis=1)
 
 # Replace Description_Unmasked
-def b(x):
-	if x["DESCRIPTION_UNMASKED"] == "":
-		return x["DESCRIPTION"]
+def fill_description(row):
+	if row["DESCRIPTION_UNMASKED"] == "":
+		return row["DESCRIPTION"]
 	else:
-		return x["DESCRIPTION_UNMASKED"]
-df["DESCRIPTION_UNMASKED"] = df.apply(b, axis=1)
+		return row["DESCRIPTION_UNMASKED"]
+df["DESCRIPTION_UNMASKED"] = df.apply(fill_description, axis=1)
 
 # Make Test and Train
 training_data_df = df[['LABEL', 'DESCRIPTION_UNMASKED']]
@@ -75,19 +85,18 @@ test_full = df[~msk]
 
 # Save
 # Check if a dir exsits, if not create one
-if not os.path.exists('data/preprocessed/'):
-	os.makedirs('data/preprocessed/')
+os.makedirs('data/preprocessedd/', exist_ok=True)
 
 label_map = dict(zip(label_map.values(), label_map.keys()))
-dict_2_json(label_map, 'data/preprocessed/' + sys.argv[2] + "_subtype_label_map.json")
-train.to_csv('data/preprocessed/' + sys.argv[2] + "_train_subtype.csv",
+dict_2_json(label_map, 'data/preprocessedd/' + sys.argv[2] + "_subtype_label_map.json")
+train.to_csv('data/preprocessedd/' + sys.argv[2] + "_train_subtype.csv",
 	 cols=["LABEL", "DESCRIPTION_UNMASKED"], header=False, index=False,
 	 index_label=False)
-test.to_csv('data/preprocessed/' + sys.argv[2] + "_test_subtype.csv",
+test.to_csv('data/preprocessedd/' + sys.argv[2] + "_test_subtype.csv",
 	 cols=["LABEL", "DESCRIPTION_UNMASKED"], header=False, index=False,
 	 index_label=False)
 
-test_full.to_csv('data/preprocessed/' + sys.argv[2] + "_test_subtype_full.csv",
+test_full.to_csv('data/preprocessedd/' + sys.argv[2] + "_test_subtype_full.csv",
 	 index=False, sep='|')
-train_full.to_csv('data/preprocessed/' + sys.argv[2] + "_train_subtype_full.csv",
+train_full.to_csv('data/preprocessedd/' + sys.argv[2] + "_train_subtype_full.csv",
 	 index=False, sep='|')
