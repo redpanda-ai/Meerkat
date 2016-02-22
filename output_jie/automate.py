9a1eaa5e1bd071c61f8280e3352c8dbdf2d647d2
Jie Zhang <jzhang@yodlee.com>
2016-02-22 23:13:09 +0000

    Add the latest t7b file.

--- output_jie/automate.py
+++ output_jie/automate.py
@@ -3,13 +3,19 @@ import logging
 from plumbum import local
 
 def getFile():
+	"""Get the latest t7b file under current directory"""
 	logging.info("Get the latest main_*.t7b file")
-	command = local["ls"]["-Falt"] | local["grep"]["main"] | local["head"]["-n"]["1"]
+	command = local["ls"]["-Falt"] \
+			| local["grep"]["main"] \
+			| local["head"]["-n"]["1"] \
+			| local["awk"]["{print $9}"]
+
 	latestFile = command()
-	print(latestFile)
+	return latestFile
 
 def main_stream():
-	getFile()
+	latest_t7b = getFile()
+	print(latest_t7b)
 
 if __name__ == "__main__":
 	main_stream()
