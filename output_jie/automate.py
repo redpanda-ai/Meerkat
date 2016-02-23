e3aade65763f8da9fbe60ab08dde65b01d17a400
Jie Zhang <jzhang@yodlee.com>
2016-02-23 03:03:43 +0000

    Load eras.

--- output_jie/automate.py
+++ output_jie/automate.py
@@ -4,6 +4,7 @@ import re
 import json
 import logging
 from plumbum import local
+import pandas as pd
 
 def getFile():
 	"""Get the latest t7b file under current directory"""
@@ -29,37 +30,25 @@ def executeLuaFile(luaFile):
 	command()
 
 def loadStaticsToMap(filename):
-	"""Load the training statics to a hashMap"""
+	"""Load the training statics to a list of dictionaries"""
 	key_val_re = re.compile("\s*([a-zA-z]+)\s:\s*(.+)")
 	index_re = re.compile("\s*([0-9]+)\s:\s*(.+)")
 	clean_re = re.compile(r'\x1b[^m]*m')
+	# A list of dictionaries.
 	eras = []
+
 	my_era = {}
 	with open(filename, encoding="utf-8") as input_file:
 		for line in input_file:
-			#print(line)
 			if key_val_re.match(line):
 				m = key_val_re.search(line)
 				my_era[m.group(1)] = clean_re.sub('', m.group(2))
-				#print('"{0}" : "{1}"'.format(m.group(1), m.group(2)))
 			elif index_re.match(line):
 				if len(my_era) != 0:
 					eras.append(my_era)
 				my_era = {}
-	import pandas as pd
 
-
-	my_dfs = []
-	for c in range(len(eras)):
-		print("Era {0}".format(c+1))
-		print(eras[c])
-	#print(result)
-
-	sys.exit()
-#		return json.loads(input_file.read())
-	#inputfile = open(filename, encoding='utf-8')
-	#hashmap = json.load(inputfile.read())
-	#return hashmap
+	return eras
 
 def main_stream():
 	"""The main program"""
@@ -67,7 +56,9 @@ def main_stream():
 	writeToLuaFile(latest_t7b, "output_statics.lua")
 	executeLuaFile("output_statics.lua")
 
-	hashmap = loadStaticsToMap("staticsJsonFile")
+	eras = loadStaticsToMap("staticsJsonFile")
+	print(len(eras))
+	print(eras[0])
 
 if __name__ == "__main__":
 	main_stream()
