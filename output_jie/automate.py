45bcb6a99e0d2f21893b76f9ca0d6a3d0a71d24d
Jie Zhang <jzhang@yodlee.com>
2016-02-23 02:52:45 +0000

    Load statics to a dictionary instead a json file.

--- output_jie/automate.py
+++ output_jie/automate.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import re
 import json
 import logging
 from plumbum import local
@@ -23,23 +25,49 @@ def writeToLuaFile(inputFileName, outputLuaFile):
 
 def executeLuaFile(luaFile):
 	"""Execute the input lua file"""
-	command = local["th"][luaFile] > "staticsJsonFile.json"
+	command = local["th"][luaFile] > "staticsJsonFile"
 	command()
 
 def loadStaticsToMap(filename):
 	"""Load the training statics to a hashMap"""
-	inputfile = open(filename, encoding='utf-8')
-	hashmap = json.load(inputfile.read())
-	return hashmap
+	key_val_re = re.compile("\s*([a-zA-z]+)\s:\s*(.+)")
+	index_re = re.compile("\s*([0-9]+)\s:\s*(.+)")
+	clean_re = re.compile(r'\x1b[^m]*m')
+	eras = []
+	my_era = {}
+	with open(filename, encoding="utf-8") as input_file:
+		for line in input_file:
+			#print(line)
+			if key_val_re.match(line):
+				m = key_val_re.search(line)
+				my_era[m.group(1)] = clean_re.sub('', m.group(2))
+				#print('"{0}" : "{1}"'.format(m.group(1), m.group(2)))
+			elif index_re.match(line):
+				if len(my_era) != 0:
+					eras.append(my_era)
+				my_era = {}
+	import pandas as pd
+
+
+	my_dfs = []
+	for c in range(len(eras)):
+		print("Era {0}".format(c+1))
+		print(eras[c])
+	#print(result)
+
+	sys.exit()
+#		return json.loads(input_file.read())
+	#inputfile = open(filename, encoding='utf-8')
+	#hashmap = json.load(inputfile.read())
+	#return hashmap
 
 def main_stream():
 	"""The main program"""
 	latest_t7b = getFile()
-
 	writeToLuaFile(latest_t7b, "output_statics.lua")
 	executeLuaFile("output_statics.lua")
 
-	loadStaticsToMap("staticsJsonFile.json")
+	hashmap = loadStaticsToMap("staticsJsonFile")
 
 if __name__ == "__main__":
 	main_stream()
