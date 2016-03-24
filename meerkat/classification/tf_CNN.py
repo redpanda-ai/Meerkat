ed3b037a722348379560981dbe7f380b49ca65ec
Matt Sevrens <msevrens@gmail.com>
2016-03-24 13:35:54 -0700

    a bunch of pylint fixes

--- meerkat/classification/tf_CNN.py
+++ meerkat/classification/tf_CNN.py
@@ -14,16 +14,15 @@ Created on Mar 14, 2016
 
 ##################################################
 
+import sys
+import csv
+import math
+import random
+
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 
-import sys
-import csv
-import json
-import math
-import random
-from math import sqrt
 from .verify_data import load_json
 from .tools import fill_description_unmasked, reverse_map
 
@@ -49,9 +48,10 @@ ALPHABET_LENGTH = len(ALPHABET)
 EPOCHS = CONFIG["epochs"]
 ERAS = CONFIG["eras"]
 
-def chunks(l, n):
-    n = max(1, n)
-    return [l[i:i + n] for i in range(0, len(l), n)]
+def chunks(array, num):
+	"""Chunk array into equal sized parts"""
+	num = max(1, num)
+	return [array[i:i + num] for i in range(0, len(array), num)]
 
 def validate_config():
 	"""Validate input configuration"""
@@ -67,7 +67,7 @@ def load_data():
 	"""Load data and label map"""
 
 	reversed_map = reverse_map(LABEL_MAP)
-	a = lambda x: reversed_map.get(str(x["PROPOSED_SUBTYPE"]), "")
+	map_labels = lambda x: reversed_map.get(str(x["PROPOSED_SUBTYPE"]), "")
 
 	df = pd.read_csv(DATASET, quoting=csv.QUOTE_NONE, na_filter=False, encoding="utf-8", sep='|', error_bad_lines=False)
 
@@ -77,7 +77,7 @@ def load_data():
 	df = groups[LEDGER_ENTRY]
 	df["DESCRIPTION_UNMASKED"] = df.apply(fill_description_unmasked, axis=1)
 	df = df.reindex(np.random.permutation(df.index))
-	df["LABEL_NUM"] = df.apply(a, axis=1)
+	df["LABEL_NUM"] = df.apply(map_labels, axis=1)
 	df = df[df["LABEL_NUM"] != ""]
 
 	msk = np.random.rand(len(df)) < 0.90
@@ -119,7 +119,7 @@ def mixed_batching(df, groups_train):
 	"""Batch from train data using equal class batching"""
 	half_batch = int(BATCH_SIZE / 2)
 	indices_to_sample = list(np.random.choice(df.index, half_batch))
-	for i in range(half_batch):
+	for index in range(half_batch):
 		label = random.randint(1, NUM_LABELS)
 		select_group = groups_train[str(label)]
 		indices_to_sample.append(np.random.choice(select_group.index, 1)[0])
@@ -133,22 +133,22 @@ def batch_to_tensor(batch):
 	labels = np.array(batch["LABEL_NUM"].astype(int)) - 1
 	labels = (np.arange(NUM_LABELS) == labels[:,None]).astype(np.float32)
 	docs = batch["DESCRIPTION_UNMASKED"].tolist()
-	trans = np.zeros(shape=(BATCH_SIZE, 1, ALPHABET_LENGTH, DOC_LENGTH))
+	transactions = np.zeros(shape=(BATCH_SIZE, 1, ALPHABET_LENGTH, DOC_LENGTH))
 	
-	for i, t in enumerate(docs):
-		trans[i][0] = string_to_tensor(t, DOC_LENGTH)
+	for index, trans in enumerate(docs):
+		transactions[index][0] = string_to_tensor(trans, DOC_LENGTH)
 
-	trans = np.transpose(trans, (0, 1, 3, 2))
-	return trans, labels
+	transactions = np.transpose(transactions, (0, 1, 3, 2))
+	return transactions, labels
 
-def string_to_tensor(doc, l):
+def string_to_tensor(doc, length):
 	"""Convert transaction to tensor format"""
-	s = doc.lower()[0:l]
-	t = np.zeros((len(ALPHABET), l), dtype=np.float32)
-	for i, c in reversed(list(enumerate(s))):
-		if c in ALPHABET:
-			t[ALPHA_DICT[c]][len(s) - i - 1] = 1
-	return t
+	doc = doc.lower()[0:length]
+	tensor = np.zeros((len(ALPHABET), length), dtype=np.float32)
+	for index, char in reversed(list(enumerate(doc))):
+		if char in ALPHABET:
+			tensor[ALPHA_DICT[char]][len(doc) - index - 1] = 1
+	return tensor
 	
 def get_tensor(graph, name):
 	"""Get tensor by name"""
@@ -165,18 +165,20 @@ def get_variable(graph, name):
 		return variable
 
 def accuracy(predictions, labels):
+	"""Return accuracy for a batch"""
 	return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0])
 
 def threshold(tensor):
+	"""ReLU with threshold at 1e-6"""
 	return tf.mul(tf.to_float(tf.greater_equal(tensor, 1e-6)), tensor)
 
-def softmax_with_temperature(tensor, T):
+def softmax_with_temperature(tensor, temperature):
 	"""Softmax with temperature variable"""
-	return tf.div(tf.exp(tensor/T), tf.reduce_sum(tf.exp(tensor/T)))
+	return tf.div(tf.exp(tensor/temperature), tf.reduce_sum(tf.exp(tensor/temperature)))
 
 def bias_variable(shape, mult):
 	"""Initialize biases"""
-	stdv = 1 / sqrt(mult)
+	stdv = 1 / math.sqrt(mult)
 	bias = tf.Variable(tf.random_uniform(shape, minval=-stdv, maxval=stdv), name="B")
 	return bias
 
@@ -185,9 +187,9 @@ def weight_variable(shape):
 	weight = tf.Variable(tf.mul(tf.random_normal(shape), RANDOMIZE), name="W")
 	return weight
 
-def conv2d(x, W):
+def conv2d(input_x, weights):
 	"""Create convolutional layer"""
-	layer = tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='VALID')
+	layer = tf.nn.conv2d(input_x, weights, strides=[1, 1, 1, 1], padding='VALID')
 	return layer
 
 def max_pool(x):
@@ -208,50 +210,51 @@ def build_graph():
 		x = tf.placeholder(tf.float32, shape=[BATCH_SIZE, 1, DOC_LENGTH, ALPHABET_LENGTH], name="x")
 		y = tf.placeholder(tf.float32, shape=(BATCH_SIZE, NUM_LABELS), name="y")
 		
-		W_conv1 = weight_variable([1, 7, ALPHABET_LENGTH, 256])
+		w_conv1 = weight_variable([1, 7, ALPHABET_LENGTH, 256])
 		b_conv1 = bias_variable([256], 7 * ALPHABET_LENGTH)
 
-		W_conv2 = weight_variable([1, 7, 256, 256])
+		w_conv2 = weight_variable([1, 7, 256, 256])
 		b_conv2 = bias_variable([256], 7 * 256)
 
-		W_conv3 = weight_variable([1, 3, 256, 256])
+		w_conv3 = weight_variable([1, 3, 256, 256])
 		b_conv3 = bias_variable([256], 3 * 256)
 
-		W_conv4 = weight_variable([1, 3, 256, 256])
+		w_conv4 = weight_variable([1, 3, 256, 256])
 		b_conv4 = bias_variable([256], 3 * 256)
 
-		W_conv5 = weight_variable([1, 3, 256, 256])
+		w_conv5 = weight_variable([1, 3, 256, 256])
 		b_conv5 = bias_variable([256], 3 * 256)
 
-		W_fc1 = weight_variable([RESHAPE, 1024])
+		w_fc1 = weight_variable([RESHAPE, 1024])
 		b_fc1 = bias_variable([1024], RESHAPE)
 
-		W_fc2 = weight_variable([1024, NUM_LABELS])
+		w_fc2 = weight_variable([1024, NUM_LABELS])
 		b_fc2 = bias_variable([NUM_LABELS], 1024)
 
 		def model(data, name, train=False):
+			"""Add model layers to the graph"""
 
-			h_conv1 = threshold(conv2d(data, W_conv1) + b_conv1)
+			h_conv1 = threshold(conv2d(data, w_conv1) + b_conv1)
 			h_pool1 = max_pool(h_conv1)
 
-			h_conv2 = threshold(conv2d(h_pool1, W_conv2) + b_conv2)
+			h_conv2 = threshold(conv2d(h_pool1, w_conv2) + b_conv2)
 			h_pool2 = max_pool(h_conv2)
 
-			h_conv3 = threshold(conv2d(h_pool2, W_conv3) + b_conv3)
+			h_conv3 = threshold(conv2d(h_pool2, w_conv3) + b_conv3)
 
-			h_conv4 = threshold(conv2d(h_conv3, W_conv4) + b_conv4)
+			h_conv4 = threshold(conv2d(h_conv3, w_conv4) + b_conv4)
 
-			h_conv5 = threshold(conv2d(h_conv4, W_conv5) + b_conv5)
+			h_conv5 = threshold(conv2d(h_conv4, w_conv5) + b_conv5)
 			h_pool5 = max_pool(h_conv5)
 
 			h_reshape = tf.reshape(h_pool5, [BATCH_SIZE, RESHAPE])
 
-			h_fc1 = threshold(tf.matmul(h_reshape, W_fc1) + b_fc1)
+			h_fc1 = threshold(tf.matmul(h_reshape, w_fc1) + b_fc1)
 
 			if train:
 				h_fc1 = tf.nn.dropout(h_fc1, 0.5)
 
-			h_fc2 = tf.matmul(h_fc1, W_fc2) + b_fc2
+			h_fc2 = tf.matmul(h_fc1, w_fc2) + b_fc2
 
 			softmax = tf.nn.softmax(h_fc2)
 			network = tf.log(tf.clip_by_value(softmax, 1e-10, 1.0), name=name)
@@ -268,7 +271,7 @@ def build_graph():
 
 	return graph, saver
 
-def train_model(sess, train, test, groups_train, chunked_test):
+def train_model(graph, sess, saver, train, test, groups_train, chunked_test):
 	"""Train the model"""
 
 	num_eras = EPOCHS * ERAS
@@ -284,23 +287,23 @@ def train_model(sess, train, test, groups_train, chunked_test):
 		sess.run(get_op(graph, "optimizer"), feed_dict=feed_dict)
 
 		# Log Loss
-		if (step % 50 == 0):
+		if step % 50 == 0:
 			print("train loss at epoch %d: %g" % (step + 1, sess.run(get_tensor(graph, "loss:0"), feed_dict=feed_dict)))
 
 		# Evaluate Testset and Log Progress
-		if (step != 0 and step % EPOCHS == 0):
+		if step != 0 and step % EPOCHS == 0:
 			model = get_tensor(graph, "model:0")
-			lr = get_variable(graph, "lr:0")
+			learning_rate = get_variable(graph, "lr:0")
 			predictions = sess.run(model, feed_dict=feed_dict)
 			print("Testing for era %d" % (step / EPOCHS))
-			print("Learning rate at epoch %d: %g" % (step + 1, sess.run(lr)))
+			print("Learning rate at epoch %d: %g" % (step + 1, sess.run(learning_rate)))
 			print("Minibatch accuracy: %.1f%%" % accuracy(predictions, labels))
 			evaluate_testset(graph, test, chunked_test, model, sess)
 
 		# Update Learning Rate
-		if (step != 0 and step % 15000 == 0):
-			lr = get_variable(graph, "lr:0")
-			sess.run(lr.assign(lr / 2))
+		if step != 0 and step % 15000 == 0:
+			learning_rate = get_variable(graph, "lr:0")
+			sess.run(learning_rate.assign(learning_rate / 2))
 
 	# Save Model  
 	save_path = saver.save(sess, "meerkat/classification/models/model_" + DATASET.split(".")[0] + ".ckpt")
@@ -316,13 +319,17 @@ def run_session(graph, saver):
 		tf.initialize_all_variables().run()
 
 		if MODE == "train":
-			train_model(sess, train, test, groups_train, chunked_test)
+			train_model(graph, sess, saver, train, test, groups_train, chunked_test)
 		elif MODE == "test":
 			saver.restore(sess, MODEL)
 			model = get_tensor(graph, "model:0")
 			evaluate_testset(graph, test, chunked_test, model, sess)
 
-if __name__ == "__main__":
+def run_from_command_line():
+	"""Run module from command line"""
 	validate_config()
 	graph, saver = build_graph()
 	run_session(graph, saver)
+
+if __name__ == "__main__":
+	run_from_command_line()
