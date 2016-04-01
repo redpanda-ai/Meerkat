b86bbdb72e66a33e4dd94ad6bf0f3cf11d6f65fa
Tina Wu <diwubupt@gmail.com>
2016-04-01 21:11:45 +0000

    ensure prefix ended with a  '/'

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,11 +1,21 @@
-"""Unit test for tools files in meerkat/tools"""
+"""Unit test for meerkat/classification/tools.py"""
 
-from pprint import pformat
 import unittest
+import meerkat.classification.tools as tools
+from nose_parameterized import parameterized
 
-class VariousToolsTests(unittest.TestCase):
-
+class ToolsTests(unittest.TestCase):
 	"""Our UnitTest class."""
 
-if __name__ == '__main__':
-	unittest.main()
+	@parameterized.expand([
+		(["input file not exist", {"bucket": "s3yodlee", "prefix": "Meerkat_tests_fixture/"}]),
+		(["input file not exist", {"bucket": "s3yodlee", "prefix": "Meerkat_tests_fixture"}])
+	])
+	def test_pull_from_s3(self, case_type, s3params):
+		"""Test check_new_input_file"""
+		if case_type == "input file not exist":
+			self.assertRaises(SystemExit, tools.check_new_input_file, **s3params)
+		#else:
+		#	self.assertEqual(tools.pull_from_s3(**inputs), "tests/fixture/csv_file_1.csv")
+		#	local["rm"]["tests/fixture/csv_file_1.csv"]()
+
