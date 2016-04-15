47618ecf617b560bb67cd190a690580cb8e0a30a
J. Andrew Key <joeandrewkey@gmail.com>
2016-04-15 01:01:25 +0000

    Adding logs.

--- meerkat/classification/patch.py
+++ meerkat/classification/patch.py
@@ -1,6 +1,7 @@
 """This module will update Meerkat's models from S3"""
 
 import boto
+import logging
 import re
 import tarfile
 import pandas as pd
@@ -31,42 +32,23 @@ def get_peer_models(candidate_dictionary, prefix=None):
 				results[model_type] = []
 			results[model_type].append(timestamp)
 		else:
-			print("Not Found")
+			logging.warning("Not Found")
 	return results
-
-def get_f_measure(stats_file):
-	"""Evaluates a single stats_file and returns the f_measure"""
-	df = pd.read_csv(stats_file)
-	print(df)
-	true_pos, false_neg = df["True_Positive"], df["False_Negative"]
-	sums = df.sum(axis=0)
-	#FIXME: I don't think this calcuation is meaningful, double-check it
-	precision = sums["True_Positive"] / (sums["True_Positive"] + sums["False_Positive"])
-	recall = sums["True_Positive"] / (sums["True_Positive"] + sums["False_Negative"])
-	f_measure = 2 * precision * recall / (precision + recall)
-	result_format = "{0:<12}: {1:0.5f}"
-	#print("Sums {0}".format(sums))
-	#print(result_format.format("Precision", precision))
-	#print(result_format.format("Recall", recall))
-	#print(result_format.format("F-measure", f_measure))
-	return f_measure
 
 def show_dataframe(name, df):
 	"""Display information about a dataframe."""
-	print("{0}\n{1},\nShape {2},\nSize {3}".format(name, df, df.shape, df.size))
+	logging.warning("{0}\n{1},\nShape {2},\nSize {3}".format(name, df, df.shape, df.size))
 
-def get_true_positive(stats_file):
+def get_model_accuracy(stats_file):
 	"""Gets the true positive for a particular confusion matrix"""
 	df = pd.read_csv(stats_file)
 	#drop columns 0, 1, and -1 to make a square confusion matrix
 	df = df.drop(df.columns[[0,1,-1]], axis=1)
 	#Reset columns names, which are off by one
 	df.rename(columns=lambda x: int(x)-1, inplace=True)
-	show_dataframe("Confusion Matrix", df)
 	#get the diagonal of true positives as a vector
 	rows, _ = df.shape
-
-	#Calculate - part 1
+	#First order calculations
 	true_positive = pd.DataFrame(df.iat[i,i] for i in range(rows))
 	col_sum = pd.DataFrame(df.sum(axis=1))
 	false_positive = pd.DataFrame(pd.DataFrame(df.sum(axis=0)).values - true_positive.values,
@@ -75,35 +57,26 @@ def get_true_positive(stats_file):
 		columns=true_positive.columns)
 	true_negative = pd.DataFrame(
 		[df.drop(i, axis=1).drop(i, axis=0).sum().sum() for i in range(rows)])
-
-	show_dataframe("True Positive", true_positive)
-	show_dataframe("True Negative", true_negative)
-	show_dataframe("False Positive", false_positive)
-	show_dataframe("False Negative", false_negative)
-
-	#Calculate - part 2
-	#FIXME: I think cnn_stats has incorrectly swapped the formula for precision and recall.
+	#Second order calculations
 	accuracy = true_positive.sum() / df.sum().sum()
 	precision = true_positive / (true_positive + false_positive)
 	recall = true_positive / (true_positive + false_negative)
 	specificity = true_negative / (true_negative + false_positive)
-	show_dataframe("Precision", precision)
-	show_dataframe("Recall", recall)
-	show_dataframe("Accuracy", accuracy)
-	show_dataframe("Specificity", specificity)
-
-	return true_positive, false_positive, false_negative, true_negative
+	#Third order calculations
+	f_measure = 2 * precision * recall / (precision + recall)
+	#Return the model accuracy, which is all we care about, actually
+	return accuracy.values[0]
 
 def get_stats_file(target, stats_file):
 	"""Untars and gunzips the stats file from the target file"""
 	if not tarfile.is_tarfile(target):
-		print("Invalid tarfile")
+		logging.warning("Invalid tarfile")
 		return None
 	with tarfile.open(name=target, mode="r:gz") as tar:
 		members = tar.getmembers()
 		stats = [ member for member in members if member.name == stats_file ]
 		if len(stats) != 1:
-			print("Invalid tarfile, must contain exactly 1 stats_file")
+			logging.warning("Invalid tarfile, must contain exactly 1 stats_file")
 			return None
 		else:
 			tar.extract(stats.pop())
@@ -115,21 +88,19 @@ def get_best_models(bucket, prefix, results, target):
 	for key in sorted(results.keys()):
 		highest_score, winner = 0.0, None
 		result_format = "\t{0:<14}: {1:16}, Score: {2:0.5f}"
-		print("Category {0}".format(key))
+		logging.warning("Evaluating {0}".format(key))
 		for timestamp in results[key]:
 			k = Key(bucket)
 			k.key = prefix + key + timestamp + target
 			k.get_contents_to_filename(target)
-			score = get_f_measure(get_stats_file(target, "CNN_stat.csv"))
 			matrix = get_stats_file(target, "Con_Matrix.csv")
-			get_true_positive(matrix)
-			print(matrix)
+			score = get_model_accuracy(matrix)
 			if score > highest_score:
 				highest_score = score
 				winner = timestamp
-			print(result_format.format("Candidate", timestamp, score))
+			logging.warning(result_format.format("Candidate", timestamp, score))
 		winners[key] = winner
-		print(result_format.format("Winner", winner, highest_score))
+		logging.warning(result_format.format("Winner", winner, highest_score))
 	return winners
 
 def main_patch():
@@ -137,15 +108,16 @@ def main_patch():
 	conn = boto.s3.connect_to_region('us-west-2')
 	bucket = conn.get_bucket("s3yodlee")
 	#REVERT HERE my_results, prefix = {}, "meerkat/cnn/data"
-	my_results, prefix = {}, "meerkat/cnn/data/category/card/credit"
+	my_results, prefix = {}, "meerkat/cnn/data/"
 	target = "results.tar.gz"
 	find_s3_objects_recursively(conn, bucket, my_results, prefix=prefix, target=target)
 	results = get_peer_models(my_results, prefix=prefix)
 	winners = get_best_models(bucket, prefix, results, target)
 	for winner in sorted(winners.keys()):
-		print("Category: {0:<25}, Winner: {1}".format(winner, winners[winner]))
+		logging.warning("Category: {0:<25}, Winner: {1}".format(winner, winners[winner]))
 
 if __name__ == "__main__":
 	#Execute the main program
+	logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s")
 	main_patch()
 
