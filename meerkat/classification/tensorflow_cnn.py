8b77d2a9b6185f9a603742358925777842d1e277
J. Andrew Key <joeandrewkey@gmail.com>
2016-04-18 21:11:49 +0000

    Ensuring that models are loaded before the label_map is added to configuration parameters.

--- meerkat/classification/tensorflow_cnn.py
+++ meerkat/classification/tensorflow_cnn.py
@@ -26,6 +26,7 @@ import csv
 import logging
 import math
 import os
+import os.path
 import pprint
 import random
 import shutil
@@ -37,6 +38,7 @@ import tensorflow as tf
 from jsonschema import validate
 
 from meerkat.classification.tools import fill_description_unmasked, reverse_map
+from meerkat.classification.auto_load import main_program as load_models_from_s3
 from meerkat.various_tools import load_params, load_piped_dataframe, validate_configuration
 
 logging.basicConfig(level=logging.INFO)
@@ -54,6 +56,8 @@ def validate_config(config):
 	config = validate_configuration(config, schema_file)
 	logging.debug("Configuration is :\n{0}".format(pprint.pformat(config)))
 	reshape = ((config["doc_length"] - 96) / 27) * 256
+	if not os.path.isfile(config["label_map"]):
+		load_models_from_s3()
 	config["reshape"] = int(reshape)
 	config["label_map"] = load_params(config["label_map"])
 	config["num_labels"] = len(config["label_map"].keys())
