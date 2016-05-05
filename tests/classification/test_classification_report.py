f8885e8b1cee6effac9ce560d7152990a1604044
Oscar Pan <oscarpan@bu.edu>
2016-05-05 04:10:54 +0000

    align unit test with classification report

--- tests/classification/test_classification_report.py
+++ tests/classification/test_classification_report.py
@@ -9,14 +9,17 @@ from nose_parameterized import parameterized
 class ClassifcationReportTests(unittest.TestCase):
 	"""Unit tests for meerkat.classification.auto_load."""
 	@parameterized.expand([
-		(False, ["--model", "foo", "--label_map", "bar", "--testdata",
-			"baz", "--label_key", "SNOZ"])
+		(False, ["model", "data", "label_map", "label",  "--doc_key", "SNOZ"])
 	])
 	def test_parse_arguments(self, exception_test, arguments):
 		"""Simple test to ensure that this function works"""
 		if not exception_test:
 			parser = cr.parse_arguments(arguments)
-			self.assertEqual(parser.model, "foo")
+			self.assertEqual(parser.model, "model")
+			self.assertEqual(parser.data, "data")
+			self.assertEqual(parser.label_map, "label_map")
+			self.assertEqual(parser.label, "label".upper())
+			self.assertEqual(parser.doc_key, "SNOZ")
 
 if __name__ == '__main__':
 	unittest.main()
