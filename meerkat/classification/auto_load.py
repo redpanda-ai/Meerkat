332e53765b1bbf6e21af49d18b88ad2382a3e408
J. Andrew Key <joeandrewkey@gmail.com>
2016-05-12 18:58:46 +0000

    Minor import change.

--- meerkat/classification/auto_load.py
+++ meerkat/classification/auto_load.py
@@ -4,7 +4,7 @@ import argparse
 import logging
 import re
 import tarfile
-from os import rename
+import os
 import pandas as pd
 
 from boto.s3 import connect_to_region
@@ -102,7 +102,7 @@ def get_best_model_of_class(target, models_dir, **kwargs):
 			winner_count = candidate_count
 			leader_model = get_single_file_from_tarball(timestamp, target, ".*ckpt")
 			new_model_path = models_dir + (kwargs["suffix"] + kwargs["key"]).replace("/", ".")[1:] + "ckpt"
-			rename(leader_model, new_model_path)
+			os.rename(leader_model, new_model_path)
 		logging.info("\t{0:<14}{1:>2}: {2:16}, Score: {3:0.5f}".format("Candidate",
 			candidate_count, timestamp, score))
 		candidate_count += 1
@@ -149,11 +149,11 @@ def set_label_map_and_meta(*args):
 	json_file = get_single_file_from_tarball("", tarball, ".*json")
 	new_path = output_path + "label_maps/" + (suffix + key).replace("/", ".")[1:] + "json"
 	logging.debug("Moving label_map to: {0}".format(new_path))
-	rename(json_file, new_path)
+	os.rename(json_file, new_path)
 	#Move graph
 	meta_file = get_single_file_from_tarball("", tarball, ".*meta")
 	new_graph_def_path = output_path + "models/" + (suffix + key).replace("/", ".")[1:] + "meta"
-	rename(meta_file, new_graph_def_path)
+	os.rename(meta_file, new_graph_def_path)
 	return new_path
 
 def main_program():
