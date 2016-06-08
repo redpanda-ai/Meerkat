8961cc248e6cd3cfc118eb55d7556831cbe15de9
J. Andrew Key <joeandrewkey@gmail.com>
2016-06-08 00:06:00 +0000

    Correcting a small off-by-one bug

--- meerkat/classification/auto_load.py
+++ meerkat/classification/auto_load.py
@@ -190,8 +190,11 @@ def get_best_models(*args):
 def load_best_model_for_type(**kwargs):
 	"""Loads the best model for a given model type from S3 to the local host."""
 	client = boto3.client('s3')
-	#Note: we removing the leading "/" from the remote_file using the number 1
-	model_type = kwargs["model_type"][1:]
+
+	model_type = kwargs["model_type"]
+	if model_type.startsWith("/"):
+		model_type = model_type[1:]
+
 	remote_file = kwargs["s3_prefix"] + "/" + model_type +\
 		kwargs["timestamp"] + "results.tar.gz"
 	logging.critical("Bucket name {0}".format(kwargs["bucket"]))
