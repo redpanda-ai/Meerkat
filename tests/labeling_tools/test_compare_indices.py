4682179d8c824b80142959fb63e6df5956e0b440
Jie Zhang <jzhang@yodlee.com>
2016-06-09 20:22:26 +0000

    Add 'test_z_score_delta' for 'compare_indices.py'

--- tests/labeling_tools/test_compare_indices.py
+++ tests/labeling_tools/test_compare_indices.py
@@ -16,5 +16,13 @@ class CompareIndicesTests(unittest.TestCase):
 		"""Test for has_mapping_changed"""
 		self.assertEqual(compare_indices.has_mapping_changed(old_mapping, new_mapping), result)
 
+	@parameterized.expand([
+		([[8.0, 4.0, 2.0, 1.0], 1.492]),
+		([[2.0, 1.0], 2.0]),
+		([[1.0], None])
+	])
+	def test_z_score_delta(self, scores, z_score_delta):
+		self.assertEqual(compare_indices.z_score_delta(scores), z_score_delta)
+
 if __name__ == '__main__':
 	unittest.main()
