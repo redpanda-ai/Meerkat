205c2a102d706fda84a3c48ecdbe3d473a9fb4a2
Tina <diwubupt@gmail.com>
2016-06-14 01:13:06 +0000

    parameterize path for fixture

--- tests/classification/fixture/tools_fixture.py
+++ tests/classification/fixture/tools_fixture.py
@@ -1,16 +1,18 @@
 """Fixtures for test_tools"""
 
+BASE_DIR = "tests/classification/fixture/"
+
 def get_archive_path(case_type):
 	"""Return the archive path"""
 	archive_paths = {
 		"invalid_tarfile": "tarball_1.tar.gz",
 		"valid_tarfile": "tarball_5.tar.gz"
 	}
-	return "tests/classification/fixture/" + archive_paths[case_type]
+	return BASE_DIR + archive_paths[case_type]
 
 def get_des_path():
 	"Return the destination path"
-	return "tests/classification/fixture/extracted_tarballs/"
+	return BASE_DIR + "extracted_tarballs/"
 
 def get_dict():
 	"""Return a dictionary"""
@@ -34,7 +36,7 @@ def get_csv_path(csv_type):
 	"""Return a csv file path"""
 	csv_path = {
 		"correct_format": "tests/fixture/correct_format.csv",
-		"with_empty_transaction": "tests/classification/fixture/with_empty_transaction.csv"
+		"with_empty_transaction": BASE_DIR + "with_empty_transaction.csv"
 	}
 	return csv_path[csv_type]
 
