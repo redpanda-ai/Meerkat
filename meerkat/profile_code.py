656aac2b0799fe5c06288bb65630448bee525b0e
Oscar Pan <oscarpan@bu.edu>
2016-06-14 19:05:15 +0000

    made code look better

--- meerkat/profile_code.py
+++ meerkat/profile_code.py
@@ -2,6 +2,7 @@
 """
 This moduele takes a .prof file and returns a csv file that contain stats of function
 in meerkat (excluding python built-in fucntions)
+
 Created on Jun 13, 2016
 @Author: Oscar Pan
 """
@@ -16,6 +17,7 @@ Created on Jun 13, 2016
 import os
 import sys
 import pstats
+import logging
 import pandas as pd
 
 def get_module_name(stats_list):
@@ -27,18 +29,20 @@ def get_module_name(stats_list):
 
 def run_from_command_line(prof_file_path):
 	"""Process .prof file and return module's functions stats in a csv file"""
+	logging.basicConfig(level=logging.INFO)
 	header = ["Primitive call", "Total call", "Exclude Subfucntion Time", "Total Time",
 		"Module name", "Line Number", "Function Name"]
 	profile = pstats.Stats(prof_file_path)
 	profile.sort_stats("time")
-	# function_stats = {key: profile.stats[key] for key in profile.stats.keys() if "meerkat" in key[0]}
-	# beautify the stats
+	profile.print_stats("meerkat")
 	function_stats = [list(profile.stats[key])[:4] + list(key) for key in profile.stats.keys()
 		if "meerkat" in key[0]]
 	module_name = get_module_name(function_stats)
 	function_stats = pd.DataFrame(function_stats)
 	function_stats.columns = header
-	function_stats.to_csv(module_name+"_function_stats.csv", index=False)
+	file_path = module_name + "_profiling.csv"
+	function_stats.to_csv(file_path, index=False)
+	logging.info("Result file saved to {0}.".format(file_path))
 
 if __name__ == "__main__":
 	run_from_command_line(sys.argv[1])
