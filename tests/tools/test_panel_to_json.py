7d7525cb357bef0d13f1cac2a3c8f8cb325fea73
Jie Zhang <jzhang@yodlee.com>
2016-06-15 21:20:59 +0000

    Add 'test_dict_2_json' to 'test_panel_to_json.py'

--- tests/tools/test_panel_to_json.py
+++ tests/tools/test_panel_to_json.py
@@ -1,5 +1,6 @@
 """Unit test for meerkat.panel_to_json"""
 
+import json
 import unittest
 import meerkat.tools.panel_to_json as panel_to_json
 from nose_parameterized import parameterized
@@ -14,6 +15,18 @@ class PaneltoJsonTests(unittest.TestCase):
 	def test_string_cleanse(self, original_string, result):
 		"""Test for string_cleanse"""
 		self.assertEqual(panel_to_json.string_cleanse(original_string), result)
+
+	def test_dict_2_json(self):
+		"""Test dict_2_json"""
+		dict_example = {'a': 1, 'bb': {'ccc': 222, 'ddd': 333}}
+		filename = 'tests/tools/fixture/dict_example.json'
+		panel_to_json.dict_2_json(dict_example, filename)
+
+		json_obj = open(filename)
+		json_str = json_obj.read()
+		dict_expected = json.loads(json_str)
+
+		self.assertEqual(dict_example, dict_expected)
 
 if __name__ == '__main__':
 	unittest.main()
