6d5bd75e19584a308c6118ddcec8ee04c4976e37
ffeizhu <ffeizhu@outlook.com>
2016-06-16 23:05:09 +0000

    add tests for auto train

--- tests/classification/test_auto_train.py
+++ tests/classification/test_auto_train.py
@@ -2,18 +2,18 @@
 
 import os
 import unittest
-import meerkat.classification.auto_load as auto_load
+import meerkat.classification.auto_train as auto_train
 from nose_parameterized import parameterized
 
 class AutoTrainTests(unittest.TestCase):
 
 	@parameterized.expand([
-		(False, ["model_type", "bank", "--credit_or_debit", 'debit'])
+		(False, ["subtype", "bank", "--credit_or_debit", 'debit'])
 	])
 	def test_parse_arguments(self, exception_test, arguments):
 		"""Simple test to ensure that this function works"""
 		if not exception_test:
-			parser = auto_load.parse_arguments(arguments)
-			self.assertEqual(parser.model_type, "model")
+			parser = auto_train.parse_arguments(arguments)
+			self.assertEqual(parser.model_type, "subtype")
 			self.assertEqual(parser.bank_or_card, "bank")
 			self.assertEqual(parser.credit_or_debit, "debit")
