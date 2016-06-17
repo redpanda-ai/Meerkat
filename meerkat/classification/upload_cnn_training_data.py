b99ce7251c90d4ba395b243fd87622bcbe2753a8
J. Andrew Key <joeandrewkey@gmail.com>
2016-06-17 07:05:22 +0000

    Added missing file.

--- meerkat/classification/upload_cnn_training_data.py
+++ meerkat/classification/upload_cnn_training_data.py
@@ -12,7 +12,8 @@ created on April 4, 2016
 
 import os
 import sys
-from meerkat.classification.tools import push_file_to_s3, get_utc_iso_timestamp, make_tarfile
+from meerkat.classification.tools import get_utc_iso_timestamp, make_tarfile
+from meerkat.various_tools import push_file_to_s3
 
 # check files
 def main_process():
