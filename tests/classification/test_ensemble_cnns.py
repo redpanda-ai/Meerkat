981a4e2dc93fcbfe70bb73ddc006f13b3d5b70fe
Oscar Pan <oscarpan@bu.edu>
2016-06-17 22:49:43 +0000

    fix memory issue for ensemble cnns tests, rm test_merchant_process

--- tests/classification/test_ensemble_cnns.py
+++ tests/classification/test_ensemble_cnns.py
@@ -13,8 +13,8 @@ class EnsembleCnnTest(unittest.TestCase):
 		(np.array([0.1, 0.2, 0.7]), 2, np.array([0.29, 0.31, 0.4]))
 	])
 	def test_softmax_with_temperature(self, tensor, temperature, expected):
-		sess = tf.Session()
-		with sess.as_default():
+		gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.25)
+		with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, gpu_options=gpu_options)) as sess:
 			result = np.round(ensemble_cnns.softmax_with_temperature(tensor, temperature).eval(), decimals=2)
 		np.testing.assert_array_equal(result, expected)
 
@@ -23,8 +23,7 @@ class EnsembleCnnTest(unittest.TestCase):
 		(np.array([0.29, -1, 0.4]), "logsoftmax",  np.array([-1.24, -23.03, -0.92]))
 	])
 	def test_logsoftmax(self, softmax, name, expected):
-		sess = tf.Session()
-		with sess.as_default():
+		gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.25)
+		with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, gpu_options=gpu_options)) as sess:
 			result = np.round(ensemble_cnns.logsoftmax(softmax, name).eval(), decimals=2)
-		print(result)
 		np.testing.assert_array_equal(result, expected)
