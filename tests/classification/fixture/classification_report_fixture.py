b95148ef9624654b77bba7eaee2a09883679f9c1
Tina <diwubupt@gmail.com>
2016-06-21 05:07:19 +0000

    add fixture for classification report test

--- tests/classification/fixture/classification_report_fixture.py
+++ tests/classification/fixture/classification_report_fixture.py
@@ -10,3 +10,11 @@ def get_confusion_matrix(case_type):
 		return "tests/classification/fixture/invalid_confusion_matrix.csv"
 	elif case_type == "valid_cf":
 		return "tests/classification/fixture/valid_confusion_matrix.csv"
+
+def get_label_map():
+	"""Return a label map"""
+	return {
+		"1": "Bank Adjustment - Adjustment",
+		"2": "Deposits & Credits - Rewards",
+		"3": "Other Deposits - Credit"
+	}
