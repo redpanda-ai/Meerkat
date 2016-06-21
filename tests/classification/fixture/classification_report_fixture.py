54beeba020a6ad43aa982d95e276b26eb817e0cc
Tina <diwubupt@gmail.com>
2016-06-21 17:00:58 +0000

    parameterize file path in fixture

--- tests/classification/fixture/classification_report_fixture.py
+++ tests/classification/fixture/classification_report_fixture.py
@@ -1,15 +1,17 @@
 """Fixtures for test_classification_report"""
 
+BASE_DIR = "tests/classification/fixture/"
+
 def get_csv():
 	"""Return a csv file"""
-	return "tests/classification/fixture/classification_report_sample.csv"
+	return BASE_DIR + "classification_report_sample.csv"
 
 def get_confusion_matrix(case_type):
 	"""Return a confusion matrix"""
 	if case_type == "invalid_cf":
-		return "tests/classification/fixture/invalid_confusion_matrix.csv"
+		return BASE_DIR + "invalid_confusion_matrix.csv"
 	elif case_type == "valid_cf":
-		return "tests/classification/fixture/valid_confusion_matrix.csv"
+		return BASE_DIR + "valid_confusion_matrix.csv"
 
 def get_label_map():
 	"""Return a label map"""
