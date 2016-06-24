534b51ec241d6a8a6c5fd27c515f46f9f4c266f1
J. Andrew Key <joeandrewkey@gmail.com>
2016-06-24 02:48:02 +0000

    Reservoir sampling with pandas dataframes

--- meerkat/fat_head/reservoir_sampling.py
+++ meerkat/fat_head/reservoir_sampling.py
@@ -15,33 +15,30 @@ def reservoir_sampling(items, k):
 
 	return sample
 
-"""
-def chunky_sampling(input_file, k):
-
+def df_reservoir_sampling(input_file, k, chunksize):
 	first_chunk = True
-	record_no = k
-	chunksize = 6
 	sample = None
+	last_i = 0
 	for chunk in pd.read_csv(input_file, chunksize=chunksize):
+		print("Last I :{0}".format(last_i))
 		if first_chunk:
 			sample = chunk[0:k]
 			chunk = chunk[k:]
 			first_chunk = False
-		else:
-			chunk = pd.concat([
-			#print("Sample\n{0}".format(sample))
-		#print(chunk)
+
+		print("Sample:\n{0}".format(sample))
+		print("Chunk:\n{0}".format(chunk))
+
 		row_iterator = chunk.iterrows()
 		for i, row in row_iterator:
 			print("I {0}".format(i))
-			j = randrange(0, i)
-			if j <= k:
+			j = randrange(0, last_i + i + 1)
+			if j < k:
 				sample.iloc[j] = row
-				print("Sample\n{0}".format(sample))
-			print(row.record_no)
-"""
-items = [1, 2]
-sample = reservoir_sampling(items, 1)
-print(sample)
+			this_i = i
+		last_i += this_i + 1
+	return sample
 
-#chunky_sampling("meerkat/fat_head/input.tab", 2)
+sample = df_reservoir_sampling("meerkat/fat_head/input.tab", 2, 6)
+print("Final Sample:\n{0}".format(sample))
+
