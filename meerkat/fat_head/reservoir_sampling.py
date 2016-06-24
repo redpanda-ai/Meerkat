bc285a5062c894973d45996878fb0ead5abf9ecb
J. Andrew Key <joeandrewkey@gmail.com>
2016-06-24 00:25:01 +0000

    Adding working example of reservoir sampling

---
+++ meerkat/fat_head/reservoir_sampling.py
@@ -0,0 +1,19 @@
+from random import randrange
+
+def reservoir_sampling(items, k):
+	"""Reservoir Sampling"""
+	sample = items[0:k]
+
+	for i in range(k, len(items)):
+		print("Looping")
+		j = randrange(0, i)
+		if j <= k:
+			print("I {0}".format(i))
+			print("J {0}".format(j))
+			sample[j] = items[i]
+
+	return sample
+
+items = [1, 2, 3, 4, 5, 6, 7]
+sample = reservoir_sampling(items, 5)
+print(sample)
