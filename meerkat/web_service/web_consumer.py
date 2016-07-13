eb8a3b378c37c2f68f1f03d87e5f82569c37c242
Jie Zhang <jzhang@yodlee.com>
2016-07-13 19:02:18 +0000

    Delete category from schema_output.json

--- meerkat/web_service/web_consumer.py
+++ meerkat/web_service/web_consumer.py
@@ -364,30 +364,24 @@ class WebConsumer():
 				trans["CNN"] = trans.get("CNN", {}).get("label", "")
 				trans.pop("subtype_CNN", None)
 
-	def __elastic_search_category(self, trans):
-		"""Get category based on elastic search and subtype"""
-		trans["search"] = {"category_labels" : trans.get("category_labels", [])} # Elasticsearch for category
+	def __search_category(self, trans):
+		"""Get category based on subtype"""
+		trans["search"] = {"category_labels" : trans.get("category_labels", [])}
 
-		'''
 		if trans.get("category_labels"):
 			trans["CNN"] = trans.get("CNN", {}).get("label", "")
 			if "subtype_CNN" in trans:
 				del trans["subtype_CNN"]
-			continue
-		'''
 
-		#fallback = trans.get("CNN", {}).get("category", "").strip() # Merchant name
-		fallback = trans.get("CNN", "")
-		print(fallback)
+		fallback = trans.get("CNN", {}).get("category", "").strip()
 		if fallback == "Use Subtype Rules for Categories" or fallback == "":
-			subtype_category = trans.get("subtype_CNN", {}).get("category", trans.get("subtype_CNN", {}).get("label", ""))
-			if isinstance(subtype_category, dict):
-				subtype_category = subtype_category[trans["ledger_entry"].lower()]
-			fallback = subtype_category
+			subtype = trans.get("subtype_CNN", {}).get("category", trans.get("subtype_CNN", {}).get("label", ""))
+			if isinstance(subtype, dict):
+				subtype = subtype[trans["ledger_entry"].lower()]
+			fallback = subtype
 
 		trans["category_labels"] = [fallback]
-		trans["category"] = fallback
-		#trans["CNN"] = trans.get("CNN", {}).get("label", "")
+		trans["CNN"] = trans.get("CNN", {}).get("label", "")
 		trans.pop("subtype_CNN", None)
 
 	def __apply_missing_categories_2(self, transactions):
@@ -399,21 +393,27 @@ class WebConsumer():
 		card_debit = json_data['card_debit']
 
 		print('__apply_missing_categories_2 has been called')
-		for trans in transactions:
-			category = trans['category'] # Category CNN
 
+		for trans in transactions:
+			if len(trans['category_labels']) == 0:
+				print('------0')
+				print(trans['category_labels'])
+				self.__search_category(trans)
+				continue
+
+			category = trans['category_labels'][0]
 			if trans['ledger_entry'] == 'credit' and trans['container'] == 'bank' and category in bank_credit:
 				print('------1')
-				self.__elastic_search_category(trans)
+				self.__search_category(trans)
 			if trans['ledger_entry'] == 'credit' and trans['container'] == 'card' and category in card_credit:
 				print('------2')
-				self.__elastic_search_category(trans)
+				self.__search_category(trans)
 			if trans['ledger_entry'] == 'debit' and trans['container'] == 'bank' and category not in bank_debit:
 				print('------3')
-				self.__elastic_search_category(trans)
+				self.__search_category(trans)
 			if trans['ledger_entry'] == 'debit' and trans['container'] == 'card' and category not in card_debit:
 				print('------4')
-				self.__elastic_search_category(trans)
+				self.__search_category(trans)
 
 	def ensure_output_schema(self, transactions, debug):
 		"""Clean output to proper schema"""
@@ -643,7 +643,7 @@ class WebConsumer():
 
 		for transaction in data["transaction_list"]:
 			category = transaction["category_CNN"].get("label", "")
-			transaction["category"] = category
+			transaction["category_labels"] = [category]
 
 		return data["transaction_list"]
 
@@ -735,7 +735,6 @@ class WebConsumer():
 		cpu_result.get() # Wait for CPU bound classifiers to finish
 
 		if not optimizing:
-			self.__apply_missing_categories(data["transaction_list"])
 			self.__apply_missing_categories_2(data["transaction_list"])
 
 		self.ensure_output_schema(data["transaction_list"], debug)
