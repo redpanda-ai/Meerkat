ae62b00ffb658641a02ba69bd177de906f93e5ff
J. Andrew Key <joeandrewkey@gmail.com>
2016-07-19 14:38:53 -0700

    Formatting classification report.

--- meerkat/fat_head/classification/classify.py
+++ meerkat/fat_head/classification/classify.py
@@ -5,12 +5,14 @@
 
 #####################################################
 
+import argparse
 import csv
 import logging
-import pandas as pd
-import numpy as np
-import argparse
+import re
 import sys
+import numpy as np
+import pandas as pd
+
 
 from meerkat.fat_head.classification.bloomfilter_classifier import BloomfilterClassifier
 from meerkat.fat_head.classification.trie_classifier import TrieClassifier
@@ -23,7 +25,38 @@ def parse_arguments():
 	parser.add_argument("classifier")
 	return parser.parse_args()
 
+def format_classification_report(report):
+	"""Make things easier to read."""
+	report_format = " {0:<20} {1:<10} {2:<10} {3:<10} {4:<10}"
+	pattern = "^(.+,\S\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+).*$"
+	digits_pattern = "^(\d+\S+)\s+(\d+\S+)\s+(\d+\S+)\s+(\d+\S+)\s+(\d+\S+).*$"
+	summary_pattern = "^(avg / total)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+).*$"
+	first_pattern = "^(\S+)\s+(\S+)\s+(\S+)\s+(\S+).*$"
+
+	lines = ["\n"]
+	my_regex = re.compile(pattern)
+	my_digits = re.compile(digits_pattern)
+	my_summary = re.compile(summary_pattern)
+	my_first = re.compile(first_pattern)
+	for line in report.split("\n"):
+		line = line.strip()
+		dig_match = my_digits.match(line)
+		sum_match = my_summary.match(line)
+		first_match = my_first.match(line)
+
+		matches = my_regex.match(line)
+		if matches:
+			lines.append(report_format.format(matches.group(1), matches.group(2), matches.group(3), matches.group(4), matches.group(5)))
+		elif dig_match:
+			lines.append(report_format.format("", dig_match.group(1), dig_match.group(2), dig_match.group(3), dig_match.group(4)))
+		elif sum_match:
+			lines.append(report_format.format(sum_match.group(1), sum_match.group(2), sum_match.group(3), sum_match.group(4), sum_match.group(5)))
+		elif first_match:
+			lines.append(report_format.format("", first_match.group(1), first_match.group(2), first_match.group(3), first_match.group(4)))
+	logging.info(str.join("\n", lines))
+
 def main_process():
+	"""This does all the work."""
 	logging.basicConfig(level=logging.INFO)
 
 	args = parse_arguments()
@@ -72,9 +105,18 @@ def main_process():
 		if y_predict[i] == y_true[i]:
 			true_positives += 1
 
+
 	# generate classification report
 	report = classification_report(y_true, y_predict)
-	logging.info(report)
+	format_classification_report(report)
+		#columns = line.split("\t")
+		#columns = [x.strip() for x in columns]
+		#if len(columns) >=4:
+		#	logging.info(report_format.format(columns[0],columns[1],columns[2],columns[3]))
+		#else:
+		#	logging.info(columns)
+		#logging.info("! {0}".format(line))
+	#logging.info(report)
 
 	# calculate accuracy
 	accuracy = true_positives / len(y_predict)
