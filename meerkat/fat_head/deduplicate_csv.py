3ba1a7dc1266f580fef8f062ba230e4c09ebc2f4
J. Andrew Key <joeandrewkey@gmail.com>
2016-07-20 21:33:20 -0500

    More restructuring work.

--- meerkat/fat_head/deduplicate_csv.py
+++ meerkat/fat_head/deduplicate_csv.py
@@ -16,18 +16,12 @@ def parse_arguments(args):
 	parser = argparse.ArgumentParser()
 	parser.add_argument("input_file")
 	parser.add_argument("--subset", default="")
-	args = parser.parse_args(args)
-	return args
+	return parser.parse_args(args)
 
-def main_process():
-	logging.basicConfig(level=logging.INFO)
-	args = parse_arguments(sys.argv[1:])
-
-	input_file = args.input_file
-	subset = args.subset
-
+def main_process(input_file, subset):
 	df = pd.read_csv(input_file, error_bad_lines=False,
 		encoding='utf-8', na_filter=False, sep=',')
+
 	if subset == "":
 		unique_df = df.drop_duplicates(keep="first")
 	else:
@@ -39,4 +33,6 @@ def main_process():
 	logging.info("csv files with unique {0} transactions saved to: {1}".format(len(unique_df), output_file))
 
 if __name__ == "__main__":
-	main_process()
+	logging.basicConfig(level=logging.INFO)
+	args = parse_arguments(sys.argv[1:])
+	main_process(args.input_file, args.subset)
--- meerkat/fat_head/generate_geo_dictionary.py
+++ meerkat/fat_head/generate_geo_dictionary.py
@@ -1,25 +1,34 @@
-import sys
+import argparse
 import csv
 import json
-import argparse
+import logging
+import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument("input_file", help="csv containing city, state, and zip")
-args = parser.parse_args()
-my_dict = {}
-with open(args.input_file) as infile:
-	for line in infile:
-		parts = line.split("\t")
-		city = parts[2].upper()
-		state = parts[4].upper()
-		zipcode = parts[1]
-		if state not in my_dict:
-			my_dict[state] = {}
-		if city not in my_dict[state]:
-			my_dict[state][city] = [zipcode]
-		else:
-			my_dict[state][city].append(zipcode)
+def parse_arguments(args):
+	parser = argparse.ArgumentParser()
+	parser.add_argument("input_file", help="csv containing city, state, and zip")
+	return parser.parse_args(args)
 
-my_json = json.dumps(my_dict, sort_keys=True, indent=4, separators=(',', ': '))
-print(my_json)
+def get_geo_dict(input_file):
+	my_dict = {}
+	with open(input_file) as infile:
+		for line in infile:
+			parts = line.split("\t")
+			city = parts[2].upper()
+			state = parts[4].upper()
+			zipcode = parts[1]
+			if state not in my_dict:
+				my_dict[state] = {}
+			if city not in my_dict[state]:
+				my_dict[state][city] = [zipcode]
+			else:
+				my_dict[state][city].append(zipcode)
+
+	my_json = json.dumps(my_dict, sort_keys=True, indent=4, separators=(',', ': '))
+	return my_json
+
+if __name__ == "__main__":
+	logging.basicConfig(level=logging.INFO)
+	args = parse_arguments(sys.argv[1:])
+	logging.info(get_geo_dict(args.input_file))
 
