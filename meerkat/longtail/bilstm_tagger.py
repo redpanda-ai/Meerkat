9db9110968157ae05f93167a0ca1cded5f88a6a3
Matt Sevrens <msevrens@gmail.com>
2016-07-27 15:47:00 -0700

    merging word embeddings and character embeddings

--- meerkat/longtail/bilstm_tagger.py
+++ meerkat/longtail/bilstm_tagger.py
@@ -41,8 +41,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.models.rnn import rnn
 
+from meerkat.classification.tools import reverse_map, get_tensor, get_op, get_variable
 from meerkat.various_tools import load_params, load_piped_dataframe
-from meerkat.longtail.tools import get_tensor, get_op, get_variable
 
 logging.basicConfig(level=logging.INFO)
 
@@ -102,10 +102,52 @@ def load_embeddings_file(file_name, sep=" ",lower=False):
 	print("loaded pre-trained embeddings (word->emb_vec) size: {} (lower: {})".format(len(emb.keys()), lower))
 	return emb, len(emb[word])
 
-def trans_to_tensor(config, sess, graph, tokens, tags):
+def words_to_indices(data):
+	"""convert tokens to int, assuming data is a df"""
+	w2i = {}
+	w2i["_UNK"] = 0
+	for row_num, row in enumerate(data.values):
+		tokens = row[0].lower().split()
+		# there are too many unique tokens in description, better to shrink the size
+		for token in tokens:
+			if token not in w2i:
+				w2i[token] = len(w2i)
+	return w2i
+
+def construct_embedding(config, w2i, loaded_embedding):
+	"""construct an embedding that contains all words in loaded_embedding and w2i"""
+	num_words = len(set(loaded_embedding.keys()).union(set(w2i.keys())))
+	# initialize a num_words * we_dim embedding table
+	temp = np.random.uniform(-1,1, (num_words, config["we_dim"]))
+	for word in loaded_embedding.keys():
+		if word not in w2i:
+			w2i[word] = len(w2i)
+		temp[w2i[word]] = loaded_embedding[word]
+	return w2i, temp
+
+def preprocess(config):
+	"""Split data into training and test, return w2i for data in training, return training data's embedding matrix"""
+	config["train"], config["test"] = load_data(config)
+	embedding, emb_dim = load_embeddings_file(config["embeddings"], lower=True)
+	# Assert that emb_dim is equal to we_dim
+	assert(emb_dim == config["we_dim"])
+	config["w2i"] = words_to_indices(config["train"])
+	config["w2i"], config["wembedding"] = construct_embedding(config, config["w2i"], embedding)
+	config["vocab_size"] = len(config["wembedding"])
+	return config
+
+def encode_tags(config, tags):
+	"""one-hot encode labels"""
+	tag2id = reverse_map(config["tag_map"])
+	tags = np.array([int(tag2id[tag]) for tag in tags])
+	encoded_tags = (np.arange(len(tag2id)) == tags[:, None]).astype(np.float32)
+	return encoded_tags
+
+def trans_to_tensor(config, sess, graph, tokens, tags, train=False):
 	"""Convert a transaction to a tensor representation of documents
 	and labels"""
 
+	w2i = config["w2i"]
 	c2i = config["c2i"]
 	char_embed, rev_char_embed = [], []
 	rev_lst = get_tensor(graph, "rev_last_state:0")
@@ -129,10 +171,20 @@ def trans_to_tensor(config, sess, graph, tokens, tags):
 		rev_char_embed.append(rev_last_state[0][token_length-1])
 
 	# Encode Words
+	if train:
+		word_feed_dict = {get_tensor(graph, "word_inputs:0"): [w2i[w] for w in tokens]}
+	else:
+		word_feed_dict = {get_tensor(graph, "word_inputs:0"): [w2i.get(w, w2i["_UNK"]) for w in tokens]}
+
+	embedded_words = sess.run(get_tensor(graph, "we_lookup:0"), feed_dict=word_feed_dict)
 
 	# Merge Encodings
-	features = [np.concatenate([c, rc], axis=0) for c, rc in zip(char_embed, reversed(rev_char_embed))]
-	features = np.array(features)
+	char_features = [np.concatenate([c, rc], axis=0) for c, rc in zip(char_embed, reversed(rev_char_embed))]
+	char_features = np.array(char_features)
+	features = np.concatenate([embedded_words, char_features], axis=1)
+
+	# Encode Tags
+	encoded_tags = encode_tags(config, tags)
 
 	print(tokens)
 	print(features.shape)
@@ -140,13 +192,6 @@ def trans_to_tensor(config, sess, graph, tokens, tags):
 	tensor = []
 
 	return tensor
-
-def evaluate_testset(config, graph, sess, model, test):
-	"""Check error on test set"""
-
-	test_accuracy = 0
-
-	return test_accuracy
 
 def build_graph(config):
 	"""Build CNN"""
@@ -186,6 +231,13 @@ def build_graph(config):
 		last_state = tf.identity(output, name="last_state")
 		output, state = rnn.dynamic_rnn(lstm, rev_ce_fixed_size, scope="rev", **options)
 		rev_last_state = tf.identity(output, name="rev_last_state")
+
+		# Word Embedding
+		word_inputs = tf.placeholder(tf.int32, [None], name="word_inputs")
+		wembed_matrix = tf.Variable(tf.constant(0.0, shape=[config["vocab_size"], config["we_dim"]]), trainable=False, name="wembed_matrix")
+		embedding_placeholder = tf.placeholder(tf.float32, [config["vocab_size"], config["we_dim"]], name="embedding_placeholder")
+		assign_wembedding = tf.assign(wembed_matrix, embedding_placeholder, name="assign_wembedding")
+		wembeds = tf.nn.embedding_lookup(wembed_matrix, word_inputs, name="we_lookup")
 		
 		# Combined Word Embedding and Character Embedding Input
 		input_shape = (None, config["ce_dim"] * 2 + config["we_dim"])
@@ -202,7 +254,7 @@ def train_model(config, graph, sess, saver):
 	dataset = config["dataset"]
 	train, test = load_data(config)
 	train_index = list(train.index)
-	# embedding, num_word = load_embeddings_file("./meerkat/longtail/embeddings/en.polyglot.txt")
+	sess.run(get_op(graph, "assign_wembedding"), feed_dict={get_tensor(graph, "embedding_placeholder:0"): config["wembedding"]})
 
 	# Train the Model
 	for step in range(eras):
@@ -215,6 +267,13 @@ def train_model(config, graph, sess, saver):
 	final_model_path = ""
 
 	return final_model_path
+
+def evaluate_testset(config, graph, sess, model, test):
+	"""Check error on test set"""
+
+	test_accuracy = 0
+
+	return test_accuracy
 
 def run_session(config, graph, saver):
 	"""Run Session"""
@@ -230,6 +289,7 @@ def run_from_command_line():
 	"""Run module from command line"""
 	logging.basicConfig(level=logging.INFO)
 	config = validate_config(sys.argv[1])
+	config = preprocess(config)
 	graph, saver = build_graph(config)
 	run_session(config, graph, saver)
 
