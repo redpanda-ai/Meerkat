22ac86f0ba686d2ee0f92a871d544fff5adbeef8
Matt Sevrens <msevrens@gmail.com>
2016-08-02 10:57:23 -0700

    many changes

--- meerkat/longtail/bilstm_tagger.py
+++ meerkat/longtail/bilstm_tagger.py
@@ -44,6 +44,15 @@ from meerkat.various_tools import load_params, load_piped_dataframe
 
 logging.basicConfig(level=logging.INFO)
 
+def last_relevant(output, length, name):
+    batch_size = tf.shape(output)[0]
+    max_length = int(output.get_shape()[1])
+    output_size = int(output.get_shape()[2])
+    index = tf.range(0, batch_size) * max_length + (tf.to_int32(length) - 1)
+    flat = tf.reshape(output, [-1, output_size])
+    relevant = tf.gather(flat, index, name=name)
+    return relevant
+
 def get_tags(trans):
 	"""Convert df row to list of tags and tokens"""
 
@@ -145,14 +154,14 @@ def encode_tags(config, tags):
 	encoded_tags = (np.arange(len(tag2id)) == tags[:, None]).astype(np.float32)
 	return encoded_tags
 
-def trans_to_indices(config, sess, graph, tokens, tags, train=False):
+def trans_to_tensor(config, sess, graph, tokens, tags, train=False):
 	"""Convert a transaction to a tensor representation of documents
 	and labels"""
 
 	w2i = config["w2i"]
 	c2i = config["c2i"]
 	max_wl = config["max_word_length"]
-	char_inputs = []
+	char_inputs, rev_char_inputs = [], []
 	word_indices = [w2i[w] for w in tokens] if train else [w2i.get(w, w2i["_UNK"]) for w in tokens]
 
 	# Encode Tags
@@ -184,7 +193,7 @@ def char_encoding(config, graph):
 		# Character Embedding
 		word_lengths = tf.placeholder(tf.int64, [None], name="word_lengths")
 		char_inputs = tf.placeholder(tf.int32, [None, max_wl], name="char_inputs")
-		cembed_matrix = tf.Variable(tf.random_uniform([len(c2i.keys()), config["ce_dim"]], -1.0, 1.0), name="cembeds")
+		cembed_matrix = tf.Variable(tf.random_uniform([len(c2i.keys()), config["ce_dim"]], -0.25, 0.25), name="cembeds")
 		cembeds = tf.nn.embedding_lookup(cembed_matrix, char_inputs, name="ce_lookup")
 
 		# Create LSTM for Character Encoding
@@ -197,9 +206,11 @@ def char_encoding(config, graph):
 			"sequence_length": word_lengths
 		}
 
-		(last_state, rev_last_state), output_states = tf.nn.bidirectional_dynamic_rnn(fw_lstm, bw_lstm, cembeds, **options)
+		(output_fw, output_bw), output_states = tf.nn.bidirectional_dynamic_rnn(fw_lstm, bw_lstm, cembeds, **options)
+		last_state = tf.identity(output_fw, name="last_state")
+		rev_last_state = tf.identity(output_bw, name="rev_last_state")
 
-		return last_state, rev_last_state
+		return last_state, rev_last_state, word_lengths
 
 def build_graph(config):
 	"""Build CNN"""
@@ -211,7 +222,7 @@ def build_graph(config):
 	with graph.as_default():
 
 		# Character Embedding
-		last_state, rev_last_state = char_encoding(config, graph)
+		last_state, rev_last_state, word_lengths = char_encoding(config, graph)
 
 		# Word Embedding
 		word_inputs = tf.placeholder(tf.int32, [None], name="word_inputs")
@@ -219,15 +230,12 @@ def build_graph(config):
 		embedding_placeholder = tf.placeholder(tf.float32, [config["vocab_size"], config["we_dim"]], name="embedding_placeholder")
 		assign_wembedding = tf.assign(wembed_matrix, embedding_placeholder, name="assign_wembedding")
 		wembeds = tf.nn.embedding_lookup(wembed_matrix, word_inputs, name="we_lookup")
-		
-		# Combined Word Embedding and Character Embedding Input
 		trans_len = tf.placeholder(tf.int64, None, name="trans_length")
-		word_lengths = get_tensor(graph, "word_lengths:0")
-		index_lookup = tf.pack([tf.range(tf.size(word_lengths)), tf.to_int32(word_lengths)-1], axis=1, name="index_lookup")
-		
-		char_embeds = tf.gather_nd(last_state, index_lookup)
-		rev_char_embeds = tf.gather_nd(rev_last_state, index_lookup)
-		combined_embeddings = tf.concat(1, [wembeds, char_embeds, tf.reverse(rev_char_embeds, [True, False])])
+
+		# Combine Embeddings
+		char_embeds = last_relevant(last_state, word_lengths, "char_embeds")
+		rev_char_embeds = last_relevant(rev_last_state, word_lengths, "rev_char_embeds")
+		combined_embeddings = tf.concat(1, [wembeds, char_embeds, tf.reverse(rev_char_embeds, [True, False])], name="combined_embeddings")
 
 		# Cells and Weights
 		input_cell = tf.nn.rnn_cell.BasicLSTMCell(config["ce_dim"] * 2 + config["we_dim"], state_is_tuple=True)
@@ -236,7 +244,7 @@ def build_graph(config):
 
 		fw_network = tf.nn.rnn_cell.MultiRNNCell([input_cell] + ([fw_lstm] * (config["num_layers"] - 1)), state_is_tuple=True)
 		bw_network = tf.nn.rnn_cell.MultiRNNCell([input_cell] + ([bw_lstm] * (config["num_layers"] - 1)), state_is_tuple=True)
-		weight = tf.Variable(tf.truncated_normal([config["h_dim"] * 2, len(config["tag_map"])], stddev=0.1))
+		weight = tf.Variable(tf.truncated_normal([config["h_dim"] * 2, len(config["tag_map"])], stddev=0.1), name="weight")
 		bias = tf.Variable(tf.constant(0.1, shape=[len(config["tag_map"])]))
 
 		def model(combined_embeddings, name, train=False, noise_sigma=0.0):
@@ -274,7 +282,7 @@ def build_graph(config):
 		# Calculate Loss and Optimize
 		labels = tf.placeholder(tf.float32, shape=[None, len(config["tag_map"].keys())], name="y")
 		loss = tf.neg(tf.reduce_mean(tf.reduce_sum(network * labels, 1)), name="loss")
-		optimizer = tf.train.GradientDescentOptimizer(config["learning_rate"]).minimize(loss, var_list=tf.trainable_variables(), name="optimizer")
+		optimizer = tf.train.GradientDescentOptimizer(config["learning_rate"]).minimize(loss, name="optimizer")
 
 		saver = tf.train.Saver()
 
@@ -297,36 +305,33 @@ def train_model(config, graph, sess, saver):
 		total_loss = 0
 
 		print("ERA: " + str(step))
+		np.set_printoptions(threshold=np.inf)
 
 		for t_index in train_index[0:1000]:
 
 			count += 1
 
-			if count % 500 == 0:
-				print(sess.run(get_tensor(graph, "cembeds:0")))
-				print("loss: " + str(total_loss / count))
-				print("%d" % (count / len(train_index) * 100) + "% complete with era")
-
 			row = train.loc[t_index]
 			tokens, tags = get_tags(row)
-			char_inputs, word_lengths, word_indices, encoded_tags = trans_to_indices(config, sess, graph, tokens, tags, train=True)
+			char_inputs, word_lengths, word_indices, labels = trans_to_tensor(config, sess, graph, tokens, tags, train=True)
 
-			try:
+			feed_dict = {
+				get_tensor(graph, "char_inputs:0") : char_inputs,
+				get_tensor(graph, "word_inputs:0") : word_indices,
+				get_tensor(graph, "word_lengths:0") : word_lengths,
+				get_tensor(graph, "trans_length:0"): len(tokens),
+				get_tensor(graph, "y:0"): labels
+			}
 
-				feed_dict = {
-					get_tensor(graph, "char_inputs:0") : char_inputs,
-					get_tensor(graph, "word_inputs:0") : word_indices,
-					get_tensor(graph, "word_lengths:0") : word_lengths,
-					get_tensor(graph, "trans_length:0"): len(tokens),
-					get_tensor(graph, "y:0"): labels
-				}
+			# Run Training Step
+			optimizer_out, loss = sess.run([get_op(graph, "optimizer"), get_tensor(graph, "loss:0")], feed_dict=feed_dict)
+			total_loss += loss
 
-				# Run Training Step
-				optimizer_out, loss = sess.run([get_op(graph, "optimizer"), get_tensor(graph, "loss:0")], feed_dict=feed_dict)
-				total_loss += loss
-				
-			except:
-				print(row)
+			# Log
+			if count % 500 == 0:
+				print(sess.run(get_tensor(graph, "combined_embeddings:0"), feed_dict=feed_dict).shape)
+				print("loss: " + str(total_loss / count))
+				print("%d" % (count / len(train_index) * 100) + "% complete with era")
 
 		# Evaluate Model
 		test_accuracy = evaluate_testset(config, graph, sess, test)
@@ -355,7 +360,7 @@ def evaluate_testset(config, graph, sess, test):
 
 		row = test.loc[i]
 		tokens, tags = get_tags(row)
-		char_inputs, word_lengths, word_indices, encoded_tags = trans_to_indices(config, sess, graph, tokens, tags, train=True)
+		char_inputs, word_lengths, word_indices, labels = trans_to_tensor(config, sess, graph, tokens, tags)
 		total_count += len(tokens)
 		
 		feed_dict = {
@@ -365,7 +370,7 @@ def evaluate_testset(config, graph, sess, test):
 			get_tensor(graph, "trans_length:0"): len(tokens)
 		}
 
-		output = sess.run(get_tensor(graph, "trained:0"), feed_dict=feed_dict_test)
+		output = sess.run(get_tensor(graph, "trained:0"), feed_dict=feed_dict)
 
 		correct_count = np.sum(np.argmax(output, 1) == np.argmax(labels, 1))
 		total_correct += correct_count
