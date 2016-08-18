910667ea8c32d479e270c12f234101bd8695c2c6
Tina Wu <diwubupt@gmail.com>
2016-08-18 01:06:48 +0000

    refact get_top_merchant_data

--- meerkat/geomancer/__main__.py
+++ meerkat/geomancer/__main__.py
@@ -53,9 +53,8 @@ def main_process():
 	logger.info("Valid configuration")
 
 	#module_list = [agg_data, merchant_dictionaries, builder, top_merchant_data, interrogate]
-	module_list = [agg_data, merchant_dictionaries]
+	module_list = [agg_data, merchant_dictionaries, top_merchant_data]
 	for module in module_list:
-		logger.info(module.Worker.name)
 		if module.Worker.name in config:
 			logger.info("Activating {0} module.".format(module.Worker.name))
 			config_snippet = config[module.Worker.name]
