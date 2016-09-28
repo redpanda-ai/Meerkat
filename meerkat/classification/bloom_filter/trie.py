b881265b72b03060cc24469e7690f911ec7d81f9
ffeizhu <ffeizhu@outlook.com>
2016-09-28 00:30:45 +0000

    pylint fix

--- meerkat/classification/bloom_filter/trie.py
+++ meerkat/classification/bloom_filter/trie.py
@@ -89,7 +89,7 @@ class Trie():
 			if word[0] == any_char:
 				#Recursive case #0: Search for the word starts with (.) until found
 				for child_key in node.children:
-					result =  find(node.children[child_key], word[1:], path + child_key)
+					result = find(node.children[child_key], word[1:], path + child_key)
 					if result:
 						return result
 				return False
