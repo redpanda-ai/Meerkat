6c55780078c5eeed13adaf1115fc39a728eacc14
J. Andrew Key <joeandrewkey@gmail.com>
2016-09-30 01:21:45 +0000

    Fixing pylint errors.

--- meerkat/geomancer/deduplicate_csv.py
+++ meerkat/geomancer/deduplicate_csv.py
@@ -1,32 +1,29 @@
-
+"""This module removes duplicates from a CSV file"""
 #################### USAGE ##########################
-
 # Use the -h flag from the command line to get help
-
 #####################################################
 
 import argparse
-import csv
 import sys
-import logging
-import pandas as pd
+
 from .tools import deduplicate_csv
 
 def parse_arguments(args):
+	"""This function parses command-line arguments"""
 	parser = argparse.ArgumentParser()
 	parser.add_argument("input_file")
 	parser.add_argument("--subset", default="")
 	parser.add_argument("--inplace", action='store_true')
 	return parser.parse_args(args)
 
-if __name__ == "__main__":
-	logging.basicConfig(level=logging.INFO)
+def real_main():
+	"""This function does all of the work."""
 	args = parse_arguments(sys.argv[1:])
-	read_csv_kwargs = { "error_bad_lines": False, "warn_bad_lines": True, "encoding": "utf-8",
-		"quotechar" : '"', "na_filter" : False, "sep": "," }
+	read_csv_kwargs = {"error_bad_lines": False, "warn_bad_lines": True, "encoding": "utf-8",
+		"quotechar" : '"', "na_filter" : False, "sep": ","}
 	to_csv_kwargs = {"sep": ","}
-	csv_kwargs = {
-		"read": read_csv_kwargs,
-		"to": to_csv_kwargs
-	}
+	csv_kwargs = {"read": read_csv_kwargs, "to": to_csv_kwargs}
 	deduplicate_csv(args.input_file, args.subset, args.inplace, **csv_kwargs)
+
+if __name__ == "__main__":
+	real_main()
