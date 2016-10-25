12bb411f473fc116f999a5387e069253cf7b30db
Jie Zhang <jzhang@yodlee.com>
2016-10-25 15:02:36 -0700

    Move config file

--- meerkat/elasticsearch/search_agg_index.py
+++ meerkat/elasticsearch/search_agg_index.py
@@ -163,7 +163,7 @@ def enrich_transaction(trans, hit):
 def search_agg_index(data, params=None):
 	"""Enrich transactions with agg index"""
 	if params is None:
-		params = json.loads(open('./meerkat/elasticsearch/search_agg_index_config.json').read())
+		params = json.loads(open('./meerkat/web_service/config/hyperparameters/search_agg_index_config.json').read())
 	#pprint(params)
 
 	requests = []
