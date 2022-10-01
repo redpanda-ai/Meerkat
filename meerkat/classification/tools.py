d87ac9b7bbd6f80343330a545865ddeaca77cca4
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-01 08:00:57 +0000

    Adding tarfile member sanitization to extractall()

--- meerkat/classification/tools.py
+++ meerkat/classification/tools.py
@@ -172,7 +172,26 @@ def extract_tarball(archive, destination):
 	with tarfile.open(name=archive, mode="r:gz") as tar:
 		members = tar.getmembers()
 		logging.debug("Members {0}".format(members))
-		tar.extractall(destination)
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner) 
+	
+
+safe_extract(tar, destination)
 
 def get_utc_iso_timestamp():
 	"""Returns a 16 digit ISO timestamp, accurate to the second that is suitable for S3
